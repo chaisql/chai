11173cda8c3f0b446a72f2621246344665bd62a1
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2018-12-30 11:35:00 +0100

    Renamed table reader to browser

---
+++ table/browser.go
@@ -0,0 +1,200 @@
+package table
+
+import (
+	"sort"
+
+	"github.com/asdine/genji/record"
+)
+
+type Browser struct {
+	Reader
+
+	err error
+}
+
+func NewBrowser(t Reader) Browser {
+	return Browser{
+		Reader: t,
+	}
+}
+
+func (b Browser) Err() error {
+	return b.err
+}
+
+func (b Browser) ForEach(fn func(record.Record) error) Browser {
+	if b.err != nil {
+		return b
+	}
+
+	c := b.Cursor()
+
+	for c.Next() {
+		if err := c.Err(); err != nil {
+			b.err = err
+			return b
+		}
+
+		err := fn(c.Record())
+		if err != nil {
+			b.err = err
+			return b
+		}
+	}
+
+	return b
+}
+
+func (b Browser) Filter(fn func(record.Record) (bool, error)) Browser {
+	var rb RecordBuffer
+
+	b = b.ForEach(func(r record.Record) error {
+		ok, err := fn(r)
+		if err != nil {
+			return err
+		}
+
+		if ok {
+			rb.Add(r)
+		}
+
+		return nil
+	})
+
+	if b.err == nil {
+		b.Reader = &rb
+	}
+
+	return b
+}
+
+func (b Browser) Map(fn func(record.Record) (record.Record, error)) Browser {
+	var rb RecordBuffer
+
+	b = b.ForEach(func(r record.Record) error {
+		r, err := fn(r)
+		if err != nil {
+			return err
+		}
+
+		rb.Add(r)
+		return nil
+	})
+
+	if b.err == nil {
+		b.Reader = rb
+	}
+
+	return b
+}
+
+func (b Browser) GroupBy(fieldName string) BrowserGroup {
+	var g BrowserGroup
+
+	if b.err != nil {
+		g.err = b.err
+		return g
+	}
+
+	m := make(map[string]*RecordBuffer)
+	var values []string
+
+	tr := b.ForEach(func(r record.Record) error {
+		f, err := r.Field(fieldName)
+		if err != nil {
+			return err
+		}
+
+		k := string(f.Data)
+		tr, ok := m[k]
+		if !ok {
+			tr = new(RecordBuffer)
+			m[k] = tr
+			values = append(values, k)
+		}
+
+		tr.Add(r)
+		return nil
+	})
+
+	if err := tr.Err(); err != nil {
+		g.err = err
+		return g
+	}
+
+	sort.Strings(values)
+
+	for _, v := range values {
+		g.Readers = append(g.Readers, NewBrowser(m[v]))
+	}
+
+	return g
+}
+
+func (b Browser) Chunk(n int) BrowserGroup {
+	var g BrowserGroup
+
+	i := 0
+	var fb RecordBuffer
+	b = b.ForEach(func(r record.Record) error {
+		if i%n == 0 {
+			fb = RecordBuffer{}
+			g.Readers = append(g.Readers, NewBrowser(&fb))
+		}
+
+		fb.Add(r)
+		i++
+		return nil
+	})
+
+	if b.err != nil {
+		g.err = b.err
+	}
+
+	return g
+}
+
+func (b Browser) Count() (int, error) {
+	if b.err != nil {
+		return 0, b.err
+	}
+
+	counter := 0
+	b = b.ForEach(func(r record.Record) error {
+		counter++
+		return nil
+	})
+
+	return counter, b.err
+}
+
+type BrowserGroup struct {
+	Readers []Browser
+	err     error
+}
+
+func (g BrowserGroup) Err() error {
+	return g.err
+}
+
+func (g BrowserGroup) Concat() Browser {
+	var b Browser
+
+	if g.err != nil {
+		b.err = g.err
+		return b
+	}
+
+	var fb RecordBuffer
+
+	for _, r := range g.Readers {
+		err := fb.AddFrom(r)
+		if err != nil {
+			b.err = err
+			return b
+		}
+	}
+
+	b.Reader = &fb
+	return b
+}
---
+++ table/browser_test.go
@@ -0,0 +1,233 @@
+package table
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/asdine/genji/field"
+	"github.com/asdine/genji/record"
+	"github.com/stretchr/testify/require"
+)
+
+func createTable(t require.TestingT, size int) Browser {
+	var rb RecordBuffer
+
+	for i := 0; i < size; i++ {
+		rb.Add(record.FieldBuffer{
+			field.NewInt64("id", int64(i)),
+			field.NewString("name", fmt.Sprintf("john-%d", i)),
+			field.NewInt64("age", int64(i*10)),
+			field.NewInt64("group", int64(i%3)),
+		})
+	}
+
+	return NewBrowser(&rb)
+}
+
+func TestBrowser(t *testing.T) {
+	b := createTable(t, 10)
+
+	t.Run("ForEach", func(t *testing.T) {
+		t.Run("Order", func(t *testing.T) {
+			i := 0
+			b = b.ForEach(func(r record.Record) error {
+				f, err := r.Field("id")
+				require.NoError(t, err)
+				v, err := field.DecodeInt64(f.Data)
+				require.NoError(t, err)
+				require.EqualValues(t, i, v)
+
+				i++
+				return nil
+			})
+
+			require.NoError(t, b.Err())
+		})
+
+		t.Run("Error", func(t *testing.T) {
+			err := errors.New("some error")
+			b2 := b.ForEach(func(r record.Record) error {
+				return err
+			})
+
+			// browsers are immutable, b should not be changed
+			require.NoError(t, b.Err())
+			require.Equal(t, err, b2.Err())
+		})
+	})
+
+	t.Run("Filter", func(t *testing.T) {
+		// filter odd ids
+		b2 := b.Filter(func(r record.Record) (bool, error) {
+			f, err := r.Field("id")
+			require.NoError(t, err)
+			v, err := field.DecodeInt64(f.Data)
+			require.NoError(t, err)
+			if v%2 == 0 {
+				return true, nil
+			}
+
+			return false, nil
+		})
+
+		require.NoError(t, b2.Err())
+
+		t.Run("Immutable", func(t *testing.T) {
+			// browsers are immutable, b should not be changed
+			count := 0
+			b := b.ForEach(func(r record.Record) error {
+				count++
+				return nil
+			})
+			require.NoError(t, b.Err())
+			require.Equal(t, 10, count)
+		})
+
+		t.Run("OK", func(t *testing.T) {
+			// b2 should only contain even ids
+			count := 0
+			b := b2.ForEach(func(r record.Record) error {
+				f, err := r.Field("id")
+				require.NoError(t, err)
+				v, err := field.DecodeInt64(f.Data)
+				require.NoError(t, err)
+				require.True(t, v%2 == 0)
+				count++
+				return nil
+			})
+			require.NoError(t, b.Err())
+			require.Equal(t, 5, count)
+		})
+
+		t.Run("Error", func(t *testing.T) {
+			err := errors.New("some error")
+			b2 := b.Filter(func(r record.Record) (bool, error) {
+				return false, err
+			})
+			require.NoError(t, b.Err())
+			require.Equal(t, err, b2.Err())
+		})
+	})
+
+	t.Run("Map", func(t *testing.T) {
+		// double the age
+		b2 := b.Map(func(r record.Record) (record.Record, error) {
+			f, err := r.Field("age")
+			require.NoError(t, err)
+			age, err := field.DecodeInt64(f.Data)
+			require.NoError(t, err)
+
+			var fb record.FieldBuffer
+			fb.AddFrom(r)
+
+			fb.Set(field.NewInt64("age", age*2))
+
+			return &fb, nil
+		})
+
+		require.NoError(t, b2.Err())
+
+		t.Run("Immutable", func(t *testing.T) {
+			// browsers are immutable, b should not be changed
+			i := 0
+			b := b.ForEach(func(r record.Record) error {
+				f, err := r.Field("age")
+				require.NoError(t, err)
+				age, err := field.DecodeInt64(f.Data)
+				require.NoError(t, err)
+
+				require.Equal(t, int64(i*10), age)
+				i++
+				return nil
+			})
+			require.NoError(t, b.Err())
+		})
+
+		t.Run("OK", func(t *testing.T) {
+			// b2 ages should be twice as big
+			i := 0
+			b := b2.ForEach(func(r record.Record) error {
+				f, err := r.Field("age")
+				require.NoError(t, err)
+				age, err := field.DecodeInt64(f.Data)
+				require.NoError(t, err)
+
+				require.Equal(t, int64(i*20), age)
+				i++
+				return nil
+			})
+			require.NoError(t, b.Err())
+		})
+
+		t.Run("Error", func(t *testing.T) {
+			err := errors.New("some error")
+			b2 := b.Map(func(r record.Record) (record.Record, error) {
+				return nil, err
+			})
+			require.NoError(t, b.Err())
+			require.Equal(t, err, b2.Err())
+		})
+	})
+
+	t.Run("Count", func(t *testing.T) {
+		t.Run("Ok", func(t *testing.T) {
+			total, err := b.Count()
+			require.NoError(t, err)
+			require.Equal(t, 10, total)
+		})
+	})
+
+	t.Run("GroupBy", func(t *testing.T) {
+		t.Run("Ok", func(t *testing.T) {
+
+			g := b.GroupBy("group")
+			for i, b := range g.Readers {
+				b.ForEach(func(r record.Record) error {
+					f, err := r.Field("group")
+					require.NoError(t, err)
+
+					j, err := field.DecodeInt64(f.Data)
+					require.NoError(t, err)
+
+					require.EqualValues(t, i, j)
+					return nil
+				})
+			}
+
+			require.NoError(t, g.Err())
+		})
+	})
+
+	t.Run("Count", func(t *testing.T) {
+		t.Run("Ok", func(t *testing.T) {
+			total, err := b.Count()
+			require.NoError(t, err)
+			require.Equal(t, 10, total)
+		})
+	})
+
+	t.Run("Chunk", func(t *testing.T) {
+		t.Run("Ok", func(t *testing.T) {
+			g := b.Chunk(2)
+			require.NoError(t, g.Err())
+			for _, r := range g.Readers {
+				total, err := r.Count()
+				require.NoError(t, err)
+				require.Equal(t, 2, total)
+			}
+		})
+	})
+}
+
+func TestBrowserGroup(t *testing.T) {
+	g := createTable(t, 10).Chunk(2)
+
+	t.Run("Concat", func(t *testing.T) {
+		r := g.Concat()
+		require.NoError(t, r.Err())
+		c, err := r.Count()
+		require.NoError(t, err)
+		require.Equal(t, 10, c)
+	})
+}
