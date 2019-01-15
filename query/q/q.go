8668a53a2032301fd5e9b9961833130e4c502a46
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-01-15 22:29:04 +0100

    Add String matchers in q package

--- query/q/q.go
+++ query/q/q.go
@@ -33,7 +33,7 @@ func (m *IndexMatcher) MatchIndex(im map[string]index.Index) ([][]byte, error) {
 	return m.fn(im)
 }
 
-func compareInts(f Field, i int, op func(a, b int) bool) func(r record.Record) (bool, error) {
+func compareInts(f Field, op func(int64) bool) func(r record.Record) (bool, error) {
 	return func(r record.Record) (bool, error) {
 		rf, err := r.Field(f.Name())
 		if err != nil {
@@ -49,144 +49,253 @@ func compareInts(f Field, i int, op func(a, b int) bool) func(r record.Record) (
 			return false, err
 		}
 
-		return op(int(v), i), nil
+		return op(v), nil
 	}
 }
 
-func EqInt(f Field, i int) *IndexMatcher {
-	data := field.EncodeInt64(int64(i))
+func compareStrings(f Field, op func([]byte) bool) func(r record.Record) (bool, error) {
+	return func(r record.Record) (bool, error) {
+		rf, err := r.Field(f.Name())
+		if err != nil {
+			return false, err
+		}
 
+		if rf.Type != field.String {
+			return false, errors.New("type mismatch")
+		}
+
+		return op(rf.Data), nil
+	}
+}
+
+func eqIndexMatcher(data []byte, idx index.Index) ([][]byte, error) {
+	c := idx.Cursor()
+	v, rowid := c.Seek(data)
+	var rowids [][]byte
+	for rowid != nil && bytes.Equal(data, v) {
+		rowids = append(rowids, rowid)
+		v, rowid = c.Next()
+	}
+
+	return rowids, nil
+}
+
+func gtIndexMatcher(data []byte, idx index.Index) ([][]byte, error) {
+	c := idx.Cursor()
+	v, rowid := c.Seek(data)
+	var rowids [][]byte
+	for rowid != nil {
+		if !bytes.Equal(data, v) {
+			rowids = append(rowids, rowid)
+		}
+
+		v, rowid = c.Next()
+	}
+
+	return rowids, nil
+}
+
+func gteIndexMatcher(data []byte, idx index.Index) ([][]byte, error) {
+	c := idx.Cursor()
+	_, rowid := c.Seek(data)
+	var rowids [][]byte
+	for rowid != nil {
+		rowids = append(rowids, rowid)
+		_, rowid = c.Next()
+	}
+
+	return rowids, nil
+}
+
+func ltIndexMatcher(data []byte, idx index.Index) ([][]byte, error) {
+	c := idx.Cursor()
+	v, rowid := c.Seek(data)
+	v, rowid = c.Prev()
+	var rowids [][]byte
+	for rowid != nil {
+		if !bytes.Equal(data, v) {
+			rowids = append([][]byte{rowid}, rowids...)
+		}
+		v, rowid = c.Prev()
+	}
+
+	return rowids, nil
+}
+
+func lteIndexMatcher(data []byte, idx index.Index) ([][]byte, error) {
+	c := idx.Cursor()
+	v, rowid := c.Seek(data)
+
+	for bytes.Equal(v, data) {
+		v, rowid = c.Next()
+	}
+	if v == nil {
+		v, rowid = c.Last()
+	}
+
+	var rowids [][]byte
+	for rowid != nil {
+		if bytes.Compare(v, data) <= 0 {
+			rowids = append([][]byte{rowid}, rowids...)
+		}
+
+		v, rowid = c.Prev()
+	}
+
+	return rowids, nil
+}
+
+func EqInt(f Field, i int) *IndexMatcher {
+	base := int64(i)
 	return &IndexMatcher{
 		Matcher: &Matcher{
-			fn: compareInts(f, i, func(a, b int) bool {
-				return a == b
+			fn: compareInts(f, func(v int64) bool {
+				return v == base
 			}),
 		},
 
 		fn: func(im map[string]index.Index) ([][]byte, error) {
-			idx := im[f.Name()]
-			c := idx.Cursor()
-			v, rowid := c.Seek(data)
-			var rowids [][]byte
-			for rowid != nil && bytes.Equal(data, v) {
-				rowids = append(rowids, rowid)
-				rowid, v = c.Next()
-			}
-
-			return rowids, nil
+			return eqIndexMatcher(field.EncodeInt64(base), im[f.Name()])
 		},
 	}
 }
 
 func GtInt(f Field, i int) *IndexMatcher {
-	data := field.EncodeInt64(int64(i))
-
+	base := int64(i)
 	return &IndexMatcher{
 		Matcher: &Matcher{
-			fn: compareInts(f, i, func(a, b int) bool {
-				return a > b
+			fn: compareInts(f, func(v int64) bool {
+				return v > base
 			}),
 		},
 
 		fn: func(im map[string]index.Index) ([][]byte, error) {
-			idx := im[f.Name()]
-			c := idx.Cursor()
-			v, rowid := c.Seek(data)
-			var rowids [][]byte
-			for rowid != nil {
-				if !bytes.Equal(data, v) {
-					rowids = append(rowids, rowid)
-				}
-
-				v, rowid = c.Next()
-			}
-
-			return rowids, nil
+			return gtIndexMatcher(field.EncodeInt64(base), im[f.Name()])
 		},
 	}
 }
 
 func GteInt(f Field, i int) *IndexMatcher {
-	data := field.EncodeInt64(int64(i))
-
+	base := int64(i)
 	return &IndexMatcher{
 		Matcher: &Matcher{
-			fn: compareInts(f, i, func(a, b int) bool {
-				return a >= b
+			fn: compareInts(f, func(v int64) bool {
+				return v >= base
 			}),
 		},
 
 		fn: func(im map[string]index.Index) ([][]byte, error) {
-			idx := im[f.Name()]
-			c := idx.Cursor()
-			_, rowid := c.Seek(data)
-			var rowids [][]byte
-			for rowid != nil {
-				rowids = append(rowids, rowid)
-				_, rowid = c.Next()
-			}
-
-			return rowids, nil
+			return gteIndexMatcher(field.EncodeInt64(base), im[f.Name()])
 		},
 	}
 }
 
 func LtInt(f Field, i int) *IndexMatcher {
-	data := field.EncodeInt64(int64(i))
-
+	base := int64(i)
 	return &IndexMatcher{
 		Matcher: &Matcher{
-			fn: compareInts(f, i, func(a, b int) bool {
-				return a < b
+			fn: compareInts(f, func(v int64) bool {
+				return v < base
 			}),
 		},
 
 		fn: func(im map[string]index.Index) ([][]byte, error) {
-			idx := im[f.Name()]
-			c := idx.Cursor()
-			v, rowid := c.Seek(data)
-			v, rowid = c.Prev()
-			var rowids [][]byte
-			for rowid != nil {
-				if !bytes.Equal(data, v) {
-					rowids = append([][]byte{rowid}, rowids...)
-				}
-				v, rowid = c.Prev()
-			}
-
-			return rowids, nil
+			return ltIndexMatcher(field.EncodeInt64(base), im[f.Name()])
 		},
 	}
 }
 
 func LteInt(f Field, i int) *IndexMatcher {
-	data := field.EncodeInt64(int64(i))
-
+	base := int64(i)
 	return &IndexMatcher{
 		Matcher: &Matcher{
-			fn: compareInts(f, i, func(a, b int) bool {
-				return a <= b
+			fn: compareInts(f, func(v int64) bool {
+				return v <= base
 			}),
 		},
 
 		fn: func(im map[string]index.Index) ([][]byte, error) {
-			idx := im[f.Name()]
-			c := idx.Cursor()
-			v, rowid := c.Seek(data)
-			if rowid == nil {
-				v, rowid = c.Prev()
-			}
+			return lteIndexMatcher(field.EncodeInt64(base), im[f.Name()])
+		},
+	}
+}
 
-			var rowids [][]byte
-			for rowid != nil {
-				if bytes.Compare(v, data) <= 0 {
-					rowids = append([][]byte{rowid}, rowids...)
-				}
+func EqStr(f Field, s string) *IndexMatcher {
+	base := []byte(s)
 
-				v, rowid = c.Prev()
-			}
+	return &IndexMatcher{
+		Matcher: &Matcher{
+			fn: compareStrings(f, func(v []byte) bool {
+				return bytes.Equal(v, base)
+			}),
+		},
 
-			return rowids, nil
+		fn: func(im map[string]index.Index) ([][]byte, error) {
+			return eqIndexMatcher(base, im[f.Name()])
+		},
+	}
+}
+
+func GtStr(f Field, s string) *IndexMatcher {
+	base := []byte(s)
+
+	return &IndexMatcher{
+		Matcher: &Matcher{
+			fn: compareStrings(f, func(v []byte) bool {
+				return bytes.Compare(v, base) > 0
+			}),
+		},
+
+		fn: func(im map[string]index.Index) ([][]byte, error) {
+			return gtIndexMatcher(base, im[f.Name()])
+		},
+	}
+}
+
+func GteStr(f Field, s string) *IndexMatcher {
+	base := []byte(s)
+
+	return &IndexMatcher{
+		Matcher: &Matcher{
+			fn: compareStrings(f, func(v []byte) bool {
+				return bytes.Compare(v, base) >= 0
+			}),
+		},
+
+		fn: func(im map[string]index.Index) ([][]byte, error) {
+			return gteIndexMatcher(base, im[f.Name()])
+		},
+	}
+}
+
+func LtStr(f Field, s string) *IndexMatcher {
+	base := []byte(s)
+
+	return &IndexMatcher{
+		Matcher: &Matcher{
+			fn: compareStrings(f, func(v []byte) bool {
+				return bytes.Compare(v, base) < 0
+			}),
+		},
+
+		fn: func(im map[string]index.Index) ([][]byte, error) {
+			return ltIndexMatcher(base, im[f.Name()])
+		},
+	}
+}
+
+func LteStr(f Field, s string) *IndexMatcher {
+	base := []byte(s)
+
+	return &IndexMatcher{
+		Matcher: &Matcher{
+			fn: compareStrings(f, func(v []byte) bool {
+				return bytes.Compare(v, base) <= 0
+			}),
+		},
+
+		fn: func(im map[string]index.Index) ([][]byte, error) {
+			return lteIndexMatcher(base, im[f.Name()])
 		},
 	}
 }
--- query/q/q_test.go
+++ query/q/q_test.go
@@ -58,7 +58,7 @@ func TestMatchers(t *testing.T) {
 	}
 }
 
-func createIndexMap(t *testing.T, ages ...int) (map[string]index.Index, func()) {
+func createIndexMap(t *testing.T, ages []int, teams []string) (map[string]index.Index, func()) {
 	dir, err := ioutil.TempDir("", "genji")
 	require.NoError(t, err)
 
@@ -68,6 +68,18 @@ func createIndexMap(t *testing.T, ages ...int) (map[string]index.Index, func()) 
 	tx, err := db.Begin(true)
 	require.NoError(t, err)
 
+	indexes := make(map[string]index.Index)
+	indexes["age"] = createIntIndex(t, tx, ages)
+	indexes["team"] = createStrIndex(t, tx, teams)
+
+	return indexes, func() {
+		tx.Rollback()
+		db.Close()
+		os.RemoveAll(dir)
+	}
+}
+
+func createIntIndex(t *testing.T, tx *bbolt.Tx, ages []int) index.Index {
 	b, err := tx.CreateBucket([]byte("age"))
 	require.NoError(t, err)
 
@@ -78,14 +90,21 @@ func createIndexMap(t *testing.T, ages ...int) (map[string]index.Index, func()) 
 		require.NoError(t, err)
 	}
 
-	indexes := make(map[string]index.Index)
-	indexes["age"] = idx
+	return idx
+}
 
-	return indexes, func() {
-		tx.Rollback()
-		db.Close()
-		os.RemoveAll(dir)
+func createStrIndex(t *testing.T, tx *bbolt.Tx, teams []string) index.Index {
+	b, err := tx.CreateBucket([]byte("team"))
+	require.NoError(t, err)
+
+	idx := bolt.NewIndex(b)
+
+	for i, team := range teams {
+		err := idx.Set([]byte(team), field.EncodeInt64(int64(i)))
+		require.NoError(t, err)
 	}
+
+	return idx
 }
 
 func TestIndexMatchers(t *testing.T) {
@@ -93,7 +112,7 @@ func TestIndexMatchers(t *testing.T) {
 		MatchIndex(im map[string]index.Index) ([][]byte, error)
 	}
 
-	im, cleanup := createIndexMap(t, 1, 2, 2, 3, 5, 10)
+	im, cleanup := createIndexMap(t, []int{1, 2, 2, 3, 5, 10}, []string{"ACA", "LOSC", "OL", "OM", "OM", "PSG"})
 	defer cleanup()
 
 	tests := []struct {
@@ -101,25 +120,48 @@ func TestIndexMatchers(t *testing.T) {
 		matcher indexMatcher
 		rowids  []int64
 	}{
-		{"eq/one", q.EqInt(q.Field("age"), 10), []int64{5}},
-		{"eq/multiple", q.EqInt(q.Field("age"), 2), []int64{1, 2}},
-		{"eq/none", q.EqInt(q.Field("age"), 15), nil},
-		{"gt/>10", q.GtInt(q.Field("age"), 10), nil},
-		{"gt/>7", q.GtInt(q.Field("age"), 7), []int64{5}},
-		{"gt/>1", q.GtInt(q.Field("age"), 1), []int64{1, 2, 3, 4, 5}},
-		{"gt/>-1", q.GtInt(q.Field("age"), -1), []int64{0, 1, 2, 3, 4, 5}},
-		{"gte/>=11", q.GteInt(q.Field("age"), 11), nil},
-		{"gte/>=7", q.GteInt(q.Field("age"), 7), []int64{5}},
-		{"gte/>=2", q.GteInt(q.Field("age"), 2), []int64{1, 2, 3, 4, 5}},
-		{"gte/>=1", q.GteInt(q.Field("age"), 1), []int64{0, 1, 2, 3, 4, 5}},
-		{"lt/<1", q.LtInt(q.Field("age"), 1), nil},
-		{"lt/<4", q.LtInt(q.Field("age"), 4), []int64{0, 1, 2, 3}},
-		{"lt/<10", q.LtInt(q.Field("age"), 10), []int64{0, 1, 2, 3, 4}},
-		{"lt/<11", q.LtInt(q.Field("age"), 11), []int64{0, 1, 2, 3, 4, 5}},
-		{"lte/<=0", q.LteInt(q.Field("age"), 0), nil},
-		{"lte/<=4", q.LteInt(q.Field("age"), 4), []int64{0, 1, 2, 3}},
-		{"lte/<=10", q.LteInt(q.Field("age"), 10), []int64{0, 1, 2, 3, 4, 5}},
-		{"lte/<=11", q.LteInt(q.Field("age"), 11), []int64{0, 1, 2, 3, 4, 5}},
+		{"eq/int/one", q.EqInt(q.Field("age"), 10), []int64{5}},
+		{"eq/int/multiple", q.EqInt(q.Field("age"), 2), []int64{1, 2}},
+		{"eq/int/none", q.EqInt(q.Field("age"), 15), nil},
+		{"eq/str/one", q.EqStr(q.Field("team"), "PSG"), []int64{5}},
+		{"eq/str/multiple", q.EqStr(q.Field("team"), "OM"), []int64{3, 4}},
+		{"eq/str/none", q.EqStr(q.Field("team"), "SCB"), nil},
+		{"gt/int/>10", q.GtInt(q.Field("age"), 10), nil},
+		{"gt/int/>7", q.GtInt(q.Field("age"), 7), []int64{5}},
+		{"gt/int/>1", q.GtInt(q.Field("age"), 1), []int64{1, 2, 3, 4, 5}},
+		{"gt/int/>-1", q.GtInt(q.Field("age"), -1), []int64{0, 1, 2, 3, 4, 5}},
+		{"gt/str/>PSG", q.GtStr(q.Field("team"), "PSG"), nil},
+		{"gt/str/>OM", q.GtStr(q.Field("team"), "OM"), []int64{5}},
+		{"gt/str/>NICE", q.GtStr(q.Field("team"), "NICE"), []int64{2, 3, 4, 5}},
+		{"gt/str/>ACA", q.GtStr(q.Field("team"), "ACA"), []int64{1, 2, 3, 4, 5}},
+		{"gt/str/>A", q.GtStr(q.Field("team"), "A"), []int64{0, 1, 2, 3, 4, 5}},
+		{"gte/int/>=11", q.GteInt(q.Field("age"), 11), nil},
+		{"gte/int/>=7", q.GteInt(q.Field("age"), 7), []int64{5}},
+		{"gte/int/>=2", q.GteInt(q.Field("age"), 2), []int64{1, 2, 3, 4, 5}},
+		{"gte/int/>=1", q.GteInt(q.Field("age"), 1), []int64{0, 1, 2, 3, 4, 5}},
+		{"gte/str/>=PSG", q.GteStr(q.Field("team"), "PSG"), []int64{5}},
+		{"gte/str/>=OM", q.GteStr(q.Field("team"), "OM"), []int64{3, 4, 5}},
+		{"gte/str/>=NICE", q.GteStr(q.Field("team"), "NICE"), []int64{2, 3, 4, 5}},
+		{"gte/str/>=ACA", q.GteStr(q.Field("team"), "ACA"), []int64{0, 1, 2, 3, 4, 5}},
+		{"lt/int/<1", q.LtInt(q.Field("age"), 1), nil},
+		{"lt/int/<4", q.LtInt(q.Field("age"), 4), []int64{0, 1, 2, 3}},
+		{"lt/int/<10", q.LtInt(q.Field("age"), 10), []int64{0, 1, 2, 3, 4}},
+		{"lt/int/<11", q.LtInt(q.Field("age"), 11), []int64{0, 1, 2, 3, 4, 5}},
+		{"lt/str/<A", q.LtStr(q.Field("team"), "A"), nil},
+		{"lt/str/<ACA", q.LtStr(q.Field("team"), "ACA"), nil},
+		{"lt/str/<NICE", q.LtStr(q.Field("team"), "NICE"), []int64{0, 1}},
+		{"lt/str/<OM", q.LtStr(q.Field("team"), "OM"), []int64{0, 1, 2}},
+		{"lt/str/<STRASBOURG", q.LtStr(q.Field("team"), "STRASBOURG"), []int64{0, 1, 2, 3, 4, 5}},
+		{"lte/int/<=0", q.LteInt(q.Field("age"), 0), nil},
+		{"lte/int/<=2", q.LteInt(q.Field("age"), 2), []int64{0, 1, 2}},
+		{"lte/int/<=4", q.LteInt(q.Field("age"), 4), []int64{0, 1, 2, 3}},
+		{"lte/int/<=10", q.LteInt(q.Field("age"), 10), []int64{0, 1, 2, 3, 4, 5}},
+		{"lte/int/<=11", q.LteInt(q.Field("age"), 11), []int64{0, 1, 2, 3, 4, 5}},
+		{"lte/str/<=A", q.LteStr(q.Field("team"), "A"), nil},
+		{"lte/str/<=ACA", q.LteStr(q.Field("team"), "ACA"), []int64{0}},
+		{"lte/str/<=NICE", q.LteStr(q.Field("team"), "NICE"), []int64{0, 1}},
+		{"lte/str/<=OM", q.LteStr(q.Field("team"), "OM"), []int64{0, 1, 2, 3, 4}},
+		{"lte/str/<=STRASBOURG", q.LteStr(q.Field("team"), "STRASBOURG"), []int64{0, 1, 2, 3, 4, 5}},
 	}
 
 	for _, test := range tests {
