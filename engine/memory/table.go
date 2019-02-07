2d317338f638a23e5784384ba2386f5a816f36df
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-02-07 17:04:00 -0500

    Add memory engine Table Cursor implementation

--- engine/memory/table.go
+++ engine/memory/table.go
@@ -1,18 +1,19 @@
 package memory
 
 import (
-	"bytes"
 	"errors"
+	"io"
 	"sync/atomic"
 
 	"github.com/asdine/genji/field"
 	"github.com/asdine/genji/record"
-	"github.com/google/btree"
+	tab "github.com/asdine/genji/table"
+	"modernc.org/b"
 )
 
 type table struct {
 	writable bool
-	tree     *btree.BTree
+	tree     *b.Tree
 	xid      uint64
 	counter  uint64
 }
@@ -23,17 +24,13 @@ type item struct {
 	xmin   uint64
 }
 
-func (i *item) Less(than btree.Item) bool {
-	return bytes.Compare(i.rowid, than.(*item).rowid) < 0
-}
-
 func (t *table) Record(rowid []byte) (record.Record, error) {
-	i := t.tree.Get(&item{rowid: rowid})
-	if i == nil {
+	v, ok := t.tree.Get(rowid)
+	if !ok {
 		return nil, errors.New("not found")
 	}
 
-	it := i.(*item)
+	it := v.(*item)
 	if it.xmin > t.xid {
 		return nil, errors.New("not found")
 	}
@@ -48,11 +45,60 @@ func (t *table) Insert(r record.Record) (rowid []byte, err error) {
 
 	rid := field.EncodeInt64(int64(atomic.AddUint64(&t.counter, 1)))
 
-	t.tree.ReplaceOrInsert(&item{
+	t.tree.Set(rid, &item{
 		record: r,
 		xmin:   t.xid,
 		rowid:  rid,
 	})
 
 	return rid, nil
+}
+
+func (t *table) Cursor() (tab.Cursor, error) {
+	enum, err := t.tree.SeekFirst()
+	if err != nil {
+		return nil, err
+	}
+
+	return &cursor{
+		xid:  t.xid,
+		enum: enum,
+	}, nil
+}
+
+type cursor struct {
+	xid      uint64
+	enum     *b.Enumerator
+	curRowid []byte
+	curItem  *item
+	err      error
+}
+
+func (c *cursor) Next() bool {
+	for {
+		k, v, err := c.enum.Next()
+		if err == io.EOF {
+			return false
+		}
+
+		if err != nil {
+			c.err = err
+			return false
+		}
+
+		it := v.(*item)
+		if it.xmin < c.xid {
+			c.curRowid = k.([]byte)
+			c.curItem = it
+			return true
+		}
+	}
+}
+
+func (c *cursor) Err() error {
+	return c.err
+}
+
+func (c *cursor) Record() record.Record {
+	return c.curItem.record
 }
