10add48738a425b05425ef80ee0f779a1f353f5b
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-02-15 23:27:09 +0100

    Get indexes in Insert method

--- db.go
+++ db.go
@@ -2,7 +2,6 @@ package genji
 
 import (
 	"github.com/asdine/genji/engine"
-	"github.com/asdine/genji/index"
 	"github.com/asdine/genji/record"
 	"github.com/asdine/genji/table"
 )
@@ -36,26 +35,31 @@ func (tx Transaction) Table(name string) (table.Table, error) {
 		return nil, err
 	}
 
-	indexes, err := tx.Transaction.Indexes(name)
+	return &Table{
+		Table: tb,
+		tx:    tx.Transaction,
+		name:  name,
+	}, nil
+}
+
+func (tx Transaction) CreateTable(name string) (table.Table, error) {
+	tb, err := tx.Transaction.CreateTable(name)
 	if err != nil {
 		return nil, err
 	}
 
 	return &Table{
-		Table:   tb,
-		tx:      tx.Transaction,
-		indexes: indexes,
+		Table: tb,
+		tx:    tx.Transaction,
+		name:  name,
 	}, nil
-}
-
-func (tx Transaction) CreateTable(name string) (table.Table, error) {
-	return tx.Transaction.CreateTable(name)
 }
 
 type Table struct {
 	table.Table
-	tx      engine.Transaction
-	indexes map[string]index.Index
+
+	tx   engine.Transaction
+	name string
 }
 
 func (t Table) Insert(r record.Record) ([]byte, error) {
@@ -64,7 +68,12 @@ func (t Table) Insert(r record.Record) ([]byte, error) {
 		return nil, err
 	}
 
-	for fieldName, idx := range t.indexes {
+	indexes, err := t.tx.Indexes(t.name)
+	if err != nil {
+		return nil, err
+	}
+
+	for fieldName, idx := range indexes {
 		f, err := r.Field(fieldName)
 		if err != nil {
 			return nil, err
