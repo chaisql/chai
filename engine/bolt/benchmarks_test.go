af71693109b3a7e3f3361cabd911d1f6935460db
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-02-20 21:12:41 +0100

    Create benchmark file for Bolt

---
+++ engine/bolt/benchmarks_test.go
@@ -0,0 +1,172 @@
+package bolt
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/asdine/genji/field"
+	"github.com/asdine/genji/record"
+	bolt "github.com/etcd-io/bbolt"
+	"github.com/stretchr/testify/require"
+)
+
+func tempDir(t require.TestingT) (string, func()) {
+	dir, err := ioutil.TempDir("", "genji")
+	require.NoError(t, err)
+
+	return dir, func() {
+		os.RemoveAll(dir)
+	}
+}
+
+func tempDB(t require.TestingT) (*bolt.DB, func()) {
+	dir, cleanup := tempDir(t)
+	db, err := bolt.Open(path.Join(dir, "test.db"), 0660, nil)
+	if err != nil {
+		cleanup()
+		require.NoError(t, err)
+	}
+
+	return db, func() {
+		db.Close()
+		cleanup()
+	}
+}
+
+func tempBucket(t require.TestingT, writable bool) (*bolt.Bucket, func()) {
+	db, cleanup := tempDB(t)
+
+	tx, err := db.Begin(writable)
+	if err != nil {
+		cleanup()
+		require.NoError(t, err)
+	}
+
+	b, err := tx.CreateBucketIfNotExists([]byte("test"))
+	if err != nil {
+		cleanup()
+		require.NoError(t, err)
+	}
+
+	return b, func() {
+		tx.Rollback()
+		cleanup()
+	}
+}
+
+func countItems(t require.TestingT, b *bolt.Bucket) int {
+	i := 0
+	err := b.ForEach(func(k, v []byte) error {
+		i++
+		return nil
+	})
+	require.NoError(t, err)
+
+	return i
+}
+
+func benchmarkTableInsert(b *testing.B, size int) {
+	db, cleanup := tempDB(b)
+	defer cleanup()
+
+	var fields []field.Field
+
+	for i := int64(0); i < 10; i++ {
+		fields = append(fields, field.NewInt64(fmt.Sprintf("name-%d", i), i))
+	}
+
+	rec := record.FieldBuffer(fields)
+
+	b.ResetTimer()
+	b.StopTimer()
+	for i := 0; i < b.N; i++ {
+		tx, err := db.Begin(true)
+		require.NoError(b, err)
+		bck, err := tx.CreateBucket([]byte("test"))
+		require.NoError(b, err)
+		tab := &Table{
+			Bucket: bck,
+		}
+
+		b.StartTimer()
+		for j := 0; j < size; j++ {
+			tab.Insert(rec)
+		}
+		b.StopTimer()
+
+		tx.Rollback()
+	}
+}
+
+func BenchmarkTableInsert1(b *testing.B) {
+	benchmarkTableInsert(b, 1)
+}
+
+func BenchmarkTableInsert10(b *testing.B) {
+	benchmarkTableInsert(b, 10)
+}
+
+func BenchmarkTableInsert100(b *testing.B) {
+	benchmarkTableInsert(b, 100)
+}
+
+func BenchmarkTableInsert1000(b *testing.B) {
+	benchmarkTableInsert(b, 1000)
+}
+
+func BenchmarkTableInsert10000(b *testing.B) {
+	benchmarkTableInsert(b, 10000)
+}
+
+func benchmarkTableScan(b *testing.B, size int) {
+	bucket, cleanup := tempBucket(b, true)
+	defer cleanup()
+
+	tab := &Table{
+		Bucket: bucket,
+	}
+
+	var fields []field.Field
+
+	for i := int64(0); i < 10; i++ {
+		fields = append(fields, field.NewInt64(fmt.Sprintf("name-%d", i), i))
+	}
+
+	rec := record.FieldBuffer(fields)
+
+	for i := 0; i < size; i++ {
+		_, err := tab.Insert(rec)
+		require.NoError(b, err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		tab.Iterate(func([]byte, record.Record) bool {
+			return true
+		})
+	}
+	b.StopTimer()
+}
+
+func BenchmarkTableScan1(b *testing.B) {
+	benchmarkTableScan(b, 1)
+}
+
+func BenchmarkTableScan10(b *testing.B) {
+	benchmarkTableScan(b, 10)
+}
+
+func BenchmarkTableScan100(b *testing.B) {
+	benchmarkTableScan(b, 100)
+}
+
+func BenchmarkTableScan1000(b *testing.B) {
+	benchmarkTableScan(b, 1000)
+}
+
+func BenchmarkTableScan10000(b *testing.B) {
+	benchmarkTableScan(b, 10000)
+}
--- engine/bolt/table_test.go
+++ engine/bolt/table_test.go
@@ -1,12 +1,9 @@
 package bolt
 
 import (
-	"fmt"
 	"path"
 	"testing"
 
-	"github.com/asdine/genji/field"
-	"github.com/asdine/genji/record"
 	"github.com/asdine/genji/table"
 	"github.com/asdine/genji/table/tabletest"
 	"github.com/stretchr/testify/require"
@@ -29,51 +26,5 @@ func TestBoltEngineTable(t *testing.T) {
 			ng.Close()
 			cleanup()
 		}
-	})
-}
-
-func TestTableInsert(t *testing.T) {
-	b, cleanup := tempBucket(t, true)
-	defer cleanup()
-
-	table := Table{Bucket: b}
-	rowid, err := table.Insert(record.FieldBuffer([]field.Field{
-		field.NewInt64("a", 10),
-	}))
-	require.NoError(t, err)
-	require.NotEmpty(t, rowid)
-}
-
-func TestTableIterate(t *testing.T) {
-	b, cleanup := tempBucket(t, true)
-	defer cleanup()
-
-	table := Table{Bucket: b}
-	for i := 0; i < 10; i++ {
-		_, err := table.Insert(record.FieldBuffer([]field.Field{
-			field.NewString("name", fmt.Sprintf("name-%d", i)),
-			field.NewInt64("age", int64(i)),
-		}))
-		require.NoError(t, err)
-	}
-
-	i := 0
-	table.Iterate(func(rowid []byte, r record.Record) bool {
-		rc := r.Cursor()
-		for rc.Next() {
-			require.NoError(t, rc.Err())
-			f := rc.Field()
-
-			switch f.Name {
-			case "name":
-				require.Equal(t, fmt.Sprintf("name-%d", i), string(f.Data))
-			case "age":
-				age, err := field.DecodeInt64(f.Data)
-				require.NoError(t, err)
-				require.EqualValues(t, i, age)
-			}
-		}
-		i++
-		return true
 	})
 }
