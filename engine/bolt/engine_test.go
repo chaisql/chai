d1723980e1319e99a77099057589036865cbf1b5
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-02-21 22:42:39 +0100

    Run tabletest benchmark in Bolt engine

--- engine/bolt/engine_test.go
+++ engine/bolt/engine_test.go
@@ -13,7 +13,6 @@ import (
 	"github.com/asdine/genji/index/indextest"
 	"github.com/asdine/genji/table"
 	"github.com/asdine/genji/table/tabletest"
-	bbolt "github.com/etcd-io/bbolt"
 	"github.com/stretchr/testify/require"
 )
 
@@ -50,7 +49,19 @@ func TestBoltEngineIndex(t *testing.T) {
 }
 
 func TestBoltEngineTable(t *testing.T) {
-	tabletest.TestSuite(t, func() (table.Table, func()) {
+	tabletest.TestSuite(t, tableBuilder(t))
+}
+
+func BenchmarkBoltEngineTableInsert(b *testing.B) {
+	tabletest.BenchmarkTableInsert(b, tableBuilder(b))
+}
+
+func BenchmarkBoltEngineTableScan(b *testing.B) {
+	tabletest.BenchmarkTableScan(b, tableBuilder(b))
+}
+
+func tableBuilder(t require.TestingT) func() (table.Table, func()) {
+	return func() (table.Table, func()) {
 		dir, cleanup := tempDir(t)
 		ng, err := bolt.NewEngine(path.Join(dir, "test.db"), 0600, nil)
 		require.NoError(t, err)
@@ -66,7 +77,7 @@ func TestBoltEngineTable(t *testing.T) {
 			ng.Close()
 			cleanup()
 		}
-	})
+	}
 }
 
 func tempDir(t require.TestingT) (string, func()) {
@@ -76,50 +87,4 @@ func tempDir(t require.TestingT) (string, func()) {
 	return dir, func() {
 		os.RemoveAll(dir)
 	}
-}
-
-func tempDB(t require.TestingT) (*bbolt.DB, func()) {
-	dir, cleanup := tempDir(t)
-	db, err := bbolt.Open(path.Join(dir, "test.db"), 0660, nil)
-	if err != nil {
-		cleanup()
-		require.NoError(t, err)
-	}
-
-	return db, func() {
-		db.Close()
-		cleanup()
-	}
-}
-
-func tempBucket(t require.TestingT, writable bool) (*bbolt.Bucket, func()) {
-	db, cleanup := tempDB(t)
-
-	tx, err := db.Begin(writable)
-	if err != nil {
-		cleanup()
-		require.NoError(t, err)
-	}
-
-	b, err := tx.CreateBucketIfNotExists([]byte("test"))
-	if err != nil {
-		cleanup()
-		require.NoError(t, err)
-	}
-
-	return b, func() {
-		tx.Rollback()
-		cleanup()
-	}
-}
-
-func countItems(t require.TestingT, b *bbolt.Bucket) int {
-	i := 0
-	err := b.ForEach(func(k, v []byte) error {
-		i++
-		return nil
-	})
-	require.NoError(t, err)
-
-	return i
 }
