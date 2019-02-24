a64fbdc3b03451ba5cb6ee24111b70dce2fba2ee
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-02-24 15:53:23 +0100

    Replace record Cursor by Iterate method

--- record/record.go
+++ record/record.go
@@ -9,21 +9,7 @@ import (
 // A Record holds a group of fields.
 type Record interface {
 	Field(string) (field.Field, error)
-	Cursor() Cursor
-}
-
-// A Cursor iterates over the fields of a record.
-type Cursor interface {
-	// Next advances the cursor to the next field which will then be available
-	// through the Field method. It returns false when the cursor stops.
-	// If an error occurs during iteration, the Err method will return it.
-	Next() bool
-
-	// Err returns the error, if any, that was encountered during iteration.
-	Err() error
-
-	// Field returns the current field.
-	Field() field.Field
+	Iterate(func(field.Field) bool) error
 }
 
 // FieldBuffer contains a list of fields. It implements the Record interface.
@@ -34,17 +20,10 @@ func (fb *FieldBuffer) Add(f field.Field) {
 }
 
 func (fb *FieldBuffer) AddFrom(r Record) error {
-	c := r.Cursor()
-
-	for c.Next() {
-		if c.Err() != nil {
-			return c.Err()
-		}
-
-		*fb = append(*fb, c.Field())
-	}
-
-	return nil
+	return r.Iterate(func(f field.Field) bool {
+		*fb = append(*fb, f)
+		return true
+	})
 }
 
 func (fb FieldBuffer) Field(name string) (field.Field, error) {
@@ -68,29 +47,13 @@ func (fb FieldBuffer) Set(f field.Field) {
 	fb.Add(f)
 }
 
-// Cursor creates a Cursor that iterate over the slice of fields.
-func (fb FieldBuffer) Cursor() Cursor {
-	return &fieldBufferCursor{buf: fb, i: -1}
-}
-
-type fieldBufferCursor struct {
-	i   int
-	buf FieldBuffer
-}
-
-func (c *fieldBufferCursor) Next() bool {
-	if c.i+1 >= len(c.buf) {
-		return false
+func (fb FieldBuffer) Iterate(fn func(field.Field) bool) error {
+	for _, f := range fb {
+		ok := fn(f)
+		if !ok {
+			return nil
+		}
 	}
 
-	c.i++
-	return true
-}
-
-func (c *fieldBufferCursor) Field() field.Field {
-	return c.buf[c.i]
-}
-
-func (c *fieldBufferCursor) Err() error {
 	return nil
 }
