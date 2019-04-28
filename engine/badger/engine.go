8b18df9daa7ffb7bfdc0be62db73c7eac619ecf7
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-04-28 16:34:39 +0200

    Add CreateTable method to Badger engine

--- engine/badger/engine.go
+++ engine/badger/engine.go
@@ -1,8 +1,17 @@
 package badger
 
 import (
+	"bytes"
+	"fmt"
+	"strings"
+
 	"github.com/asdine/genji/engine"
 	"github.com/dgraph-io/badger"
+)
+
+const (
+	separator     byte = 0x1F
+	tableListName      = "__genji.table"
 )
 
 type Engine struct {
@@ -21,14 +30,52 @@ func NewEngine(opts badger.Options) (*Engine, error) {
 }
 
 func (e *Engine) Begin(writable bool) (engine.Transaction, error) {
-	tx := e.DB.NewTransaction(writable)
+	_ = e.DB.NewTransaction(writable)
 
-	return &Transaction{
-		tx:       tx,
-		writable: writable,
-	}, nil
+	return nil, nil
 }
 
 func (e *Engine) Close() error {
 	return e.DB.Close()
+}
+
+type Transaction struct {
+	tx       *badger.Txn
+	writable bool
+}
+
+func (t *Transaction) Rollback() error {
+	t.tx.Discard()
+
+	return nil
+}
+
+func (t *Transaction) Commit() error {
+	return t.tx.Commit()
+}
+
+func (t *Transaction) CreateTable(name string) error {
+	if !t.writable {
+		return engine.ErrTransactionReadOnly
+	}
+
+	if idx := strings.IndexByte(name, separator); idx != -1 {
+		return fmt.Errorf("table name contains forbidden character at pos %d", idx)
+	}
+
+	var buf bytes.Buffer
+	buf.Grow(len(tableListName) + 1 + len(name))
+	buf.WriteString(tableListName)
+	buf.WriteByte(separator)
+	buf.WriteString(name)
+
+	_, err := t.tx.Get(buf.Bytes())
+	if err == nil {
+		return engine.ErrTableAlreadyExists
+	}
+	if err != badger.ErrKeyNotFound {
+		return err
+	}
+
+	return t.tx.Set(buf.Bytes(), nil)
 }
--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ require (
 	github.com/AndreasBriese/bbloom v0.0.0-20190306092124-e2d15f34fcf9 // indirect
 	github.com/boltdb/bolt v1.3.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/dgraph-io/badger v1.5.4
+	github.com/dgraph-io/badger v2.0.0-rc.2+incompatible
 	github.com/dgryski/go-farm v0.0.0-20190423205320-6a90982ecee2 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/etcd-io/bbolt v1.3.0
 	github.com/golang/protobuf v1.3.1 // indirect
 	github.com/google/btree v0.0.0-20180813153112-4030bb1f1f0c
