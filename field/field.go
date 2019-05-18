4459637fd31358cba9d5e69928a288eecb21aa53
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-05-18 12:17:59 +0200

    Add DecodeBool function

--- field/field.go
+++ field/field.go
@@ -3,6 +3,7 @@ package field
 import (
 	"encoding/binary"
 	"errors"
+	"math"
 )
 
 // Type represents a field type supported by the database.
@@ -34,247 +35,250 @@ type Field struct {
 	Data []byte
 }
 
-func NewString(name string, value string) Field {
+func NewString(name string, x string) Field {
 	return Field{
 		Name: name,
 		Type: String,
-		Data: []byte(value),
+		Data: []byte(x),
 	}
 }
 
-func NewBytes(name string, value []byte) Field {
+func NewBytes(name string, x []byte) Field {
 	return Field{
 		Name: name,
 		Type: Bytes,
-		Data: value,
+		Data: x,
 	}
 }
 
-func NewBool(name string, value bool) Field {
+func NewBool(name string, x bool) Field {
 	return Field{
 		Name: name,
 		Type: Bool,
-		Data: EncodeBool(value),
+		Data: EncodeBool(x),
 	}
 }
 
-func NewUint(name string, value uint) Field {
+func NewUint(name string, x uint) Field {
 	return Field{
 		Name: name,
 		Type: Uint,
-		Data: EncodeUint(value),
+		Data: EncodeUint(x),
 	}
 }
 
-func NewUint8(name string, value uint8) Field {
+func NewUint8(name string, x uint8) Field {
 	return Field{
 		Name: name,
 		Type: Uint8,
-		Data: EncodeUint8(value),
+		Data: EncodeUint8(x),
 	}
 }
 
-func NewUint16(name string, value uint16) Field {
+func NewUint16(name string, x uint16) Field {
 	return Field{
 		Name: name,
 		Type: Uint16,
-		Data: EncodeUint16(value),
+		Data: EncodeUint16(x),
 	}
 }
 
-func NewUint32(name string, value uint32) Field {
+func NewUint32(name string, x uint32) Field {
 	return Field{
 		Name: name,
 		Type: Uint32,
-		Data: EncodeUint32(value),
+		Data: EncodeUint32(x),
 	}
 }
 
-func NewUint64(name string, value uint64) Field {
+func NewUint64(name string, x uint64) Field {
 	return Field{
 		Name: name,
 		Type: Uint64,
-		Data: EncodeUint64(value),
+		Data: EncodeUint64(x),
 	}
 }
 
-func NewInt(name string, value int) Field {
+func NewInt(name string, x int) Field {
 	return Field{
 		Name: name,
 		Type: Int,
-		Data: EncodeInt(value),
+		Data: EncodeInt(x),
 	}
 }
 
-func NewInt8(name string, value int8) Field {
+func NewInt8(name string, x int8) Field {
 	return Field{
 		Name: name,
 		Type: Int8,
-		Data: EncodeInt8(value),
+		Data: EncodeInt8(x),
 	}
 }
 
-func NewInt16(name string, value int16) Field {
+func NewInt16(name string, x int16) Field {
 	return Field{
 		Name: name,
 		Type: Int16,
-		Data: EncodeInt16(value),
+		Data: EncodeInt16(x),
 	}
 }
 
-func NewInt32(name string, value int32) Field {
+func NewInt32(name string, x int32) Field {
 	return Field{
 		Name: name,
 		Type: Int32,
-		Data: EncodeInt32(value),
+		Data: EncodeInt32(x),
 	}
 }
 
-func NewInt64(name string, value int64) Field {
+func NewInt64(name string, x int64) Field {
 	return Field{
 		Name: name,
 		Type: Int64,
-		Data: EncodeInt64(value),
+		Data: EncodeInt64(x),
 	}
 }
 
-func NewFloat32(name string, value float32) Field {
+func NewFloat32(name string, x float32) Field {
 	return Field{
 		Name: name,
 		Type: Float32,
-		Data: EncodeFloat32(value),
+		Data: EncodeFloat32(x),
 	}
 }
 
-func NewFloat64(name string, value float64) Field {
+func NewFloat64(name string, x float64) Field {
 	return Field{
 		Name: name,
 		Type: Float64,
-		Data: EncodeFloat64(value),
+		Data: EncodeFloat64(x),
 	}
 }
 
-func EncodeBool(v bool) []byte {
-	if v {
+func EncodeBool(x bool) []byte {
+	if x {
 		return []byte{1}
 	}
 	return []byte{0}
 }
 
-func EncodeUint(v uint) []byte {
+func EncodeUint(x uint) []byte {
 	buf := make([]byte, binary.MaxVarintLen64)
-	n := binary.PutUvarint(buf, uint64(v))
+	n := binary.PutUvarint(buf, uint64(x))
 	return buf[:n]
 }
 
-func EncodeUint8(v uint8) []byte {
-	return []byte{v}
+func EncodeUint8(x uint8) []byte {
+	return []byte{x}
 }
 
-func EncodeUint16(v uint16) []byte {
+func EncodeUint16(x uint16) []byte {
 	var buf [2]byte
-	binary.BigEndian.PutUint16(buf[:], v)
+	binary.BigEndian.PutUint16(buf[:], x)
 	return buf[:]
 }
 
-func EncodeUint32(v uint32) []byte {
+func EncodeUint32(x uint32) []byte {
 	var buf [4]byte
-	binary.BigEndian.PutUint32(buf[:], v)
+	binary.BigEndian.PutUint32(buf[:], x)
 	return buf[:]
 }
 
-func EncodeUint64(v uint64) []byte {
+func EncodeUint64(x uint64) []byte {
 	var buf [8]byte
-	binary.BigEndian.PutUint64(buf[:], v)
+	binary.BigEndian.PutUint64(buf[:], x)
 	return buf[:]
 }
 
-func EncodeInt(v int) []byte {
+func EncodeInt(x int) []byte {
 	buf := make([]byte, binary.MaxVarintLen64)
-	n := binary.PutVarint(buf, int64(v))
+	n := binary.PutVarint(buf, int64(x))
 	return buf[:n]
 }
 
-func EncodeInt8(v int8) []byte {
-	return EncodeUint8(uint8(v))
+func EncodeInt8(x int8) []byte {
+	return EncodeUint8(uint8(x))
 }
 
-func EncodeInt16(v int16) []byte {
-	return EncodeUint16(uint16(v))
+func EncodeInt16(x int16) []byte {
+	return EncodeUint16(uint16(x))
 }
 
-func EncodeInt32(v int32) []byte {
-	return EncodeUint32(uint32(v))
+func EncodeInt32(x int32) []byte {
+	return EncodeUint32(uint32(x))
 }
 
 // EncodeInt64 takes an int64 and returns its binary representation.
-func EncodeInt64(v int64) []byte {
-	return EncodeUint64(uint64(v))
+func EncodeInt64(x int64) []byte {
+	return EncodeUint64(uint64(x))
 }
 
-func EncodeFloat32(v float32) []byte {
-	return nil
+func EncodeFloat32(x float32) []byte {
+	return EncodeUint32(math.Float32bits(x))
 }
 
-func EncodeFloat64(v float64) []byte {
-	return nil
+func EncodeFloat64(x float64) []byte {
+	return EncodeUint64(math.Float64bits(x))
 }
 
-func DecodeBool(v []byte) (bool, error) {
-	return false, nil
+func DecodeBool(buf []byte) (bool, error) {
+	if len(buf) != 1 {
+		return false, errors.New("cannot decode buffer to bool")
+	}
+	return buf[0] == 1, nil
 }
 
-func DecodeUint(v []byte) (uint, error) {
+func DecodeUint(buf []byte) (uint, error) {
 	return 0, nil
 }
 
-func DecodeUint8(v []byte) (uint8, error) {
+func DecodeUint8(buf []byte) (uint8, error) {
 	return 0, nil
 }
 
-func DecodeUint16(v []byte) (uint16, error) {
+func DecodeUint16(buf []byte) (uint16, error) {
 	return 0, nil
 }
 
-func DecodeUint32(v []byte) (uint32, error) {
+func DecodeUint32(buf []byte) (uint32, error) {
 	return 0, nil
 }
 
-func DecodeUint64(v []byte) (uint64, error) {
+func DecodeUint64(buf []byte) (uint64, error) {
 	return 0, nil
 }
 
-func DecodeInt(v []byte) (int, error) {
+func DecodeInt(buf []byte) (int, error) {
 	return 0, nil
 }
 
-func DecodeInt8(v []byte) (int8, error) {
+func DecodeInt8(buf []byte) (int8, error) {
 	return 0, nil
 }
 
-func DecodeInt16(v []byte) (int16, error) {
+func DecodeInt16(buf []byte) (int16, error) {
 	return 0, nil
 }
 
-func DecodeInt32(v []byte) (int32, error) {
+func DecodeInt32(buf []byte) (int32, error) {
 	return 0, nil
 }
 
 // DecodeInt64 takes a byte slice and decodes it into an int64.
 // An error is returned if the value is invalid.
-func DecodeInt64(v []byte) (int64, error) {
-	i, n := binary.Varint(v)
+func DecodeInt64(buf []byte) (int64, error) {
+	i, n := binary.Varint(buf)
 	if n <= 0 {
-		return 0, errors.New("field: cannot decode value to in64")
+		return 0, errors.New("cannot decode buffer to in64")
 	}
 
 	return i, nil
 }
 
-func DecodeFloat32(v []byte) (float32, error) {
+func DecodeFloat32(buf []byte) (float32, error) {
 	return 0, nil
 }
 
-func DecodeFloat64(v []byte) (float64, error) {
+func DecodeFloat64(buf []byte) (float64, error) {
 	return 0, nil
 }
