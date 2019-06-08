dbeec7a13a95ca0230236406ee1643492c0a3516
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-06-08 19:39:13 +0200

    Use Expr instead of matchers in Where method

--- query/query.go
+++ query/query.go
@@ -9,31 +9,10 @@ import (
 	"github.com/asdine/genji/table"
 )
 
-type Result struct {
-	t   table.Reader
-	err error
-}
-
-func (q Result) Err() error {
-	return q.err
-}
-
-func (q Result) Scan(s table.Scanner) error {
-	if q.err != nil {
-		return q.err
-	}
-
-	return s.ScanTable(q.t)
-}
-
-func (q Result) Table() table.Reader {
-	return q.t
-}
-
 type Query struct {
 	fieldSelectors []FieldSelector
 	tableSelector  TableSelector
-	matchers       []Matcher
+	whereExpr      Expr
 }
 
 func Select(selectors ...FieldSelector) Query {
@@ -50,24 +29,27 @@ func (q Query) Run(tx *genji.Tx) Result {
 		return Result{err: err}
 	}
 
-	matcher := And(q.matchers...)
-	tree, ok, err := matcher.MatchIndex(tx, q.tableSelector.Name())
-	if err != nil && err != engine.ErrIndexNotFound {
-		return Result{err: err}
-	}
-
 	var b table.Browser
 
-	if ok && err == nil {
-		b.Reader = &indexResultTable{
-			tree:  tree,
-			table: t,
+	if im, ok := q.whereExpr.(IndexMatcher); ok {
+		tree, ok, err := im.MatchIndex(tx, q.tableSelector.Name())
+		if err != nil && err != engine.ErrIndexNotFound {
+			return Result{err: err}
 		}
-	} else {
-		b = table.NewBrowser(t).
-			Filter(func(rowid []byte, r record.Record) (bool, error) {
-				return matcher.Match(r)
-			})
+
+		if ok && err == nil {
+			b.Reader = &indexResultTable{
+				tree:  tree,
+				table: t,
+			}
+		}
+	}
+
+	if b.Reader == nil {
+		b.Reader, err = whereClause(tx, t, q.whereExpr)
+		if err != nil {
+			return Result{err: err}
+		}
 	}
 
 	b = b.Map(func(rowid []byte, r record.Record) (record.Record, error) {
@@ -92,12 +74,45 @@ func (q Query) Run(tx *genji.Tx) Result {
 	return Result{t: b.Reader}
 }
 
-func (q Query) Where(matchers ...Matcher) Query {
-	q.matchers = append(q.matchers, matchers...)
+func (q Query) Where(e Expr) Query {
+	q.whereExpr = e
 	return q
 }
 
 func (q Query) From(selector TableSelector) Query {
 	q.tableSelector = selector
 	return q
+}
+
+func whereClause(tx *genji.Tx, t table.Reader, e Expr) (table.Reader, error) {
+	b := table.NewBrowser(t).Filter(func(_ []byte, r record.Record) (bool, error) {
+		sc, err := e.Eval(EvalContext{Tx: tx, Record: r})
+		if err != nil {
+			return false, err
+		}
+
+		return sc.Truthy(), nil
+	})
+	return b.Reader, b.Err()
+}
+
+type Result struct {
+	t   table.Reader
+	err error
+}
+
+func (q Result) Err() error {
+	return q.err
+}
+
+func (q Result) Scan(s table.Scanner) error {
+	if q.err != nil {
+		return q.err
+	}
+
+	return s.ScanTable(q.t)
+}
+
+func (q Result) Table() table.Reader {
+	return q.t
 }
