d56c8940b136dfb27bb052411304bb6ea1aeb058
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-06-29 21:07:08 +0200

    Add package doc

---
+++ query/doc.go
@@ -0,0 +1,13 @@
+// Package query provides a type safe SQL like API using Go types and functions.
+// This API can be run within a Genji transaction to query or alter the database.
+// The typical use is to run the provided DSLs for each of the most common SQL
+// commands.
+// Each DSL is thread safe and can be reused in or out of a transaction using its Run method.
+// 
+// Select:
+//   query.Select().From(...).Where(...).Run(tx)
+// Insert:
+//   Insert().Into(...).Fields(...).Values(...).Run(tx)
+// Delete:
+//   Delete().From(...).Where(...).Run(tx)
+package query
