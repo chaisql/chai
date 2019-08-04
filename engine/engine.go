cb6baaec6e1f8358268215b6d35a374348e492d1
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-08-04 12:39:01 +0200

    Improve engine doc

--- engine/engine.go
+++ engine/engine.go
@@ -1,3 +1,4 @@
+// Package engine defines interfaces to be implemented by engines in order to be compatible with Genji.
 package engine
 
 import (
@@ -6,7 +7,7 @@ import (
 
 // Common errors returned by the engine implementations.
 var (
-	// ErrTransactionReadOnly must be returned when attempting to call write methods on a read-only transaction.
+	// ErrTransactionReadOnly is returned when attempting to call write methods on a read-only transaction.
 	ErrTransactionReadOnly = errors.New("transaction is read-only")
 
 	// ErrStoreNotFound is returned when the targeted store doesn't exist.
@@ -25,23 +26,39 @@ var (
 // and data structures of their choice.
 // Engines must support read-only and read/write transactions.
 type Engine interface {
+	// Begin returns a read-only or read/write transaction depending on whether writable is set to false
+	// or true, respectively.
+	// The behaviour of opening a transaction when another one is already opened depends on the implementation.
 	Begin(writable bool) (Transaction, error)
+	// Close the engine after ensuring all the transactions have completed.
 	Close() error
 }
 
 // A Transaction provides methods for managing the collection of stores and the transaction itself.
 // The transaction is either read-only or read/write. Read-only transactions can be used to read stores
 // and read/write ones can be used to read, create, delete and modify stores.
+// If the transaction is read-only, any call to a write method must return the ErrTransactionReadOnly error.
 type Transaction interface {
+	// Rollback the transaction and cancel any change made during its lifetime.
+	// If the transaction was already commited or rolled back, no error is returned.
 	Rollback() error
+	// Commit the transaction and any change made during its lifetime.
+	// If the transaction was already rolled back or commited, an error is returned.
 	Commit() error
+	// Fetch a store by name. If the store doesn't exist, it returns the ErrStoreNotFound error.
 	Store(name string) (Store, error)
+	// Create a store with the given name. If the store already exists, it returns ErrStoreAlreadyExists.
 	CreateStore(name string) error
+	// Drop a store by name. If the store doesn't exist, it returns ErrStoreNotFound.
+	// It deletes all the values stored in it.
 	DropStore(name string) error
+	// Returns a list of store names lexicographically sorted.
+	// If there are no stores, an empty slice is returned.
 	ListStores(prefix string) ([]string, error)
 }
 
-// A Store manages key value pairs. It is an abstraction on top of data structures that can provide random readThe store can be implemented by any data stru
+// A Store manages key value pairs. It is an abstraction on top of any data structure that can provide
+// random read, random write, and ordered sequential read.
 type Store interface {
 	// Get returns a value associated with the given key. If no key is not found, it returns ErrKeyNotFound.
 	Get(k []byte) ([]byte, error)
