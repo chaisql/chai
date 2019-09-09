99886dea5fdaeba209a09c7da23406f113b32ac7
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-09-09 11:02:32 -0700

    Add Query, Statement and TxManager types to query package

--- query/query.go
+++ query/query.go
@@ -10,6 +10,46 @@ import (
 	"github.com/asdine/genji/table"
 )
 
+// A Query can execute actions against the database. It can read or write data
+// from any table, or even alter the structure of the database.
+// Results are returned as a stream.
+type Query interface {
+	Run(*genji.DB) Result
+}
+
+// A Statement represents a unique action that can be executed against the database.
+type Statement interface {
+	Run(*TxManager) (*table.Stream, error)
+}
+
+// TxManager is used by statements to automatically open transactions.
+// If the Tx field is nil, it will automatically create a new transaction.
+// If the Tx field is not nil, it will be passed to View and Update.
+type TxManager struct {
+	DB *genji.DB
+	Tx *genji.Tx
+}
+
+// View runs fn in a read-only transaction if the Tx field is nil.
+// If not, it will pass it to fn regardless of it being a read-only or read-write transaction.
+func (tx TxManager) View(fn func(tx *genji.Tx) error) error {
+	if tx.Tx != nil {
+		return fn(tx.Tx)
+	}
+
+	return tx.DB.View(fn)
+}
+
+// Update runs fn in a read-write transaction if the Tx field is nil.
+// If not, it will pass it to fn regardless of it being a read-only or read-write transaction.
+func (tx TxManager) Update(fn func(tx *genji.Tx) error) error {
+	if tx.Tx != nil {
+		return fn(tx.Tx)
+	}
+
+	return tx.DB.Update(fn)
+}
+
 // Result of a query.
 type Result struct {
 	*table.Stream
