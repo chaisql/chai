3a278ba25cd9623bef5f7c7c6a5a5b3843caf774
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-09-11 15:44:09 -0700

    Rework Expr API

--- query/expr.go
+++ query/expr.go
@@ -11,31 +11,11 @@ import (
 )
 
 var (
-	trueScalar    = Scalar{Type: value.Bool, Data: value.EncodeBool(true)}
-	falseScalar   = Scalar{Type: value.Bool, Data: value.EncodeBool(false)}
 	trueLitteral  = LitteralValue{Value: value.NewBool(true)}
 	falseLitteral = LitteralValue{Value: value.NewBool(true)}
 )
 
-// A Scalar represents a value of any type defined by the value package.
-type Scalar struct {
-	Type  value.Type
-	Data  []byte
-	Value interface{}
-}
-
-// Truthy returns true if the Data is different than the zero value of
-// the type of s.
-func (s Scalar) Truthy() bool {
-	return !value.IsZeroValue(s.Type, s.Data)
-}
-
-// Eval returns s. It implements the Expr interface.
-func (s Scalar) Eval(EvalContext) (Scalar, error) {
-	return s, nil
-}
-
-// An Expr evaluates to a scalar.
+// An Expr evaluates to a value.
 // It can be used as an argument to a WHERE clause or any other method that
 // expects an expression.
 // This package provides several ways of creating expressions.
@@ -53,7 +33,7 @@ func (s Scalar) Eval(EvalContext) (Scalar, error) {
 // Using Values:
 //    <T>Value() (i.e. StringValue(), Int32Value(), ...)
 type Expr interface {
-	Eval(EvalContext) (Scalar, error)
+	Eval(EvalContext) (Value, error)
 }
 
 // EvalContext contains information about the context in which
@@ -64,21 +44,17 @@ type EvalContext struct {
 	Record record.Record // can be nil
 }
 
-type EExpr interface {
-	Eval(EvalContext) (Value, error)
-}
-
 // A Value is the result of evaluating an expression.
 type Value interface {
 	Truthy() bool
-	String() string
 }
 
-// A ValueList is a value that contains other values.
-type ValueList interface {
-	Value
+// A ExprList is an expression that contains other expressions.
+type ExprList interface {
+	Expr
 
-	Iterate(func(Value) error) error
+	Iterate(func(Expr) error) error
+	Length() int
 }
 
 // A LitteralValue represents a litteral value of any type defined by the value package.
@@ -98,61 +74,62 @@ func (l LitteralValue) Eval(EvalContext) (Value, error) {
 	return l, nil
 }
 
-// LitteralValueList is a list of values.
-type LitteralValueList []Value
+// LitteralExprList is a list of expressions.
+type LitteralExprList []Expr
 
 // Truthy returns true if the length of l is greater than zero.
 // It implements the Value interface.
-func (l LitteralValueList) Truthy() bool {
+func (l LitteralExprList) Truthy() bool {
 	return len(l) > 0
 }
 
-// Eval returns l. It implements the Expr interface.
-func (l LitteralValueList) Eval(EvalContext) (Value, error) {
-	return l, nil
-}
-
-// String returns the string representation of l. It implements the Value interface.
-func (l LitteralValueList) String() string {
-	var builder strings.Builder
-
-	builder.WriteRune('(')
-	for i, v := range l {
-		if i > 0 {
-			builder.WriteString(", ")
-		}
-		builder.WriteString(v.String())
+// Eval returns the first element of l. It implements the Expr interface.
+func (l LitteralExprList) Eval(ctx EvalContext) (Value, error) {
+	if len(l) == 0 {
+		return LitteralValue{}, nil
 	}
-	builder.WriteRune(')')
-	return builder.String()
+
+	return l[0].Eval(ctx)
 }
 
-// FieldValue is a field that can be used as a Value.
-type FieldValue field.Field
+// Iterate over the list of exprs and calls fn for each one sequentially.
+func (l LitteralExprList) Iterate(fn func(Expr) error) error {
+	for _, e := range l {
+		err := fn(e)
+		if err != nil {
+			return err
+		}
+	}
 
-// String calls the String method of the field.
-func (f FieldValue) String() string {
-	return field.Field(f).String()
+	return nil
 }
+
+// Length of the list.
+func (l LitteralExprList) Length() int {
+	return len(l)
+}
+
+// FieldExpr is a field that can be used as a Value.
+type FieldExpr field.Field
 
 // Eval returns l. It implements the Expr interface.
-func (f FieldValue) Eval(EvalContext) (Value, error) {
+func (f FieldExpr) Eval(EvalContext) (Value, error) {
 	return f, nil
 }
 
 // Truthy returns true if the record is nil
 // It implements the Value interface.
-func (f FieldValue) Truthy() bool {
+func (f FieldExpr) Truthy() bool {
 	return !value.IsZeroValue(f.Type, f.Data)
 }
 
-// RecordValue is a record that can be used as a Value.
-type RecordValue struct {
+// RecordExpr is a record that can be used as a Value.
+type RecordExpr struct {
 	r record.Record
 }
 
-// String returns the string representation of r. It implements the Value interface.
-func (r RecordValue) String() string {
+// String returns the string representation of r.
+func (r RecordExpr) String() string {
 	var builder strings.Builder
 
 	builder.WriteRune('(')
@@ -171,30 +148,52 @@ func (r RecordValue) String() string {
 }
 
 // Eval returns r. It implements the Expr interface.
-func (r RecordValue) Eval(EvalContext) (Value, error) {
-	return r, nil
+func (r RecordExpr) Eval(EvalContext) (v Value, err error) {
+	err = r.r.Iterate(func(f field.Field) error {
+		v = FieldExpr(f)
+		return errStop
+	})
+	if err == errStop {
+		err = nil
+	}
+
+	if v == nil {
+		v = FieldExpr{}
+	}
+
+	return
 }
 
 // Truthy returns true if the record is not nil.
 // It implements the Value interface.
-func (r RecordValue) Truthy() bool {
+func (r RecordExpr) Truthy() bool {
 	return r.r != nil
 }
 
 // Iterate over the list of fields and calls fn for each one sequentially.
-func (r RecordValue) Iterate(fn func(Value) error) error {
+func (r RecordExpr) Iterate(fn func(Expr) error) error {
 	return r.r.Iterate(func(f field.Field) error {
-		return fn(FieldValue(f))
+		return fn(FieldExpr(f))
 	})
 }
 
-// TableValue is a table.Reader that can be used as a Value.
-type TableValue struct {
+// Length of the record.
+func (r RecordExpr) Length() int {
+	var i int
+	r.r.Iterate(func(field.Field) error {
+		i++
+		return nil
+	})
+	return i
+}
+
+// TableExpr is a table.Reader that can be used as an Expr.
+type TableExpr struct {
 	r table.Reader
 }
 
-// String returns the string representation of t. It implements the Value interface.
-func (t TableValue) String() string {
+// String returns the string representation of t.
+func (t TableExpr) String() string {
 	var builder strings.Builder
 
 	builder.WriteRune('(')
@@ -204,7 +203,7 @@ func (t TableValue) String() string {
 		if i > 0 {
 			builder.WriteString(", ")
 		}
-		builder.WriteString(RecordValue{r}.String())
+		builder.WriteString(RecordExpr{r}.String())
 		return nil
 	})
 
@@ -213,19 +212,65 @@ func (t TableValue) String() string {
 }
 
 // Eval returns t. It implements the Expr interface.
-func (t TableValue) Eval(EvalContext) (Value, error) {
-	return t, nil
+func (t TableExpr) Eval(EvalContext) (v Value, err error) {
+	err = t.r.Iterate(func(_ []byte, r record.Record) error {
+		v = RecordExpr{r}
+		return errStop
+	})
+	if err == errStop {
+		err = nil
+	}
+
+	if v == nil {
+		v = RecordExpr{}
+	}
+
+	return
 }
 
 // Truthy returns true if the table is not nil.
 // It implements the Value interface.
-func (t TableValue) Truthy() bool {
+func (t TableExpr) Truthy() bool {
 	return t.r != nil
 }
 
 // Iterate over the list of records and calls fn for each one sequentially.
-func (t TableValue) Iterate(fn func(Value) error) error {
+func (t TableExpr) Iterate(fn func(Expr) error) error {
 	return t.r.Iterate(func(_ []byte, r record.Record) error {
-		return fn(RecordValue{r})
+		return fn(RecordExpr{r})
 	})
+}
+
+// Length of the table.
+func (t TableExpr) Length() int {
+	i, _ := table.NewStream(t.r).Count()
+	return i
+}
+
+// ValueFromExprList evaluates el recursively until it returns a single value.
+func ValueFromExprList(ctx EvalContext, el ExprList) (Value, error) {
+	var val Value
+
+	err := el.Iterate(func(e Expr) error {
+		v, err := e.Eval(ctx)
+		if err != nil {
+			return err
+		}
+
+		if list, ok := v.(ExprList); ok {
+			val, err = ValueFromExprList(ctx, list)
+			if err != nil {
+				return err
+			}
+
+			return errStop
+		}
+
+		val = v
+		return errStop
+	})
+	if err == errStop {
+		return val, nil
+	}
+	return val, err
 }
