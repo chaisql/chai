fac2695d0cb4e87e6eeab890d1fa6d9dd559b780
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-10-05 21:34:05 +0200

    Add CreateIndex statement

--- query/create.go
+++ query/create.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 
 	"github.com/asdine/genji/database"
+	"github.com/asdine/genji/index"
 )
 
 // CreateTableStmt is a DSL that allows creating a full CREATE TABLE statement.
@@ -51,6 +52,80 @@ func (stmt CreateTableStmt) exec(tx *database.Tx, _ []driver.NamedValue) Result 
 		_, err = tx.CreateTableIfNotExists(stmt.tableName)
 	} else {
 		_, err = tx.CreateTable(stmt.tableName)
+	}
+
+	return Result{err: err}
+}
+
+// CreateIndexStmt is a DSL that allows creating a full CREATE INDEX statement.
+// It is typically created using the CreateIndex function.
+type CreateIndexStmt struct {
+	idxName     string
+	tableName   string
+	fieldName   string
+	ifNotExists bool
+	unique      bool
+}
+
+// CreateIndex creates a DSL equivalent to the SQL CREATE INDEX command.
+func CreateIndex(idxName, tableName, fieldName string) CreateIndexStmt {
+	return CreateIndexStmt{
+		idxName:   idxName,
+		tableName: tableName,
+		fieldName: fieldName,
+	}
+}
+
+// IsReadOnly always returns false. It implements the Statement interface.
+func (stmt CreateIndexStmt) IsReadOnly() bool {
+	return false
+}
+
+// Unique configures the statement to create a unique index.
+func (stmt CreateIndexStmt) Unique() CreateIndexStmt {
+	stmt.unique = true
+	return stmt
+}
+
+// Run runs the Create table statement in the given transaction.
+// It implements the Statement interface.
+func (stmt CreateIndexStmt) Run(tx *database.Tx, args []driver.NamedValue) Result {
+	return stmt.exec(tx, args)
+}
+
+// Exec the CreateTable statement within tx.
+func (stmt CreateIndexStmt) Exec(tx *database.Tx, args ...interface{}) Result {
+	return stmt.exec(tx, argsToNamedValues(args))
+}
+
+// IfNotExists sets the ifNotExists flag to true.
+func (stmt CreateIndexStmt) IfNotExists() CreateIndexStmt {
+	stmt.ifNotExists = true
+	return stmt
+}
+
+func (stmt CreateIndexStmt) exec(tx *database.Tx, _ []driver.NamedValue) Result {
+	if stmt.tableName == "" {
+		return Result{err: errors.New("missing table name")}
+	}
+
+	if stmt.idxName == "" {
+		return Result{err: errors.New("missing index name")}
+	}
+
+	if stmt.fieldName == "" {
+		return Result{err: errors.New("missing field name")}
+	}
+
+	t, err := tx.GetTable(stmt.tableName)
+	if err != nil {
+		return Result{err: err}
+	}
+
+	if stmt.ifNotExists {
+		_, err = t.CreateIndexIfNotExists(stmt.idxName, stmt.fieldName, index.Options{Unique: stmt.unique})
+	} else {
+		_, err = t.CreateIndex(stmt.idxName, stmt.fieldName, index.Options{Unique: stmt.unique})
 	}
 
 	return Result{err: err}
--- query/create_test.go
+++ query/create_test.go
@@ -26,3 +26,20 @@ func TestCreateTableStatement(t *testing.T) {
 	res = query.CreateTable("bar").IfNotExists().Exec(tx)
 	require.NoError(t, res.Err())
 }
+
+func TestCreateIndexStatement(t *testing.T) {
+	tx, cleanup := createTable(t, 0, false)
+	defer cleanup()
+
+	res := query.CreateIndex("foo", "test", "a").Exec(tx)
+	require.NoError(t, res.Err())
+
+	res = query.CreateIndex("foo", "test", "a").Exec(tx)
+	require.Error(t, res.Err())
+
+	res = query.CreateIndex("foo", "test", "a").IfNotExists().Exec(tx)
+	require.NoError(t, res.Err())
+
+	res = query.CreateIndex("bar", "test", "a").IfNotExists().Exec(tx)
+	require.NoError(t, res.Err())
+}
