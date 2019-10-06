5c6fd543efaaf73cd2238f2b48b5f6181d75ae27
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-10-06 11:27:25 +0200

    Add DROP TABLE statement

---
+++ query/drop.go
@@ -0,0 +1,56 @@
+package query
+
+import (
+	"database/sql/driver"
+	"errors"
+
+	"github.com/asdine/genji/database"
+)
+
+// DropTableStmt is a DSL that allows creating a DROP TABLE query.
+type DropTableStmt struct {
+	tableName string
+	ifExists  bool
+}
+
+// DropTable creates a DSL equivalent to the SQL DROP TABLE command.
+func DropTable(tableName string) DropTableStmt {
+	return DropTableStmt{
+		tableName: tableName,
+	}
+}
+
+// IsReadOnly always returns false. It implements the Statement interface.
+func (stmt DropTableStmt) IsReadOnly() bool {
+	return false
+}
+
+// Run runs the DropTable statement in the given transaction.
+// It implements the Statement interface.
+func (stmt DropTableStmt) Run(tx *database.Tx, args []driver.NamedValue) Result {
+	return stmt.exec(tx, args)
+}
+
+// Exec the DropTable statement within tx.
+func (stmt DropTableStmt) Exec(tx *database.Tx, args ...interface{}) Result {
+	return stmt.exec(tx, argsToNamedValues(args))
+}
+
+// IfExists sets the ifExists flag to true.
+func (stmt DropTableStmt) IfExists() DropTableStmt {
+	stmt.ifExists = true
+	return stmt
+}
+
+func (stmt DropTableStmt) exec(tx *database.Tx, args []driver.NamedValue) Result {
+	if stmt.tableName == "" {
+		return Result{err: errors.New("missing table name")}
+	}
+
+	err := tx.DropTable(stmt.tableName)
+	if err == database.ErrTableNotFound && stmt.ifExists {
+		return Result{}
+	}
+
+	return Result{err: err}
+}
