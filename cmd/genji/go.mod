2052875a23bc6596d5a16e2bd78887f67079b53c
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-10-23 22:10:13 +0200

    Create a module for the genji command

---
+++ cmd/genji/go.mod
@@ -0,0 +1,12 @@
+module github.com/asdine/genji/cmd/genji
+
+go 1.13
+
+require (
+	github.com/asdine/genji v0.2.1
+	github.com/pkg/errors v0.8.1
+	github.com/urfave/cli v1.22.1
+	golang.org/x/tools v0.0.0-20191025023517-2077df36852e
+)
+
+replace github.com/asdine/genji v0.2.1 => ../../
--- cmd/genji/main.go
+++ cmd/genji/main.go
@@ -1,47 +1,58 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
-	"io"
-	"io/ioutil"
+	"log"
 	"os"
-	"path/filepath"
-	"strings"
 
-	"github.com/asdine/genji/cmd/genji/generator"
-	"github.com/pkg/errors"
+	"github.com/urfave/cli"
 )
 
-type stringFlags []string
-
-func (i *stringFlags) String() string {
-	return "list of strings"
-}
-
-func (i *stringFlags) Set(value string) error {
-	*i = append(*i, value)
-	return nil
-}
-
 func main() {
-	var files, structs stringFlags
-	var output string
+	app := cli.NewApp()
+	app.Name = "genji"
+	app.EnableBashCompletion = true
+	app.Commands = []cli.Command{
+		{
+			Name:    "generate",
+			Aliases: []string{"gen"},
+			Usage:   "scan a structure and generate methods implementing various Genji interfaces",
+			Flags: []cli.Flag{
+				cli.StringSliceFlag{
+					Name:     "f",
+					Required: true,
+					Usage:    "paths of the files to parse",
+				},
+				cli.StringSliceFlag{
+					Name:     "s",
+					Required: true,
+					Usage:    "names of the source structures",
+				},
+				cli.StringFlag{
+					Name:  "output, o",
+					Usage: "name of the generated file",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				files := c.StringSlice("f")
+				structs := c.StringSlice("s")
+				if len(files) == 0 {
+					return cli.NewExitError("missing files", 2)
+				}
 
-	flag.Var(&files, "f", "path of the files to parse")
-	flag.Var(&structs, "s", "name of the source structure")
-	flag.StringVar(&output, "o", "", "name of the generated file, optional")
+				if len(structs) == 0 {
+					return cli.NewExitError("missing structs", 2)
+				}
 
-	flag.Parse()
-
-	if len(files) == 0 || len(structs) == 0 {
-		exitRecordUsage()
+				return generate(c.StringSlice("f"), c.StringSlice("s"), c.String("o"))
+			},
+		},
 	}
 
-	err := generate(files, structs, output)
+	err := app.Run(os.Args)
 	if err != nil {
-		fail("%v\n", err)
+		log.Fatal(err)
 	}
 }
 
@@ -53,62 +64,4 @@ func fail(format string, a ...interface{}) {
 func exitRecordUsage() {
 	flag.Usage()
 	os.Exit(2)
-}
-
-func generate(files []string, structs []string, output string) error {
-	if !areGoFiles(files) {
-		return errors.New("input files must be Go files")
-	}
-
-	sources := make([]io.Reader, len(files))
-
-	for i, fname := range files {
-		f, err := os.Open(fname)
-		if err != nil {
-			return errors.Wrap(err, "failed to read file")
-		}
-
-		sources[i] = f
-	}
-
-	list := make([]generator.Struct, len(structs))
-	for i, name := range structs {
-		list[i].Name = name
-	}
-
-	var buf bytes.Buffer
-	err := generator.Generate(&buf, generator.Config{
-		Sources: sources,
-		Structs: list,
-	})
-	if err != nil {
-		return err
-	}
-
-	if output == "" {
-		suffix := filepath.Ext(files[0])
-		base := strings.TrimSuffix(files[0], suffix)
-		if strings.HasSuffix(base, "_test") {
-			base = strings.TrimSuffix(base, "_test")
-			suffix = "_test" + suffix
-		}
-		output = base + ".genji" + suffix
-	}
-
-	err = ioutil.WriteFile(output, buf.Bytes(), 0644)
-	if err != nil {
-		return errors.Wrapf(err, "failed to generate file at location %s", output)
-	}
-
-	return nil
-}
-
-func areGoFiles(files []string) bool {
-	for _, f := range files {
-		if filepath.Ext(f) != ".go" {
-			return false
-		}
-	}
-
-	return true
 }
