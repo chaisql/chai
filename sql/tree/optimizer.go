f69b8d7961391d7afe6d5889cfd76b5f7b1533fd
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2020-06-12 21:58:38 +0400

    Add optimizer rules

---
+++ sql/tree/optimizer.go
@@ -0,0 +1,219 @@
+package tree
+
+import (
+	"github.com/genjidb/genji/document"
+	"github.com/genjidb/genji/sql/query/expr"
+)
+
+var optimizerRules = []func(t *Tree) (*Tree, error){
+	splitANDConditionRule,
+	precalculateExprRule,
+	removeUnnecessarySelectionNodesRule,
+}
+
+func optimizer(t *Tree) (*Tree, error) {
+	var err error
+
+	for _, rule := range optimizerRules {
+		t, err = rule(t)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return t, nil
+}
+
+// splitANDConditionRule splits any selection node whose condition
+// is one or more AND operators into one or more selection nodes.
+func splitANDConditionRule(t *Tree) (*Tree, error) {
+	n := t.Root
+	var prev Node
+
+	for n != nil {
+		if n.Operation() == Selection {
+			cond := n.(*selectionNode).cond
+			if cond != nil {
+				if op, ok := cond.(expr.Operator); ok && expr.IsAndOperator(op) {
+					exprs := splitANDCondition(cond)
+
+					cur := n.Left()
+					i := len(exprs) - 1
+					var newNode Node
+					for i >= 0 {
+						newNode = NewSelectionNode(cur, exprs[i])
+						cur = newNode
+
+						i--
+					}
+
+					if prev != nil {
+						prev.SetLeft(newNode)
+					} else {
+						t.Root = newNode
+					}
+				}
+			}
+		}
+
+		prev = n
+		n = n.Left()
+	}
+
+	return t, nil
+}
+
+func splitANDCondition(cond expr.Expr) (exprs []expr.Expr) {
+	op, ok := cond.(expr.Operator)
+	if ok && expr.IsAndOperator(op) {
+		exprs = append(exprs, splitANDCondition(op.LeftHand())...)
+		exprs = append(exprs, splitANDCondition(op.RightHand())...)
+		return
+	}
+
+	exprs = append(exprs, cond)
+	return
+}
+
+// precalculateExprRule evaluates any constant sub-expression that can be evaluated
+// before running the query and replaces it by the result of the evaluation.
+// The result of constant sub-expressions, like "3 + 4", is always the same and thus
+// can be precalculated.
+// Examples:
+//   3 + 4 --> 7
+//   3 + 1 > 10 - a --> 4 > 10 - a
+func precalculateExprRule(t *Tree) (*Tree, error) {
+	n := t.Root
+
+	for n != nil {
+		if n.Operation() == Selection {
+			sn := n.(*selectionNode)
+			sn.cond = precalculateExpr(sn.cond)
+		}
+
+		n = n.Left()
+	}
+
+	return t, nil
+}
+
+// precalculateExpr is a recursive function that tries to precalculate
+// expression nodes when possible.
+// it returns a new expression with simplified nodes.
+// if no simplification is possible it returns the same expression.
+func precalculateExpr(e expr.Expr) expr.Expr {
+	switch t := e.(type) {
+	case expr.LiteralExprList:
+		// we assume that the list of expressions contains only literals
+		// until proven wrong.
+		literalsOnly := true
+		for i, te := range t {
+			newExpr := precalculateExpr(te)
+			if _, ok := newExpr.(expr.LiteralValue); !ok {
+				literalsOnly = false
+			}
+			t[i] = newExpr
+		}
+
+		// if literalsOnly is still true, it means we have a list of constant expressions
+		// (ex: [1, 4, true]). We can transform that into a document.Array.
+		if literalsOnly {
+			vb := make(document.ValueBuffer, len(t))
+			for i := range t {
+				vb[i] = document.Value(t[i].(expr.LiteralValue))
+			}
+
+			return expr.ArrayValue(&vb)
+		}
+
+	case expr.KVPairs:
+		// we assume that the list of kvpairs contains only literals
+		// until proven wrong.
+		literalsOnly := true
+
+		for i, kv := range t {
+			kv.V = precalculateExpr(kv.V)
+			if _, ok := kv.V.(expr.LiteralValue); !ok {
+				literalsOnly = false
+			}
+			t[i] = kv
+		}
+
+		// if literalsOnly is still true, it means we have a list of kvpairs
+		// that only contain constant values (ex: {"a": 1, "b": true}.
+		// We can transform that into a document.Document.
+		if literalsOnly {
+			var fb document.FieldBuffer
+			for i := range t {
+				fb.Add(t[i].K, document.Value(t[i].V.(expr.LiteralValue)))
+			}
+
+			return expr.LiteralValue(document.NewDocumentValue(&fb))
+		}
+	case expr.Operator:
+		// since expr.Operator is an interface,
+		// this optimization must only be applied to
+		// a few selected operators that we know about.
+		if !expr.IsAndOperator(t) &&
+			!expr.IsOrOperator(t) &&
+			!expr.IsArithmeticOperator(t) &&
+			!expr.IsComparisonOperator(t) {
+			return e
+		}
+
+		lh := precalculateExpr(t.LeftHand())
+		rh := precalculateExpr(t.RightHand())
+		t.SetLeftHandExpr(lh)
+		t.SetRightHandExpr(rh)
+
+		_, leftIsLit := lh.(expr.LiteralValue)
+		_, rightIsLit := rh.(expr.LiteralValue)
+		// if both operands are literals, we can precalculate them now
+		if leftIsLit && rightIsLit {
+			v, err := t.Eval(expr.EvalStack{})
+			// any error encountered here is unexpected
+			if err != nil {
+				panic(err)
+			}
+			// we replace this expression with the result of its evaluation
+			return expr.LiteralValue(v)
+		}
+	}
+
+	return e
+}
+
+// removeUnnecessarySelectionNodesRule removes any selection node whose
+// condition is a constant expression that evaluates to a truthy value.
+// if it evaluates to a falsy value, it considers that the tree
+// will not stream any document, so it returns an empty tree.
+func removeUnnecessarySelectionNodesRule(t *Tree) (*Tree, error) {
+	n := t.Root
+	var prev Node
+
+	for n != nil {
+		if n.Operation() == Selection {
+			sn := n.(*selectionNode)
+			if sn.cond != nil {
+				if lit, ok := sn.cond.(expr.LiteralValue); ok {
+					// if the expr is falsy, we return an empty tree
+					if !document.Value(lit).IsTruthy() {
+						return &Tree{}, nil
+					}
+					// if the expr is truthy, we remove the node from the tree
+					if prev != nil {
+						prev.SetLeft(n.Left())
+					} else {
+						t.Root = n.Left()
+					}
+				}
+
+			}
+		}
+
+		prev = n
+		n = n.Left()
+	}
+
+	return t, nil
+}
---
+++ sql/tree/optimizer_test.go
@@ -0,0 +1,204 @@
+package tree
+
+import (
+	"testing"
+
+	"github.com/genjidb/genji/document"
+	"github.com/genjidb/genji/sql/query/expr"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSplitANDConditionRule(t *testing.T) {
+	tests := []struct {
+		name           string
+		root, expected Node
+	}{
+		{
+			"no and",
+			NewSelectionNode(NewTableInputNode("foo"), expr.BoolValue(true)),
+			NewSelectionNode(NewTableInputNode("foo"), expr.BoolValue(true)),
+		},
+		{
+			"and / top-level selection node",
+			NewSelectionNode(NewTableInputNode("foo"),
+				expr.And(
+					expr.BoolValue(true),
+					expr.BoolValue(false),
+				),
+			),
+			NewSelectionNode(
+				NewSelectionNode(
+					NewTableInputNode("foo"),
+					expr.BoolValue(false)),
+				expr.BoolValue(true)),
+		},
+		{
+			"and / middle-level selection node",
+			NewLimitNode(
+				NewSelectionNode(NewTableInputNode("foo"),
+					expr.And(
+						expr.BoolValue(true),
+						expr.BoolValue(false),
+					),
+				), expr.BoolValue(true)),
+			NewLimitNode(
+				NewSelectionNode(
+					NewSelectionNode(
+						NewTableInputNode("foo"),
+						expr.BoolValue(false)),
+					expr.BoolValue(true),
+				), expr.BoolValue(true)),
+		},
+		{
+			"multi and",
+			NewLimitNode(
+				NewSelectionNode(NewTableInputNode("foo"),
+					expr.And(
+						expr.And(
+							expr.IntValue(1),
+							expr.IntValue(2),
+						),
+						expr.And(
+							expr.IntValue(3),
+							expr.IntValue(4),
+						),
+					),
+				), expr.IntValue(10)),
+			NewLimitNode(
+				NewSelectionNode(
+					NewSelectionNode(
+						NewSelectionNode(
+							NewSelectionNode(
+								NewTableInputNode("foo"),
+								expr.IntValue(4)),
+							expr.IntValue(3)),
+						expr.IntValue(2)),
+					expr.IntValue(1)),
+				expr.IntValue(10),
+			),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			res, err := splitANDConditionRule(New(test.root))
+			require.NoError(t, err)
+			require.True(t, res.Root.Equal(test.expected))
+		})
+	}
+}
+
+func TestPrecalculateExprRule(t *testing.T) {
+	tests := []struct {
+		name        string
+		e, expected expr.Expr
+	}{
+		{
+			"constant expr: 3 -> 3",
+			expr.IntValue(3),
+			expr.IntValue(3),
+		},
+		{
+			"operator with two constant operands: 3 + true -> 4",
+			expr.Add(expr.IntValue(3), expr.BoolValue(true)),
+			expr.IntValue(4),
+		},
+		{
+			"operator with constant nested operands: 3 > true - 40 -> true",
+			expr.Gt(expr.IntValue(3), expr.Sub(expr.BoolValue(true), expr.Float64Value(40))),
+			expr.BoolValue(true),
+		},
+		{
+			"constant sub-expr: a > true - 40 -> a > -39",
+			expr.Gt(expr.FieldSelector{"a"}, expr.Sub(expr.BoolValue(true), expr.Float64Value(40))),
+			expr.Gt(expr.FieldSelector{"a"}, expr.Float64Value(-39)),
+		},
+		{
+			"non-constant expr list: [a, true - 40] -> [a, -39]",
+			expr.LiteralExprList{
+				expr.FieldSelector([]string{"a"}),
+				expr.Sub(expr.BoolValue(true), expr.Float64Value(40)),
+			},
+			expr.LiteralExprList{
+				expr.FieldSelector([]string{"a"}),
+				expr.Float64Value(-39),
+			},
+		},
+		{
+			"constant expr list: [3, true - 40] -> array([3, 40])",
+			expr.LiteralExprList{
+				expr.IntValue(3),
+				expr.Sub(expr.BoolValue(true), expr.Float64Value(40)),
+			},
+			expr.LiteralValue(document.NewArrayValue(document.NewValueBuffer().
+				Append(document.NewIntValue(3)).
+				Append(document.NewFloat64Value(-39)))),
+		},
+		{
+			`non-constant kvpair: {"a": d, "b": 1 - 40} -> {"a": 3, "b": -39}`,
+			expr.KVPairs{
+				{K: "a", V: expr.FieldSelector{"d"}},
+				{K: "b", V: expr.Sub(expr.BoolValue(true), expr.Float64Value(40))},
+			},
+			expr.KVPairs{
+				{K: "a", V: expr.FieldSelector{"d"}},
+				{K: "b", V: expr.Float64Value(-39)},
+			},
+		},
+		{
+			`constant kvpair: {"a": 3, "b": 1 - 40} -> document({"a": 3, "b": -39})`,
+			expr.KVPairs{
+				{K: "a", V: expr.IntValue(3)},
+				{K: "b", V: expr.Sub(expr.BoolValue(true), expr.Float64Value(40))},
+			},
+			expr.LiteralValue(document.NewDocumentValue(document.NewFieldBuffer().
+				Add("a", document.NewIntValue(3)).
+				Add("b", document.NewFloat64Value(-39)),
+			)),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			res, err := precalculateExprRule(New(NewSelectionNode(NewTableInputNode("foo"), test.e)))
+			require.NoError(t, err)
+			require.True(t, res.Root.Equal(NewSelectionNode(NewTableInputNode("foo"), test.expected)))
+		})
+	}
+}
+
+func TestRemoveUnnecessarySelectionNodesRule(t *testing.T) {
+	tests := []struct {
+		name           string
+		root, expected Node
+	}{
+		{
+			"non-constant expr",
+			NewSelectionNode(NewTableInputNode("foo"), expr.FieldSelector{"a"}),
+			NewSelectionNode(NewTableInputNode("foo"), expr.FieldSelector{"a"}),
+		},
+		{
+			"truthy constant expr",
+			NewSelectionNode(NewTableInputNode("foo"), expr.IntValue(10)),
+			NewTableInputNode("foo"),
+		},
+		{
+			"falsy constant expr",
+			NewSelectionNode(NewTableInputNode("foo"), expr.IntValue(0)),
+			nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			res, err := removeUnnecessarySelectionNodesRule(New(test.root))
+			require.NoError(t, err)
+			if test.expected != nil {
+				require.True(t, test.expected.Equal(res.Root))
+			} else {
+				require.Equal(t, test.expected, res.Root)
+			}
+
+		})
+	}
+}
