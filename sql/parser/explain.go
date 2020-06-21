917688fceabbc3414564db6f506e23da19d22868
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2020-06-21 14:51:33 +0400

    Parse explain statement

---
+++ sql/parser/explain.go
@@ -0,0 +1,25 @@
+package parser
+
+import (
+	"github.com/genjidb/genji/sql/planner"
+	"github.com/genjidb/genji/sql/query"
+	"github.com/genjidb/genji/sql/scanner"
+)
+
+// parseDropStatement parses a drop string and returns a Statement AST object.
+// This function assumes the DROP token has already been consumed.
+func (p *Parser) parseExplainStatement() (query.Statement, error) {
+	// ensure we don't have multiple EXPLAIN keywords
+	tok, pos, lit := p.ScanIgnoreWhitespace()
+	if tok == scanner.EXPLAIN {
+		return nil, newParseError(scanner.Tokstr(tok, lit), []string{"TABLE", "INDEX"}, pos)
+	}
+	p.Unscan()
+
+	innerStmt, err := p.ParseStatement()
+	if err != nil {
+		return nil, err
+	}
+
+	return &planner.ExplainStmt{Statement: innerStmt}, nil
+}
---
+++ sql/parser/explain_test.go
@@ -0,0 +1,34 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/genjidb/genji/sql/planner"
+	"github.com/genjidb/genji/sql/query"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParserExplain(t *testing.T) {
+	tests := []struct {
+		name     string
+		s        string
+		expected query.Statement
+		errored  bool
+	}{
+		{"Explain create table", "EXPLAIN CREATE TABLE test", &planner.ExplainStmt{Statement: query.CreateTableStmt{TableName: "test"}}, false},
+		{"Multiple Explains", "EXPLAIN EXPLAIN CREATE TABLE test", nil, true},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			q, err := ParseQuery(test.s)
+			if test.errored {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.Len(t, q.Statements, 1)
+			require.EqualValues(t, test.expected, q.Statements[0])
+		})
+	}
+}
