ab9d1968aa70a00fbb0132628a03ded95dbefd5f
tzzed <zerouali.t@gmail.com>
2020-07-13 21:36:57 +0200

    revert to master

--- document/document.go
+++ document/document.go
@@ -9,11 +9,7 @@ import (
 
 // ErrFieldNotFound must be returned by Document implementations, when calling the GetByField method and
 // the field wasn't found in the document.
-//ErrIndexOutOfBound
-var (
-	ErrFieldNotFound   = errors.New("field not found")
-	ErrNotDocument     = errors.New("type must be  a document")
-)
+var ErrFieldNotFound = errors.New("field not found")
 
 // A Document represents a group of key value pairs.
 type Document interface {
@@ -31,15 +27,24 @@ type Keyer interface {
 	Key() []byte
 }
 
+// Length returns the length of a document.
+func Length(d Document) (int, error) {
+	if fb, ok := d.(*FieldBuffer); ok {
+		return fb.Len(), nil
+	}
+
+	var len int
+	err := d.Iterate(func(_ string, _ Value) error {
+		len++
+		return nil
+	})
+	return len, err
+}
+
 // FieldBuffer stores a group of fields in memory. It implements the Document interface.
 type FieldBuffer struct {
 	fields []fieldValue
-}
-
-//fieldValue  Document structure field and value pairs.
-type fieldValue struct {
-	Field string
-	Value Value
+	key    []byte
 }
 
 // NewFieldBuffer creates a FieldBuffer.
@@ -47,17 +52,10 @@ func NewFieldBuffer() *FieldBuffer {
 	return new(FieldBuffer)
 }
 
-// NewFieldBufferByCopy creates a FieldBuffer from a DocumentValue.
-func NewFieldBufferByCopy(v Value) (FieldBuffer, error) {
-	var buf FieldBuffer
-	if v.Type != DocumentValue {
-		return buf, ErrNotDocument
-	}
-
-	err := buf.Copy(v.V.(Document))
-	return buf, err
+type fieldValue struct {
+	Field string
+	Value Value
 }
-
 
 // Add a field to the buffer.
 func (fb *FieldBuffer) Add(field string, v Value) *FieldBuffer {
@@ -67,6 +65,10 @@ func (fb *FieldBuffer) Add(field string, v Value) *FieldBuffer {
 
 // ScanDocument copies all the fields of d to the buffer.
 func (fb *FieldBuffer) ScanDocument(d Document) error {
+	if k, ok := d.(Keyer); ok {
+		fb.key = k.Key()
+	}
+
 	return d.Iterate(func(f string, v Value) error {
 		fb.Add(f, v)
 		return nil
@@ -75,7 +77,6 @@ func (fb *FieldBuffer) ScanDocument(d Document) error {
 
 // GetByField returns a value by field. Returns an error if the field doesn't exists.
 func (fb FieldBuffer) GetByField(field string) (Value, error) {
-
 	for _, fv := range fb.fields {
 		if fv.Field == field {
 			return fv.Value, nil
@@ -85,101 +86,16 @@ func (fb FieldBuffer) GetByField(field string) (Value, error) {
 	return Value{}, ErrFieldNotFound
 }
 
-// SetUniqueFieldOfDocument Add/Replace a value if the request is  field.
-func (fb *FieldBuffer) SetUniqueFieldOfDocument(field string, reqValue Value) error {
-	_, err := fb.GetByField(field)
-	switch err {
-	case ErrFieldNotFound:
-		fb.Add(field, reqValue)
-		return nil
-	case nil:
-		_ = fb.Replace(field, reqValue)
-		return nil
-	}
-
-	return err
-}
-
-// SetValueFromValuePath make a deep replacement or creation of a field document or index of an array.
-func (fb *FieldBuffer) SetValueFromValuePath(v Value, path ValuePath, reqValue Value) (Value, error) {
-
-	switch v.Type {
-	case DocumentValue:
-		buf, err := NewFieldBufferByCopy(v)
-		if err != nil {
-			return v, err
-		}
-
-		if len(path) == 1 {
-			buf.SetUniqueFieldOfDocument(path[0], reqValue)
-			return NewDocumentValue(buf), nil
-		}
-
-		va, err := buf.GetByField(path[0])
-		if err != nil {
-			return v, err
-		}
-
-		va, err = buf.SetValueFromValuePath(va, path[1:], reqValue)
-		if err != nil {
-			return v, err
-		}
-
-		buf.SetUniqueFieldOfDocument(path[0], va)
-		return NewDocumentValue(buf), nil
-	case ArrayValue:
-		buf, err := NewValueBufferByCopy(v)
-		if err != nil {
-			return v, err
-		}
-
-		va, index, err := buf.GetByIndexWithString(path[0])
-		if err != nil {
-			return v, err
-		}
-
-		if len(path) == 1 {
-			_ = buf.Replace(index, reqValue)
-			return NewArrayValue(buf), nil
-		}
-
-		va, err = fb.SetValueFromValuePath(va, path[1:], reqValue)
-		_ = buf.Replace(index, va)
-		return NewArrayValue(buf), nil
-	}
-
-	return v, nil
-}
-
 // Set replaces a field if it already exists or creates one if not.
-func (fb *FieldBuffer) Set(path ValuePath, reqValue Value) error {
-
-	// check if the ValuePath contains only one field to set
-	if len(path) == 1 {
-		//Set or replace the unique field
-		return fb.SetUniqueFieldOfDocument(path[0], reqValue)
-	}
-
-	for i, field := range fb.fields {
-		if path[0] == field.Field {
-			var buf FieldBuffer
-			err := buf.Copy(fb)
-			if err != nil {
-				return err
-			}
-			v, err := buf.SetValueFromValuePath(field.Value, path[1:], reqValue)
-			if err != nil {
-				return err
-			}
-
+func (fb *FieldBuffer) Set(f string, v Value) {
+	for i := range fb.fields {
+		if fb.fields[i].Field == f {
 			fb.fields[i].Value = v
-			return nil
+			return
 		}
 	}
 
-
-	//return Err if the request is like foo.1.2.etc where foo doesn't exist
-	return ErrFieldNotFound
+	fb.Add(f, v)
 }
 
 // Iterate goes through all the fields of the document and calls the given function by passing each one of them.
@@ -209,7 +125,6 @@ func (fb *FieldBuffer) Delete(field string) error {
 
 // Replace the value of the field by v.
 func (fb *FieldBuffer) Replace(field string, v Value) error {
-
 	for i := range fb.fields {
 		if fb.fields[i].Field == field {
 			fb.fields[i].Value = v
@@ -227,6 +142,7 @@ func (fb *FieldBuffer) Copy(d Document) error {
 	if err != nil {
 		return err
 	}
+
 	for i, f := range fb.fields {
 		switch f.Value.Type {
 		case DocumentValue:
@@ -243,6 +159,7 @@ func (fb *FieldBuffer) Copy(d Document) error {
 			if err != nil {
 				return err
 			}
+
 			fb.fields[i].Value = NewArrayValue(&buf)
 		}
 	}
@@ -260,8 +177,12 @@ func (fb *FieldBuffer) Reset() {
 	fb.fields = fb.fields[:0]
 }
 
+// Key of the document if any.
+func (fb *FieldBuffer) Key() []byte {
+	return fb.key
+}
+
 // A ValuePath represents the path to a particular value within a document.
-// is used for dot notation
 type ValuePath []string
 
 // NewValuePath takes a string representation of a value path and returns a ValuePath.
@@ -319,6 +240,9 @@ func (p ValuePath) getValueFromValue(v Value) (Value, error) {
 
 	switch v.Type {
 	case DocumentValue:
+		if len(p) == 1 {
+			return v, nil
+		}
 
 		d, err := v.ConvertToDocument()
 		if err != nil {
