bffa21ce64722738aee96a5398ea2db1f885d5a3
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2020-08-07 22:44:38 +0200

    Improve comparison rules

--- document/compare.go
+++ document/compare.go
@@ -2,8 +2,8 @@ package document
 
 import (
 	"bytes"
-	"errors"
-	"fmt"
+	"strings"
+	"time"
 )
 
 type operator uint8
@@ -74,35 +74,29 @@ func compare(op operator, l, r Value) (bool, error) {
 	case l.Type == NullValue || r.Type == NullValue:
 		return compareWithNull(op, l, r)
 
-	// compare documents together
-	case l.Type == DocumentValue && r.Type == DocumentValue:
-		return compareDocuments(op, l, r)
+	// compare booleans together
+	case l.Type == BoolValue && r.Type == BoolValue:
+		return compareBooleans(op, l.V.(bool), r.V.(bool)), nil
 
-	// compare arrays together
-	case l.Type == ArrayValue && r.Type == ArrayValue:
-		return compareArrays(op, l, r)
-
-	// compare boolean and another value
-	case l.Type == BoolValue || r.Type == BoolValue:
-		return compareWithBool(op, l, r)
-
-	// compare strings and bytes together
+	// compare texts together
 	case l.Type == TextValue && r.Type == TextValue:
-		fallthrough
-	case l.Type == BlobValue && r.Type == BlobValue:
-		fallthrough
-	case l.Type == TextValue && r.Type == BlobValue:
-		fallthrough
-	case r.Type == TextValue && l.Type == BlobValue:
-		return compareBytes(op, l, r)
+		return compareTexts(op, l.V.(string), r.V.(string)), nil
 
-	// integer OP integer
-	case l.Type.IsInteger() && r.Type.IsInteger():
-		return compareIntegers(op, l, r)
+	// compare blobs together
+	case r.Type == BlobValue && l.Type == BlobValue:
+		return compareBlobs(op, l.V.([]byte), r.V.([]byte)), nil
 
-	// number OP number
+	// compare integers together
+	case l.Type == IntegerValue && r.Type == IntegerValue:
+		return compareIntegers(op, l.V.(int64), r.V.(int64)), nil
+
+	// compare numbers together
 	case l.Type.IsNumber() && r.Type.IsNumber():
 		return compareNumbers(op, l, r)
+
+	// compare durations together
+	case l.Type == DurationValue && r.Type == DurationValue:
+		return compareIntegers(op, int64(l.V.(time.Duration)), int64(r.V.(time.Duration))), nil
 	}
 
 	return false, nil
@@ -116,97 +110,81 @@ func compareWithNull(op operator, l, r Value) (bool, error) {
 		return false, nil
 	}
 
-	return false, fmt.Errorf("unknown operator %v", op)
+	return false, nil
 }
 
-// when comparing booleans with numbers, true equals 1 and false 0
-// when comparing booleans with other types, the boolean is always smaller.
-func compareWithBool(op operator, l, r Value) (bool, error) {
-	// if comparing a boolean with something other than a number or another bool, always return false.
-	if (!l.Type.IsNumber() && l.Type != BoolValue) || (!r.Type.IsNumber() && r.Type != BoolValue) {
-		return false, nil
-	}
-
-	var a, b bool
-
-	a, err := l.IsTruthy()
-	if err != nil {
-		return false, err
-	}
-	b, err = r.IsTruthy()
-	if err != nil {
-		return false, err
-	}
-
+func compareBooleans(op operator, a, b bool) bool {
 	switch op {
 	case operatorEq:
-		return a == b, nil
+		return a == b
 	case operatorGt:
-		return a == true && b == false, nil
+		return a == true && b == false
 	case operatorGte:
-		return a == b || a == true, nil
+		return a == b || a == true
 	case operatorLt:
-		return a == false && b == true, nil
+		return a == false && b == true
 	case operatorLte:
-		return a == b || a == false, nil
+		return a == b || a == false
 	}
 
-	return false, fmt.Errorf("unknown operator %v", op)
+	return false
 }
 
-func compareBytes(op operator, l, r Value) (bool, error) {
-	var ok bool
-
+func compareTexts(op operator, l, r string) bool {
 	switch op {
 	case operatorEq:
-		ok = bytes.Equal(l.V.([]byte), r.V.([]byte))
+		return l == r
 	case operatorGt:
-		ok = bytes.Compare(l.V.([]byte), r.V.([]byte)) > 0
+		return strings.Compare(l, r) > 0
 	case operatorGte:
-		ok = bytes.Compare(l.V.([]byte), r.V.([]byte)) >= 0
+		return strings.Compare(l, r) >= 0
 	case operatorLt:
-		ok = bytes.Compare(l.V.([]byte), r.V.([]byte)) < 0
+		return strings.Compare(l, r) < 0
 	case operatorLte:
-		ok = bytes.Compare(l.V.([]byte), r.V.([]byte)) <= 0
+		return strings.Compare(l, r) <= 0
 	}
 
-	return ok, nil
+	return false
 }
 
-func compareIntegers(op operator, l, r Value) (bool, error) {
-	l, err := l.CastAsInteger()
-	if err != nil {
-		return false, err
-	}
-	r, err = r.CastAsInteger()
-	if err != nil {
-		return false, err
-	}
-
-	// integer OP integer
-	ai := l.V.(int64)
-	bi := r.V.(int64)
-
-	var ok bool
-
+func compareBlobs(op operator, l, r []byte) bool {
 	switch op {
 	case operatorEq:
-		ok = ai == bi
+		return bytes.Equal(l, r)
 	case operatorGt:
-		ok = ai > bi
+		return bytes.Compare(l, r) > 0
 	case operatorGte:
-		ok = ai >= bi
+		return bytes.Compare(l, r) >= 0
 	case operatorLt:
-		ok = ai < bi
+		return bytes.Compare(l, r) < 0
 	case operatorLte:
-		ok = ai <= bi
+		return bytes.Compare(l, r) <= 0
 	}
 
-	return ok, nil
+	return false
+}
+
+func compareIntegers(op operator, l, r int64) bool {
+	switch op {
+	case operatorEq:
+		return l == r
+	case operatorGt:
+		return l > r
+	case operatorGte:
+		return l >= r
+	case operatorLt:
+		return l < r
+	case operatorLte:
+		return l <= r
+	}
+
+	return false
 }
 
 func compareNumbers(op operator, l, r Value) (bool, error) {
-	l, err := l.CastAsDouble()
+	var err error
+
+	l, err = l.CastAsDouble()
 	if err != nil {
 		return false, err
 	}
@@ -234,126 +212,4 @@ func compareNumbers(op operator, l, r Value) (bool, error) {
 	}
 
 	return ok, nil
-}
-
-var errStop = errors.New("stop")
-
-func compareDocuments(op operator, l, r Value) (bool, error) {
-	if op != operatorEq {
-		return false, nil
-	}
-
-	ld := l.V.(Document)
-	rd := r.V.(Document)
-
-	var lsize, rsize int
-	err := ld.Iterate(func(field string, lv Value) error {
-		lsize++
-		return nil
-	})
-	if err != nil {
-		return false, err
-	}
-	err = rd.Iterate(func(field string, lv Value) error {
-		rsize++
-		return nil
-	})
-	if err != nil {
-		return false, err
-	}
-
-	if lsize != rsize {
-		return false, nil
-	}
-
-	// if both empty documents
-	if lsize == 0 {
-		return true, nil
-	}
-
-	var ok bool
-
-	err = ld.Iterate(func(field string, lv Value) error {
-		rv, err := rd.GetByField(field)
-		if err != nil {
-			return err
-		}
-
-		ok, err = compare(op, lv, rv)
-		if err != nil {
-			return err
-		}
-
-		if !ok {
-			return errStop
-		}
-
-		return nil
-	})
-
-	if err != nil && err != errStop {
-		return false, err
-	}
-
-	return ok, nil
-}
-
-func compareArrays(op operator, l, r Value) (bool, error) {
-	la := l.V.(Array)
-	ra := r.V.(Array)
-
-	var i, j int
-
-	for {
-		lv, lerr := la.GetByIndex(i)
-		rv, rerr := ra.GetByIndex(j)
-
-		if lerr == nil {
-			i++
-		}
-		if rerr == nil {
-			j++
-		}
-
-		if lerr != nil || rerr != nil {
-			break
-		}
-
-		isEq, err := compare(operatorEq, lv, rv)
-		if err != nil {
-			return false, err
-		}
-
-		if !isEq && op != operatorEq {
-			return compare(op, lv, rv)
-		}
-
-		if !isEq {
-			return false, nil
-		}
-	}
-
-	switch {
-	case i > j:
-		switch op {
-		case operatorEq, operatorLt, operatorLte:
-			return false, nil
-		default:
-			return true, nil
-		}
-	case i < j:
-		switch op {
-		case operatorEq, operatorGt, operatorGte:
-			return false, nil
-		default:
-			return true, nil
-		}
-	default:
-		switch op {
-		case operatorEq, operatorGte, operatorLte:
-			return true, nil
-		default:
-			return false, nil
-		}
-	}
 }
