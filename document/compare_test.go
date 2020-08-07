986596cf0c6bf4d159f5c1aa515ff34389ae116e
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2020-08-07 22:44:38 +0200

    Improve comparison rules

--- document/compare_test.go
+++ document/compare_test.go
@@ -4,415 +4,221 @@ import (
 	"encoding/json"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/genjidb/genji/document"
 	"github.com/stretchr/testify/require"
 )
 
-var numericFuncs = []struct {
-	name string
-	fn   func(x interface{}) document.Value
-}{
-	{"integer", func(x interface{}) document.Value { return document.NewIntegerValue(int64(x.(int))) }},
-	{"double", func(x interface{}) document.Value { return document.NewDoubleValue(float64(x.(int))) }},
+func jsonToInteger(t testing.TB, x string) document.Value {
+	var i int64
+	err := json.Unmarshal([]byte(x), &i)
+	require.NoError(t, err)
+
+	return document.NewIntegerValue(i)
 }
 
-var textFuncs = []struct {
-	name string
-	fn   func(x interface{}) document.Value
-}{
-	{"text", func(x interface{}) document.Value { return document.NewTextValue(x.(string)) }},
-	{"bytes", func(x interface{}) document.Value { return document.NewBlobValue([]byte(x.(string))) }},
+func jsonToDouble(t testing.TB, x string) document.Value {
+	var f float64
+	err := json.Unmarshal([]byte(x), &f)
+	require.NoError(t, err)
+
+	return document.NewDoubleValue(f)
 }
 
-func TestComparisonNumbers(t *testing.T) {
+func jsonToBoolean(t testing.TB, x string) document.Value {
+	var b bool
+	err := json.Unmarshal([]byte(x), &b)
+	require.NoError(t, err)
+
+	return document.NewBoolValue(b)
+}
+
+func jsonToDuration(t testing.TB, x string) document.Value {
+	var d time.Duration
+	err := json.Unmarshal([]byte(x), &d)
+	require.NoError(t, err)
+
+	return document.NewDurationValue(d)
+}
+
+func toText(t testing.TB, x string) document.Value {
+	return document.NewTextValue(x)
+}
+
+func toBlob(t testing.TB, x string) document.Value {
+	return document.NewBlobValue([]byte(x))
+}
+
+func jsonToArray(t testing.TB, x string) document.Value {
+	var vb document.ValueBuffer
+	err := json.Unmarshal([]byte(x), &vb)
+	require.NoError(t, err)
+
+	return document.NewArrayValue(vb)
+}
+
+func jsonToDocument(t testing.TB, x string) document.Value {
+	var fb document.FieldBuffer
+	err := json.Unmarshal([]byte(x), &fb)
+	require.NoError(t, err)
+
+	return document.NewDocumentValue(fb)
+}
+
+func TestCompare(t *testing.T) {
 	tests := []struct {
-		op   string
-		a, b int
-		ok   bool
+		op        string
+		a, b      string
+		ok        bool
+		converter func(testing.TB, string) document.Value
 	}{
-		{"=", 2, 1, false},
-		{"=", 2, 2, true},
-		{"!=", 2, 1, true},
-		{"!=", 2, 2, false},
-		{">", 2, 1, true},
-		{">", 1, 2, false},
-		{">", 2, 2, false},
-		{">=", 2, 1, true},
-		{">=", 1, 2, false},
-		{">=", 2, 2, true},
-		{"<", 2, 1, false},
-		{"<", 1, 2, true},
-		{"<", 2, 2, false},
-		{"<=", 2, 1, false},
-		{"<=", 1, 2, true},
-		{"<=", 2, 2, true},
-	}
+		// bool
+		{"=", "true", "false", false, jsonToBoolean},
+		{"=", "true", "true", true, jsonToBoolean},
+		{"!=", "true", "false", true, jsonToBoolean},
+		{"!=", "true", "true", false, jsonToBoolean},
+		{">", "true", "false", true, jsonToBoolean},
+		{">", "false", "true", false, jsonToBoolean},
+		{">", "true", "true", false, jsonToBoolean},
+		{">=", "true", "false", true, jsonToBoolean},
+		{">=", "false", "true", false, jsonToBoolean},
+		{">=", "true", "true", true, jsonToBoolean},
+		{"<", "true", "false", false, jsonToBoolean},
+		{"<", "false", "true", true, jsonToBoolean},
+		{"<", "true", "true", false, jsonToBoolean},
+		{"<=", "true", "false", false, jsonToBoolean},
+		{"<=", "false", "true", true, jsonToBoolean},
+		{"<=", "true", "true", true, jsonToBoolean},
 
-	for i := 0; i < len(numericFuncs); i++ {
-		for j := 0; j < len(numericFuncs); j++ {
-			for _, test := range tests {
-				t.Run(fmt.Sprintf("%s(%d)%s%s(%d)", numericFuncs[i].name, test.a, test.op, numericFuncs[j].name, test.b), func(t *testing.T) {
-					var ok bool
-					var err error
+		// integer
+		{"=", "2", "1", false, jsonToInteger},
+		{"=", "2", "2", true, jsonToInteger},
+		{"!=", "2", "1", true, jsonToInteger},
+		{"!=", "2", "2", false, jsonToInteger},
+		{">", "2", "1", true, jsonToInteger},
+		{">", "1", "2", false, jsonToInteger},
+		{">", "2", "2", false, jsonToInteger},
+		{">=", "2", "1", true, jsonToInteger},
+		{">=", "1", "2", false, jsonToInteger},
+		{">=", "2", "2", true, jsonToInteger},
+		{"<", "2", "1", false, jsonToInteger},
+		{"<", "1", "2", true, jsonToInteger},
+		{"<", "2", "2", false, jsonToInteger},
+		{"<=", "2", "1", false, jsonToInteger},
+		{"<=", "1", "2", true, jsonToInteger},
+		{"<=", "2", "2", true, jsonToInteger},
 
-					switch test.op {
-					case "=":
-						ok, err = numericFuncs[i].fn(test.a).IsEqual(numericFuncs[j].fn(test.b))
-					case "!=":
-						ok, err = numericFuncs[i].fn(test.a).IsNotEqual(numericFuncs[j].fn(test.b))
-					case ">":
-						ok, err = numericFuncs[i].fn(test.a).IsGreaterThan(numericFuncs[j].fn(test.b))
-					case ">=":
-						ok, err = numericFuncs[i].fn(test.a).IsGreaterThanOrEqual(numericFuncs[j].fn(test.b))
-					case "<":
-						ok, err = numericFuncs[i].fn(test.a).IsLesserThan(numericFuncs[j].fn(test.b))
-					case "<=":
-						ok, err = numericFuncs[i].fn(test.a).IsLesserThanOrEqual(numericFuncs[j].fn(test.b))
-					}
-					require.NoError(t, err)
-					require.Equal(t, test.ok, ok)
-				})
-			}
-		}
-	}
-}
+		// double
+		{"=", "2", "1", false, jsonToDouble},
+		{"=", "2", "2", true, jsonToDouble},
+		{"!=", "2", "1", true, jsonToDouble},
+		{"!=", "2", "2", false, jsonToDouble},
+		{">", "2", "1", true, jsonToDouble},
+		{">", "1", "2", false, jsonToDouble},
+		{">", "2", "2", false, jsonToDouble},
+		{">=", "2", "1", true, jsonToDouble},
+		{">=", "1", "2", false, jsonToDouble},
+		{">=", "2", "2", true, jsonToDouble},
+		{"<", "2", "1", false, jsonToDouble},
+		{"<", "1", "2", true, jsonToDouble},
+		{"<", "2", "2", false, jsonToDouble},
+		{"<=", "2", "1", false, jsonToDouble},
+		{"<=", "1", "2", true, jsonToDouble},
+		{"<=", "2", "2", true, jsonToDouble},
 
-func TestComparisonNumbersWithNull(t *testing.T) {
-	tests := []struct {
-		op string
-		a  int
-		ok bool
-	}{
-		{"=", 1, false},
-		{"=", 0, false},
-		{"!=", 0, true},
-		{"!=", 1, true},
-		{">", 1, false},
-		{">", 0, false},
-		{">=", 1, false},
-		{">=", 0, false},
-		{"<", 1, false},
-		{"<", 0, false},
-		{"<=", 1, false},
-		{"<=", 0, false},
-	}
+		// duration
+		{"=", "2", "1", false, jsonToDuration},
+		{"=", "2", "2", true, jsonToDuration},
+		{"!=", "2", "1", true, jsonToDuration},
+		{"!=", "2", "2", false, jsonToDuration},
+		{">", "2", "1", true, jsonToDuration},
+		{">", "1", "2", false, jsonToDuration},
+		{">", "2", "2", false, jsonToDuration},
+		{">=", "2", "1", true, jsonToDuration},
+		{">=", "1", "2", false, jsonToDuration},
+		{">=", "2", "2", true, jsonToDuration},
+		{"<", "2", "1", false, jsonToDuration},
+		{"<", "1", "2", true, jsonToDuration},
+		{"<", "2", "2", false, jsonToDuration},
+		{"<=", "2", "1", false, jsonToDuration},
+		{"<=", "1", "2", true, jsonToDuration},
+		{"<=", "2", "2", true, jsonToDuration},
 
-	for i := 0; i < len(numericFuncs); i++ {
-		for j := 0; j < len(numericFuncs); j++ {
-			for _, test := range tests {
-				t.Run(fmt.Sprintf("%s(%q)%sNULL", numericFuncs[i].name, test.a, test.op), func(t *testing.T) {
-					var ok bool
-					var err error
+		// text
+		{"=", "b", "a", false, toText},
+		{"=", "b", "b", true, toText},
+		{"!=", "b", "a", true, toText},
+		{"!=", "b", "b", false, toText},
+		{">", "b", "a", true, toText},
+		{">", "a", "b", false, toText},
+		{">", "b", "b", false, toText},
+		{">=", "b", "a", true, toText},
+		{">=", "a", "b", false, toText},
+		{">=", "b", "b", true, toText},
+		{"<", "b", "a", false, toText},
+		{"<", "a", "b", true, toText},
+		{"<", "b", "b", false, toText},
+		{"<=", "b", "a", false, toText},
+		{"<=", "a", "b", true, toText},
+		{"<=", "b", "b", true, toText},
 
-					switch test.op {
-					case "=":
-						ok, err = numericFuncs[i].fn(test.a).IsEqual(document.NewNullValue())
-					case "!=":
-						ok, err = numericFuncs[i].fn(test.a).IsNotEqual(document.NewNullValue())
-					case ">":
-						ok, err = numericFuncs[i].fn(test.a).IsGreaterThan(document.NewNullValue())
-					case ">=":
-						ok, err = numericFuncs[i].fn(test.a).IsGreaterThanOrEqual(document.NewNullValue())
-					case "<":
-						ok, err = numericFuncs[i].fn(test.a).IsLesserThan(document.NewNullValue())
-					case "<=":
-						ok, err = numericFuncs[i].fn(test.a).IsLesserThanOrEqual(document.NewNullValue())
-					}
-					require.NoError(t, err)
-					require.Equal(t, test.ok, ok)
-				})
-			}
-		}
-	}
-}
+		// blob
+		{"=", "b", "a", false, toBlob},
+		{"=", "b", "b", true, toBlob},
+		{"!=", "b", "a", true, toBlob},
+		{"!=", "b", "b", false, toBlob},
+		{">", "b", "a", true, toBlob},
+		{">", "a", "b", false, toBlob},
+		{">", "b", "b", false, toBlob},
+		{">=", "b", "a", true, toBlob},
+		{">=", "a", "b", false, toBlob},
+		{">=", "b", "b", true, toBlob},
+		{"<", "b", "a", false, toBlob},
+		{"<", "a", "b", true, toBlob},
+		{"<", "b", "b", false, toBlob},
+		{"<=", "b", "a", false, toBlob},
+		{"<=", "a", "b", true, toBlob},
+		{"<=", "b", "b", true, toBlob},
 
-func TestComparisonText(t *testing.T) {
-	tests := []struct {
-		op   string
-		a, b string
-		ok   bool
-	}{
-		{"=", "b", "a", false},
-		{"=", "b", "b", true},
-		{"!=", "b", "a", true},
-		{"!=", "b", "b", false},
-		{">", "b", "a", true},
-		{">", "a", "b", false},
-		{">", "b", "b", false},
-		{">=", "b", "a", true},
-		{">=", "a", "b", false},
-		{">=", "b", "b", true},
-		{"<", "b", "a", false},
-		{"<", "a", "b", true},
-		{"<", "b", "b", false},
-		{"<=", "b", "a", false},
-		{"<=", "a", "b", true},
-		{"<=", "b", "b", true},
-	}
+		// array
+		{"=", `[1]`, `[1]`, false, jsonToArray},
+		{"!=", `[1]`, `[1]`, false, toBlob},
+		{">", `[1]`, `[1]`, false, jsonToArray},
+		{">=", `[1]`, `[1]`, false, jsonToArray},
+		{"<", `[1]`, `[1]`, false, jsonToArray},
+		{"<=", `[1]`, `[1]`, false, jsonToArray},
 
-	for i := 0; i < len(textFuncs); i++ {
-		for j := 0; j < len(textFuncs); j++ {
-			for _, test := range tests {
-				t.Run(fmt.Sprintf("%s(%q)%s%s(%q)", textFuncs[i].name, test.a, test.op, textFuncs[j].name, test.b), func(t *testing.T) {
-					var ok bool
-					var err error
-
-					switch test.op {
-					case "=":
-						ok, err = textFuncs[i].fn(test.a).IsEqual(textFuncs[j].fn(test.b))
-					case "!=":
-						ok, err = textFuncs[i].fn(test.a).IsNotEqual(textFuncs[j].fn(test.b))
-					case ">":
-						ok, err = textFuncs[i].fn(test.a).IsGreaterThan(textFuncs[j].fn(test.b))
-					case ">=":
-						ok, err = textFuncs[i].fn(test.a).IsGreaterThanOrEqual(textFuncs[j].fn(test.b))
-					case "<":
-						ok, err = textFuncs[i].fn(test.a).IsLesserThan(textFuncs[j].fn(test.b))
-					case "<=":
-						ok, err = textFuncs[i].fn(test.a).IsLesserThanOrEqual(textFuncs[j].fn(test.b))
-					}
-					require.NoError(t, err)
-					require.Equal(t, test.ok, ok)
-				})
-			}
-		}
-	}
-
-}
-
-func TestComparisonTextWithNull(t *testing.T) {
-	nullTextTests := []struct {
-		op string
-		a  string
-		ok bool
-	}{
-		{"=", "a", false},
-		{"=", "", false},
-		{"!=", "a", true},
-		{"!=", "", true},
-		{">", "a", false},
-		{">", "", false},
-		{">=", "a", false},
-		{">=", "", false},
-		{"<", "a", false},
-		{"<", "", false},
-		{"<=", "a", false},
-		{"<=", "", false},
-	}
-
-	for i := 0; i < len(textFuncs); i++ {
-		for j := 0; j < len(textFuncs); j++ {
-			for _, test := range nullTextTests {
-				t.Run(fmt.Sprintf("%s(%q)%sNULL", textFuncs[i].name, test.a, test.op), func(t *testing.T) {
-					var ok bool
-					var err error
-
-					switch test.op {
-					case "=":
-						ok, err = textFuncs[i].fn(test.a).IsEqual(document.NewNullValue())
-					case "!=":
-						ok, err = textFuncs[i].fn(test.a).IsNotEqual(document.NewNullValue())
-					case ">":
-						ok, err = textFuncs[i].fn(test.a).IsGreaterThan(document.NewNullValue())
-					case ">=":
-						ok, err = textFuncs[i].fn(test.a).IsGreaterThanOrEqual(document.NewNullValue())
-					case "<":
-						ok, err = textFuncs[i].fn(test.a).IsLesserThan(document.NewNullValue())
-					case "<=":
-						ok, err = textFuncs[i].fn(test.a).IsLesserThanOrEqual(document.NewNullValue())
-					}
-					require.NoError(t, err)
-					require.Equal(t, test.ok, ok)
-				})
-			}
-		}
-	}
-}
-
-func TestComparisonDocuments(t *testing.T) {
-	tests := []struct {
-		op string
-		a  string
-		b  string
-	}{
-		{"=", `{}`, `{}`},
-		{"=", `{"a": 1}`, `{"a": 1}`},
-		{
-			"=",
-			`{"a": 1, "b": 2}`,
-			`{"b": 2, "a": 1}`,
-		},
-		{">", `{"a": 2}`, `{"a": 1}`},
-		{"<", `{"a": 1}`, `{"a": 2}`},
+		// document
+		{"=", `{"a": 1}`, `{"a": 1}`, false, jsonToDocument},
+		{">", `{"a": 1}`, `{"a": 1}`, false, jsonToDocument},
+		{">=", `{"a": 1}`, `{"a": 1}`, false, jsonToDocument},
+		{"<", `{"a": 1}`, `{"a": 1}`, false, jsonToDocument},
+		{"<=", `{"a": 1}`, `{"a": 1}`, false, jsonToDocument},
 	}
 
 	for _, test := range tests {
-		t.Run(fmt.Sprintf("%v %s %v", test.a, test.op, test.b), func(t *testing.T) {
+		a, b := test.converter(t, test.a), test.converter(t, test.b)
+		t.Run(fmt.Sprintf("%s/%v%v%v", a.Type.String(), a, test.op, b), func(t *testing.T) {
 			var ok bool
 			var err error
 
-			var d1, d2 document.FieldBuffer
-			require.NoError(t, json.Unmarshal([]byte(test.a), &d1))
-			require.NoError(t, json.Unmarshal([]byte(test.b), &d2))
-
 			switch test.op {
 			case "=":
-				ok, err = document.NewDocumentValue(d1).IsEqual(document.NewDocumentValue(d2))
-				require.NoError(t, err)
-				require.True(t, ok)
-			case ">":
-				ok, err = document.NewDocumentValue(d1).IsGreaterThan(document.NewDocumentValue(d2))
-				require.NoError(t, err)
-				require.False(t, ok)
-			case ">=":
-				ok, err = document.NewDocumentValue(d1).IsGreaterThanOrEqual(document.NewDocumentValue(d2))
-				require.NoError(t, err)
-				require.False(t, ok)
-			case "<":
-				ok, err = document.NewDocumentValue(d1).IsLesserThan(document.NewDocumentValue(d2))
-				require.NoError(t, err)
-				require.False(t, ok)
-			case "<=":
-				ok, err = document.NewDocumentValue(d1).IsLesserThanOrEqual(document.NewDocumentValue(d2))
-				require.NoError(t, err)
-				require.False(t, ok)
-			}
-		})
-	}
-}
-
-func TestComparisonArrays(t *testing.T) {
-	tests := []struct {
-		op       string
-		a        string
-		b        string
-		expected bool
-	}{
-		{"=", `[]`, `[]`, true},
-		{"=", `[1]`, `[1]`, true},
-		{"=", `[1]`, `[]`, false},
-		{"=", `[1.0, 2]`, `[1, 2]`, true},
-		{"=", `[1,2,3]`, `[1,2,3]`, true},
-		{"!=", `[1]`, `[5]`, true},
-		{"!=", `[1]`, `[1, 1]`, true},
-		{"!=", `[1,2,3]`, `[1,2,3]`, false},
-		{"!=", `[1]`, `[]`, true},
-		{">", `[2]`, `[1]`, true},
-		{">", `[2]`, `[1, 1000]`, true},
-		{">", `[1]`, `[1, 1000]`, false},
-		{">", `[1, 2]`, `[1, 1000]`, false},
-		{">", `[2, 1000]`, `[1]`, true},
-		{">", `[2, 1000]`, `[2]`, true},
-		{">", `[1,2,3]`, `[1,2,3]`, false},
-		{">", `[1,2,3]`, `[]`, true},
-		{">=", `[2]`, `[1]`, true},
-		{">=", `[2]`, `[2]`, true},
-		{">=", `[2]`, `[1, 1000]`, true},
-		{">=", `[1]`, `[1, 1000]`, false},
-		{">=", `[1, 2]`, `[1, 2]`, true},
-		{">=", `[1, 2]`, `[1, 1000]`, false},
-		{">=", `[2, 1000]`, `[1]`, true},
-		{">=", `[2, 1000]`, `[2]`, true},
-		{">=", `[1,2,3]`, `[1,2,3]`, true},
-		{">=", `[1,2,3]`, `[]`, true},
-		{"<", `[1]`, `[2]`, true},
-		{"<", `[1,2,3]`, `[1,2]`, false},
-		{"<", `[1,2,3]`, `[1,2,3]`, false},
-		{"<", `[1,2]`, `[1,2,3]`, true},
-		{"<", `[1, 1000]`, `[2]`, true},
-		{"<", `[2]`, `[2, 1000]`, true},
-		{"<", `[1,2,3]`, `[]`, false},
-		{"<", `[]`, `[1,2,3]`, true},
-		{"<=", `[1]`, `[2]`, true},
-		{"<=", `[1, 1000]`, `[2]`, true},
-		{"<=", `[1,2,3]`, `[1,2]`, false},
-		{">=", `[2]`, `[1]`, true},
-		{">=", `[2]`, `[2]`, true},
-		{">=", `[2]`, `[1, 1000]`, true},
-		{">=", `[2, 1000]`, `[1]`, true},
-		{"<=", `[1,2,3]`, `[1,2,3]`, true},
-		{"<=", `[]`, `[]`, true},
-		{"<=", `[]`, `[1,2,3]`, true},
-	}
-
-	for _, test := range tests {
-		t.Run(fmt.Sprintf("%v %s %v", test.a, test.op, test.b), func(t *testing.T) {
-			var ok bool
-			var err error
-
-			var a1, a2 document.ValueBuffer
-			require.NoError(t, json.Unmarshal([]byte(test.a), &a1))
-			require.NoError(t, json.Unmarshal([]byte(test.b), &a2))
-
-			switch test.op {
-			case "=":
-				ok, err = document.NewArrayValue(a1).IsEqual(document.NewArrayValue(a2))
-			case ">":
-				ok, err = document.NewArrayValue(a1).IsGreaterThan(document.NewArrayValue(a2))
-			case ">=":
-				ok, err = document.NewArrayValue(a1).IsGreaterThanOrEqual(document.NewArrayValue(a2))
-			case "<":
-				ok, err = document.NewArrayValue(a1).IsLesserThan(document.NewArrayValue(a2))
-			case "<=":
-				ok, err = document.NewArrayValue(a1).IsLesserThanOrEqual(document.NewArrayValue(a2))
+				ok, err = a.IsEqual(b)
 			case "!=":
-				ok, err = document.NewArrayValue(a1).IsNotEqual(document.NewArrayValue(a2))
+				ok, err = a.IsNotEqual(b)
+			case ">":
+				ok, err = a.IsGreaterThan(b)
+			case ">=":
+				ok, err = a.IsGreaterThanOrEqual(b)
+			case "<":
+				ok, err = a.IsLesserThan(b)
+			case "<=":
+				ok, err = a.IsLesserThanOrEqual(b)
 			}
 			require.NoError(t, err)
-			require.Equal(t, test.expected, ok)
+			require.Equal(t, test.ok, ok)
 		})
 	}
-}
-
-func TestComparisonDifferentTypes(t *testing.T) {
-	tests := []struct {
-		op string
-		a  int
-		b  string
-	}{
-		{"=", 1, "1"},
-		{"=", 0, ""},
-		{"=", 0, ""},
-		{">", 2, "1"},
-		{">", 1, ""},
-		{">=", 1, "1"},
-		{">=", 0, ""},
-		{"<", 0, "1"},
-		{"<", -1, ""},
-		{"<=", 1, "1"},
-		{"<=", 0, ""},
-	}
-
-	for i := 0; i < len(numericFuncs); i++ {
-		for j := 0; j < len(textFuncs); j++ {
-			for _, test := range tests {
-				t.Run(fmt.Sprintf("%s(%d)%s%s(%q)", numericFuncs[i].name, test.a, test.op, textFuncs[j].name, test.b), func(t *testing.T) {
-					var ok bool
-					var err error
-
-					switch test.op {
-					case "=":
-						ok, err = numericFuncs[i].fn(test.a).IsEqual(textFuncs[j].fn(test.b))
-					case ">":
-						ok, err = numericFuncs[i].fn(test.a).IsGreaterThan(textFuncs[j].fn(test.b))
-					case ">=":
-						ok, err = numericFuncs[i].fn(test.a).IsGreaterThanOrEqual(textFuncs[j].fn(test.b))
-					case "<":
-						ok, err = numericFuncs[i].fn(test.a).IsLesserThan(textFuncs[j].fn(test.b))
-					case "<=":
-						ok, err = numericFuncs[i].fn(test.a).IsLesserThanOrEqual(textFuncs[j].fn(test.b))
-					}
-					require.NoError(t, err)
-					require.False(t, ok)
-				})
-			}
-		}
-	}
-
-	t.Run("not equal with different types", func(t *testing.T) {
-		ok, err := document.NewIntegerValue(1).IsNotEqual(document.NewTextValue("foo"))
-		require.NoError(t, err)
-		require.True(t, ok)
-	})
 }
