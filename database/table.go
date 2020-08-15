d7f1b4832108f9ff600d5bcbeb183080abccae89
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2020-08-15 09:24:29 +0200

    Prevent indexes and arrays to be indexed

--- database/table.go
+++ database/table.go
@@ -83,6 +83,11 @@ func (t *Table) Insert(d document.Document) ([]byte, error) {
 			v = document.NewNullValue()
 		}
 
+		// arrays and documents are not indexed.
+		if v.Type == document.ArrayValue || v.Type == document.DocumentValue {
+			continue
+		}
+
 		err = idx.Set(v, key)
 		if err != nil {
 			if err == index.ErrDuplicate {
