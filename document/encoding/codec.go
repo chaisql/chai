c7ea2f4775734a460c0bddda5b7e9a2183dcb1fd
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2020-09-03 19:49:15 +0400

    Add Codec interface

---
+++ document/encoding/codec.go
@@ -0,0 +1,22 @@
+// Package encoding defines types that deal with documents encoding.
+package encoding
+
+import (
+	"io"
+
+	"github.com/genjidb/genji/document"
+)
+
+// A Codec is able to create encoders and decoders for a specific encoding format.
+type Codec interface {
+	NewEncoder(io.Writer) Encoder
+	// NewDocument returns a document without decoding its given binary representation.
+	// The returned document should ideally support random-access, i.e. decoding one path
+	// without decoding the entire document. If not, the document must be lazily decoded.
+	NewDocument([]byte) document.Document
+}
+
+// An Encoder encodes one document to the underlying writer.
+type Encoder interface {
+	EncodeDocument(d document.Document) error
+}
