andrewhare/genji

Add Grouping node and support for aggregators to planner

sql/parser/select_test.go
@@ -18,8 +18,8 @@ func TestParserSelect(t *testing.T) {
 	}{
 		{"NoTable", "SELECT 1",
 			planner.NewTree(planner.NewProjectionNode(nil,
-				[]planner.ResultField{
-					planner.ResultFieldExpr{Expr: expr.IntegerValue(1), ExprName: "1"},
+				[]planner.ProjectedField{
+					planner.ProjectedExpr{Expr: expr.IntegerValue(1), ExprName: "1"},
 				}, "")),
 			false,
 		},
@@ -27,7 +27,7 @@ func TestParserSelect(t *testing.T) {
 			planner.NewTree(
 				planner.NewProjectionNode(
 					planner.NewTableInputNode("test"),
-					[]planner.ResultField{planner.Wildcard{}},
+					[]planner.ProjectedField{planner.Wildcard{}},
 					"test",
 				)),
 			false},
@@ -35,7 +35,7 @@ func TestParserSelect(t *testing.T) {
 			planner.NewTree(
 				planner.NewProjectionNode(
 					planner.NewTableInputNode("test"),
-					[]planner.ResultField{planner.ResultFieldExpr{Expr: expr.FieldSelector(parsePath(t, "a")), ExprName: "a"}, planner.ResultFieldExpr{Expr: expr.FieldSelector(parsePath(t, "b")), ExprName: "b"}},
+					[]planner.ProjectedField{planner.ProjectedExpr{Expr: expr.FieldSelector(parsePath(t, "a")), ExprName: "a"}, planner.ProjectedExpr{Expr: expr.FieldSelector(parsePath(t, "b")), ExprName: "b"}},
 					"test",
 				)),
 			false},
@@ -43,7 +43,7 @@ func TestParserSelect(t *testing.T) {
 			planner.NewTree(
 				planner.NewProjectionNode(
 					planner.NewTableInputNode("test"),
-					[]planner.ResultField{planner.ResultFieldExpr{Expr: expr.FieldSelector(parsePath(t, "`long \"field\"`")), ExprName: "long \"field\""}},
+					[]planner.ProjectedField{planner.ProjectedExpr{Expr: expr.FieldSelector(parsePath(t, "`long \"field\"`")), ExprName: "long \"field\""}},
 					"test",
 				)),
 			false},
@@ -51,7 +51,7 @@ func TestParserSelect(t *testing.T) {
 			planner.NewTree(
 				planner.NewProjectionNode(
 					planner.NewTableInputNode("test"),
-					[]planner.ResultField{planner.ResultFieldExpr{Expr: expr.FieldSelector(parsePath(t, "a")), ExprName: "A"}, planner.ResultFieldExpr{Expr: expr.FieldSelector(parsePath(t, "b")), ExprName: "b"}},
+					[]planner.ProjectedField{planner.ProjectedExpr{Expr: expr.FieldSelector(parsePath(t, "a")), ExprName: "A"}, planner.ProjectedExpr{Expr: expr.FieldSelector(parsePath(t, "b")), ExprName: "b"}},
 					"test",
 				)),
 			false},
@@ -59,7 +59,7 @@ func TestParserSelect(t *testing.T) {
 			planner.NewTree(
 				planner.NewProjectionNode(
 					planner.NewTableInputNode("test"),
-					[]planner.ResultField{planner.ResultFieldExpr{Expr: expr.FieldSelector(parsePath(t, "a")), ExprName: "a"}, planner.ResultFieldExpr{Expr: expr.FieldSelector(parsePath(t, "b")), ExprName: "b"}, planner.Wildcard{}},
+					[]planner.ProjectedField{planner.ProjectedExpr{Expr: expr.FieldSelector(parsePath(t, "a")), ExprName: "a"}, planner.ProjectedExpr{Expr: expr.FieldSelector(parsePath(t, "b")), ExprName: "b"}, planner.Wildcard{}},
 					"test",
 				)),
 			false},
@@ -67,7 +67,7 @@ func TestParserSelect(t *testing.T) {
 			planner.NewTree(
 				planner.NewProjectionNode(
 					planner.NewTableInputNode("test"),
-					[]planner.ResultField{planner.ResultFieldExpr{Expr: expr.Gt(expr.FieldSelector(parsePath(t, "a")), expr.IntegerValue(1)), ExprName: "a    > 1"}},
+					[]planner.ProjectedField{planner.ProjectedExpr{Expr: expr.Gt(expr.FieldSelector(parsePath(t, "a")), expr.IntegerValue(1)), ExprName: "a    > 1"}},
 					"test",
 				)),
 			false},
@@ -78,7 +78,7 @@ func TestParserSelect(t *testing.T) {
 						planner.NewTableInputNode("test"),
 						expr.Eq(expr.FieldSelector(parsePath(t, "age")), expr.IntegerValue(10)),
 					),
-					[]planner.ResultField{planner.Wildcard{}},
+					[]planner.ProjectedField{planner.Wildcard{}},
 					"test",
 				)),
 			false},
@@ -93,7 +93,7 @@ func TestParserSelect(t *testing.T) {
 						expr.FieldSelector(parsePath(t, "a.b.c")),
 						scanner.ASC,
 					),
-					[]planner.ResultField{planner.Wildcard{}},
+					[]planner.ProjectedField{planner.Wildcard{}},
 					"test",
 				)),
 			false},
@@ -108,7 +108,7 @@ func TestParserSelect(t *testing.T) {
 						expr.FieldSelector(parsePath(t, "a.b.c")),
 						scanner.ASC,
 					),
-					[]planner.ResultField{planner.Wildcard{}},
+					[]planner.ProjectedField{planner.Wildcard{}},
 					"test",
 				)),
 			false},
@@ -123,7 +123,7 @@ func TestParserSelect(t *testing.T) {
 						expr.FieldSelector(parsePath(t, "a.b.c")),
 						scanner.DESC,
 					),
-					[]planner.ResultField{planner.Wildcard{}},
+					[]planner.ProjectedField{planner.Wildcard{}},
 					"test",
 				)),
 			false},
@@ -137,7 +137,7 @@ func TestParserSelect(t *testing.T) {
 						),
 						20,
 					),
-					[]planner.ResultField{planner.Wildcard{}},
+					[]planner.ProjectedField{planner.Wildcard{}},
 					"test",
 				)),
 			false},
@@ -151,7 +151,7 @@ func TestParserSelect(t *testing.T) {
 						),
 						20,
 					),
-					[]planner.ResultField{planner.Wildcard{}},
+					[]planner.ProjectedField{planner.Wildcard{}},
 					"test",
 				)),
 			false},
@@ -168,7 +168,7 @@ func TestParserSelect(t *testing.T) {
 						),
 						10,
 					),
-					[]planner.ResultField{planner.Wildcard{}},
+					[]planner.ProjectedField{planner.Wildcard{}},
 					"test",
 				)),
 			false},

sql/planner/projection.go
@@ -18,7 +18,7 @@ import (
 type ProjectionNode struct {
 	node
 
-	Expressions []ResultField
+	Expressions []ProjectedField
 	tableName   string
 
 	info *database.TableInfo
@@ -29,7 +29,7 @@ var _ outputNode = (*ProjectionNode)(nil)
 var _ operationNode = (*ProjectionNode)(nil)
 
 // NewProjectionNode creates a ProjectionNode.
-func NewProjectionNode(n Node, expressions []ResultField, tableName string) Node {
+func NewProjectionNode(n Node, expressions []ProjectedField, tableName string) Node {
 	return &ProjectionNode{
 		node: node{
 			op:   Projection,
@@ -57,6 +57,18 @@ func (n *ProjectionNode) Bind(tx *database.Transaction, params []expr.Param) (er
 }
 
 func (n *ProjectionNode) toStream(st document.Stream) (document.Stream, error) {
+	var aggregators []func(d document.Document, fb *document.FieldBuffer) error
+
+	for _, e := range n.Expressions {
+		if agg, ok := e.(Aggregator); ok {
+			aggregators = append(aggregators, agg.Aggregate)
+		}
+	}
+
+	if len(aggregators) > 0 {
+		st = st.Aggregate(aggregators...)
+	}
+
 	if st.IsEmpty() {
 		d := documentMask{
 			resultFields: n.Expressions,
@@ -67,17 +79,19 @@ func (n *ProjectionNode) toStream(st document.Stream) (document.Stream, error) {
 			return st, err
 		}
 
-		return document.NewStream(document.NewIterator(fb)), nil
+		st = document.NewStream(document.NewIterator(fb))
+	} else {
+		var dm documentMask
+		st = st.Map(func(d document.Document) (document.Document, error) {
+			dm.info = n.info
+			dm.r = d
+			dm.resultFields = n.Expressions
+
+			return &dm, nil
+		})
 	}
 
-	var dm documentMask
-	return st.Map(func(d document.Document) (document.Document, error) {
-		dm.info = n.info
-		dm.r = d
-		dm.resultFields = n.Expressions
-
-		return &dm, nil
-	}), nil
+	return st, nil
 }
 
 func (n *ProjectionNode) toResult(st document.Stream) (res query.Result, err error) {
@@ -106,7 +120,7 @@ func (n *ProjectionNode) String() string {
 type documentMask struct {
 	info         *database.TableInfo
 	r            document.Document
-	resultFields []ResultField
+	resultFields []ProjectedField
 }
 
 var _ document.Document = documentMask{}
@@ -149,26 +163,26 @@ func (r documentMask) MarshalJSON() ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-// A ResultField is a field that will be part of the result document that will be returned at the end of a Select statement.
-type ResultField interface {
+// A ProjectedField is a field that will be part of the projected document that will be returned at the end of a Select statement.
+type ProjectedField interface {
 	Iterate(stack expr.EvalStack, fn func(field string, value document.Value) error) error
 	Name() string
 }
 
-// ResultFieldExpr turns any expression into a ResultField.
-type ResultFieldExpr struct {
+// ProjectedExpr turns any expression into a ResultField.
+type ProjectedExpr struct {
 	expr.Expr
 
 	ExprName string
 }
 
 // Name returns the raw expression.
-func (r ResultFieldExpr) Name() string {
+func (r ProjectedExpr) Name() string {
 	return r.ExprName
 }
 
 // Iterate evaluates Expr and calls fn once with the result.
-func (r ResultFieldExpr) Iterate(stack expr.EvalStack, fn func(field string, value document.Value) error) error {
+func (r ProjectedExpr) Iterate(stack expr.EvalStack, fn func(field string, value document.Value) error) error {
 	v, err := r.Expr.Eval(stack)
 	if err != nil {
 		return err
@@ -177,7 +191,7 @@ func (r ResultFieldExpr) Iterate(stack expr.EvalStack, fn func(field string, val
 	return fn(r.ExprName, v)
 }
 
-func (r ResultFieldExpr) String() string {
+func (r ProjectedExpr) String() string {
 	return fmt.Sprintf("%s", r.Expr)
 }
 
@@ -200,4 +214,8 @@ func (w Wildcard) Iterate(stack expr.EvalStack, fn func(field string, value docu
 	}
 
 	return stack.Document.Iterate(fn)
+}
+
+type Aggregator interface {
+	Aggregate(d document.Document, fb *document.FieldBuffer) error
 }

chaisql/chai

sql/parser/select_test.go
@@ -18,8 +18,8 @@ func TestParserSelect(t *testing.T) {
 	}{
 		{"NoTable", "SELECT 1",
 			planner.NewTree(planner.NewProjectionNode(nil,
-				[]planner.ResultField{
-					planner.ResultFieldExpr{Expr: expr.IntegerValue(1), ExprName: "1"},
+				[]planner.ProjectedField{
+					planner.ProjectedExpr{Expr: expr.IntegerValue(1), ExprName: "1"},
 				}, "")),
 			false,
 		},
@@ -27,7 +27,7 @@ func TestParserSelect(t *testing.T) {
 			planner.NewTree(
 				planner.NewProjectionNode(
 					planner.NewTableInputNode("test"),
-					[]planner.ResultField{planner.Wildcard{}},
+					[]planner.ProjectedField{planner.Wildcard{}},
 					"test",
 				)),
 			false},
@@ -35,7 +35,7 @@ func TestParserSelect(t *testing.T) {
 			planner.NewTree(
 				planner.NewProjectionNode(
 					planner.NewTableInputNode("test"),
-					[]planner.ResultField{planner.ResultFieldExpr{Expr: expr.FieldSelector(parsePath(t, "a")), ExprName: "a"}, planner.ResultFieldExpr{Expr: expr.FieldSelector(parsePath(t, "b")), ExprName: "b"}},
+					[]planner.ProjectedField{planner.ProjectedExpr{Expr: expr.FieldSelector(parsePath(t, "a")), ExprName: "a"}, planner.ProjectedExpr{Expr: expr.FieldSelector(parsePath(t, "b")), ExprName: "b"}},
 					"test",
 				)),
 			false},
@@ -43,7 +43,7 @@ func TestParserSelect(t *testing.T) {
 			planner.NewTree(
 				planner.NewProjectionNode(
 					planner.NewTableInputNode("test"),
-					[]planner.ResultField{planner.ResultFieldExpr{Expr: expr.FieldSelector(parsePath(t, "`long \"path\"`")), ExprName: "long \"path\""}},
+					[]planner.ProjectedField{planner.ProjectedExpr{Expr: expr.FieldSelector(parsePath(t, "`long \"path\"`")), ExprName: "long \"path\""}},
 					"test",
 				)),
 			false},
@@ -51,7 +51,7 @@ func TestParserSelect(t *testing.T) {
 			planner.NewTree(
 				planner.NewProjectionNode(
 					planner.NewTableInputNode("test"),
-					[]planner.ResultField{planner.ResultFieldExpr{Expr: expr.FieldSelector(parsePath(t, "a")), ExprName: "A"}, planner.ResultFieldExpr{Expr: expr.FieldSelector(parsePath(t, "b")), ExprName: "b"}},
+					[]planner.ProjectedField{planner.ProjectedExpr{Expr: expr.FieldSelector(parsePath(t, "a")), ExprName: "A"}, planner.ProjectedExpr{Expr: expr.FieldSelector(parsePath(t, "b")), ExprName: "b"}},
 					"test",
 				)),
 			false},
@@ -59,7 +59,7 @@ func TestParserSelect(t *testing.T) {
 			planner.NewTree(
 				planner.NewProjectionNode(
 					planner.NewTableInputNode("test"),
-					[]planner.ResultField{planner.ResultFieldExpr{Expr: expr.FieldSelector(parsePath(t, "a")), ExprName: "a"}, planner.ResultFieldExpr{Expr: expr.FieldSelector(parsePath(t, "b")), ExprName: "b"}, planner.Wildcard{}},
+					[]planner.ProjectedField{planner.ProjectedExpr{Expr: expr.FieldSelector(parsePath(t, "a")), ExprName: "a"}, planner.ProjectedExpr{Expr: expr.FieldSelector(parsePath(t, "b")), ExprName: "b"}, planner.Wildcard{}},
 					"test",
 				)),
 			false},
@@ -67,7 +67,7 @@ func TestParserSelect(t *testing.T) {
 			planner.NewTree(
 				planner.NewProjectionNode(
 					planner.NewTableInputNode("test"),
-					[]planner.ResultField{planner.ResultFieldExpr{Expr: expr.Gt(expr.FieldSelector(parsePath(t, "a")), expr.IntegerValue(1)), ExprName: "a    > 1"}},
+					[]planner.ProjectedField{planner.ProjectedExpr{Expr: expr.Gt(expr.FieldSelector(parsePath(t, "a")), expr.IntegerValue(1)), ExprName: "a    > 1"}},
 					"test",
 				)),
 			false},
@@ -78,7 +78,7 @@ func TestParserSelect(t *testing.T) {
 						planner.NewTableInputNode("test"),
 						expr.Eq(expr.FieldSelector(parsePath(t, "age")), expr.IntegerValue(10)),
 					),
-					[]planner.ResultField{planner.Wildcard{}},
+					[]planner.ProjectedField{planner.Wildcard{}},
 					"test",
 				)),
 			false},
@@ -90,7 +90,7 @@ func TestParserSelect(t *testing.T) {
 							planner.NewTableInputNode("test"),
 							expr.Eq(expr.FieldSelector(parsePath(t, "age")), expr.IntegerValue(10)),
 						),
-						[]planner.ResultField{planner.Wildcard{}},
+						[]planner.ProjectedField{planner.Wildcard{}},
 						"test",
 					),
 					expr.FieldSelector(parsePath(t, "a.b.c")),
@@ -105,7 +105,7 @@ func TestParserSelect(t *testing.T) {
 							planner.NewTableInputNode("test"),
 							expr.Eq(expr.FieldSelector(parsePath(t, "age")), expr.IntegerValue(10)),
 						),
-						[]planner.ResultField{planner.Wildcard{}},
+						[]planner.ProjectedField{planner.Wildcard{}},
 						"test",
 					),
 					expr.FieldSelector(parsePath(t, "a.b.c")),
@@ -120,7 +120,7 @@ func TestParserSelect(t *testing.T) {
 							planner.NewTableInputNode("test"),
 							expr.Eq(expr.FieldSelector(parsePath(t, "age")), expr.IntegerValue(10)),
 						),
-						[]planner.ResultField{planner.Wildcard{}},
+						[]planner.ProjectedField{planner.Wildcard{}},
 						"test",
 					),
 					expr.FieldSelector(parsePath(t, "a.b.c")),
@@ -135,7 +135,7 @@ func TestParserSelect(t *testing.T) {
 							planner.NewTableInputNode("test"),
 							expr.Eq(expr.FieldSelector(parsePath(t, "age")), expr.IntegerValue(10)),
 						),
-						[]planner.ResultField{planner.Wildcard{}},
+						[]planner.ProjectedField{planner.Wildcard{}},
 						"test",
 					),
 					20,
@@ -149,7 +149,7 @@ func TestParserSelect(t *testing.T) {
 							planner.NewTableInputNode("test"),
 							expr.Eq(expr.FieldSelector(parsePath(t, "age")), expr.IntegerValue(10)),
 						),
-						[]planner.ResultField{planner.Wildcard{}},
+						[]planner.ProjectedField{planner.Wildcard{}},
 						"test",
 					),
 					20,
@@ -164,7 +164,7 @@ func TestParserSelect(t *testing.T) {
 								planner.NewTableInputNode("test"),
 								expr.Eq(expr.FieldSelector(parsePath(t, "age")), expr.IntegerValue(10)),
 							),
-							[]planner.ResultField{planner.Wildcard{}},
+							[]planner.ProjectedField{planner.Wildcard{}},
 							"test",
 						),
 						20,

sql/planner/projection.go
@@ -17,7 +17,7 @@ import (
 type ProjectionNode struct {
 	node
 
-	Expressions []ResultField
+	Expressions []ProjectedField
 	tableName   string
 
 	info *database.TableInfo
@@ -27,7 +27,7 @@ type ProjectionNode struct {
 var _ operationNode = (*ProjectionNode)(nil)
 
 // NewProjectionNode creates a ProjectionNode.
-func NewProjectionNode(n Node, expressions []ResultField, tableName string) Node {
+func NewProjectionNode(n Node, expressions []ProjectedField, tableName string) Node {
 	return &ProjectionNode{
 		node: node{
 			op:   Projection,
@@ -55,6 +55,18 @@ func (n *ProjectionNode) Bind(tx *database.Transaction, params []expr.Param) (er
 }
 
 func (n *ProjectionNode) toStream(st document.Stream) (document.Stream, error) {
+	var aggregators []func(d document.Document, fb *document.FieldBuffer) error
+
+	for _, e := range n.Expressions {
+		if agg, ok := e.(Aggregator); ok {
+			aggregators = append(aggregators, agg.Aggregate)
+		}
+	}
+
+	if len(aggregators) > 0 {
+		st = st.Aggregate(aggregators...)
+	}
+
 	if st.IsEmpty() {
 		d := documentMask{
 			resultFields: n.Expressions,
@@ -65,17 +77,19 @@ func (n *ProjectionNode) toStream(st document.Stream) (document.Stream, error) {
 			return st, err
 		}
 
-		return document.NewStream(document.NewIterator(fb)), nil
+		st = document.NewStream(document.NewIterator(fb))
+	} else {
+		var dm documentMask
+		st = st.Map(func(d document.Document) (document.Document, error) {
+			dm.info = n.info
+			dm.d = d
+			dm.resultFields = n.Expressions
+
+			return &dm, nil
+		})
 	}
 
-	var dm documentMask
-	return st.Map(func(d document.Document) (document.Document, error) {
-		dm.info = n.info
-		dm.d = d
-		dm.resultFields = n.Expressions
-
-		return &dm, nil
-	}), nil
+	return st, nil
 }
 
 func (n *ProjectionNode) String() string {
@@ -94,7 +108,7 @@ func (n *ProjectionNode) String() string {
 type documentMask struct {
 	info         *database.TableInfo
 	d            document.Document
-	resultFields []ResultField
+	resultFields []ProjectedField
 }
 
 var _ document.Document = documentMask{}
@@ -137,26 +151,26 @@ func (r documentMask) MarshalJSON() ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-// A ResultField is a field that will be part of the result document that will be returned at the end of a Select statement.
-type ResultField interface {
+// A ProjectedField is a field that will be part of the projected document that will be returned at the end of a Select statement.
+type ProjectedField interface {
 	Iterate(stack expr.EvalStack, fn func(field string, value document.Value) error) error
 	Name() string
 }
 
-// ResultFieldExpr turns any expression into a ResultField.
-type ResultFieldExpr struct {
+// ProjectedExpr turns any expression into a ResultField.
+type ProjectedExpr struct {
 	expr.Expr
 
 	ExprName string
 }
 
 // Name returns the raw expression.
-func (r ResultFieldExpr) Name() string {
+func (r ProjectedExpr) Name() string {
 	return r.ExprName
 }
 
 // Iterate evaluates Expr and calls fn once with the result.
-func (r ResultFieldExpr) Iterate(stack expr.EvalStack, fn func(field string, value document.Value) error) error {
+func (r ProjectedExpr) Iterate(stack expr.EvalStack, fn func(field string, value document.Value) error) error {
 	v, err := r.Expr.Eval(stack)
 	if err != nil {
 		return err
@@ -165,7 +179,7 @@ func (r ResultFieldExpr) Iterate(stack expr.EvalStack, fn func(field string, val
 	return fn(r.ExprName, v)
 }
 
-func (r ResultFieldExpr) String() string {
+func (r ProjectedExpr) String() string {
 	return fmt.Sprintf("%s", r.Expr)
 }
 
@@ -188,4 +202,8 @@ func (w Wildcard) Iterate(stack expr.EvalStack, fn func(field string, value docu
 	}
 
 	return stack.Document.Iterate(fn)
+}
+
+type Aggregator interface {
+	Aggregate(d document.Document, fb *document.FieldBuffer) error
 }