a22a8a467fa04d68526ad51cfcea10a54deb39aa
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2020-09-16 10:51:58 +0400

    Fix scan of encoded documents and arrays

--- document/scan_test.go
+++ document/scan_test.go
@@ -1,15 +1,33 @@
 package document_test
 
 import (
+	"bytes"
 	"testing"
 	"time"
 
 	"github.com/genjidb/genji/document"
+	"github.com/genjidb/genji/document/encoding/msgpack"
 	"github.com/stretchr/testify/require"
 )
 
 func TestScan(t *testing.T) {
 	now := time.Now()
+
+	simpleDoc := document.NewFieldBuffer().
+		Add("foo", document.NewTextValue("foo")).
+		Add("bar", document.NewTextValue("bar")).
+		Add("baz", document.NewArrayValue(document.NewValueBuffer(
+			document.NewIntegerValue(10),
+			document.NewDoubleValue(20.5),
+		)))
+
+	nestedDoc := document.NewFieldBuffer().
+		Add("foo", document.NewDocumentValue(simpleDoc))
+
+	var buf bytes.Buffer
+	codec := msgpack.NewCodec()
+	err := codec.NewEncoder(&buf).EncodeDocument(nestedDoc)
+	require.NoError(t, err)
 
 	doc := document.NewFieldBuffer().
 		Add("a", document.NewBlobValue([]byte("foo"))).
@@ -48,7 +66,8 @@ func TestScan(t *testing.T) {
 				Add("bar", document.NewTextValue("bar")),
 		)).
 		Add("o", document.NewDurationValue(10*time.Nanosecond)).
-		Add("p", document.NewTextValue(now.Format(time.RFC3339Nano)))
+		Add("p", document.NewTextValue(now.Format(time.RFC3339Nano))).
+		Add("r", document.NewDocumentValue(codec.NewDocument(buf.Bytes())))
 
 	type foo struct {
 		Foo string
@@ -72,8 +91,9 @@ func TestScan(t *testing.T) {
 	var n map[string]string
 	var o time.Duration
 	var p time.Time
+	var r map[string]interface{}
 
-	err := document.Scan(doc, &a, &b, &c, &d, &e, &f, &g, &h, &i, &j, &k, &l, &m, &n, &o, &p)
+	err = document.Scan(doc, &a, &b, &c, &d, &e, &f, &g, &h, &i, &j, &k, &l, &m, &n, &o, &p, &r)
 	require.NoError(t, err)
 	require.Equal(t, a, []byte("foo"))
 	require.Equal(t, b, "bar")
@@ -92,6 +112,15 @@ func TestScan(t *testing.T) {
 	require.Equal(t, map[string]string{"foo": "foo", "bar": "bar"}, n)
 	require.Equal(t, 10*time.Nanosecond, o)
 	require.Equal(t, now.Format(time.RFC3339Nano), p.Format(time.RFC3339Nano))
+	require.Equal(t, map[string]interface{}{
+		"foo": map[string]interface{}{
+			"foo": "foo",
+			"bar": "bar",
+			"baz": []interface{}{
+				int64(10), float64(20.5),
+			},
+		},
+	}, r)
 
 	t.Run("DocumentScanner", func(t *testing.T) {
 		var ds documentScanner
@@ -107,14 +136,14 @@ func TestScan(t *testing.T) {
 		m := make(map[string]interface{})
 		err := document.MapScan(doc, m)
 		require.NoError(t, err)
-		require.Len(t, m, 16)
+		require.Len(t, m, 17)
 	})
 
 	t.Run("MapPtr", func(t *testing.T) {
 		var m map[string]interface{}
 		err := document.MapScan(doc, &m)
 		require.NoError(t, err)
-		require.Len(t, m, 16)
+		require.Len(t, m, 17)
 	})
 
 	t.Run("Small Slice", func(t *testing.T) {
