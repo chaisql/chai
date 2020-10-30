9e26d5e53bd4fdad4c4fe09deb2e0d032a47afe4
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2020-10-30 11:10:49 +0400

    Add failing test

---
+++ database/database_test.go
@@ -0,0 +1,69 @@
+package database_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/genjidb/genji"
+	"github.com/stretchr/testify/require"
+)
+
+// See issue https://github.com/genjidb/genji/issues/298
+func TestConcurrentTransactionManagement(t *testing.T) {
+	db, err := genji.Open(":memory:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	ch := make(chan struct{})
+	done := make(chan struct{})
+
+	go func() {
+		// 1. Start transaction T1.
+		tx, err := db.Begin(true)
+		require.NoError(t, err)
+
+		// Start transaction T2.
+		ch <- struct{}{}
+		// Wait in case goroutine gets rescheduled.
+		time.Sleep(time.Millisecond)
+
+		// 3. Commit or rollback T1.
+		tx.Rollback()
+
+		// Wait for T2 to finish and return.
+		<-ch
+		done <- struct{}{}
+	}()
+
+	go func() {
+		<-ch // wait for T1 to start.
+
+		// 2. Attempt to start transaction T2.
+		// Waits for T1 to finish.
+		tx, err := db.Begin(true)
+		require.NoError(t, err)
+		defer tx.Rollback()
+
+		ch <- struct{}{}
+	}()
+
+	r := make(chan bool)
+	go func() {
+		t := time.NewTimer(time.Second)
+		select {
+		case <-t.C:
+			r <- false
+		case <-done:
+			r <- true
+		}
+		if !t.Stop() {
+			<-t.C
+		}
+	}()
+
+	if ok := <-r; !ok {
+		t.Fatal("deadlock")
+	}
+}
