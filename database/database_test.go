ac9e33be665939fcfb7c6ad02629916f0ea0faef
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2020-10-30 16:15:46 +0400

    Check errors on deferred functions
    Co-authored-by: Ivan Trubach <mr.trubach@icloud.com>

--- database/database_test.go
+++ database/database_test.go
@@ -12,7 +12,9 @@ import (
 func TestConcurrentTransactionManagement(t *testing.T) {
 	db, err := genji.Open(":memory:")
 	require.NoError(t, err)
-	defer db.Close()
+	defer func() {
+		require.NoError(t, db.Close())
+	}()
 
 	ch := make(chan struct{})
 	done := make(chan struct{})
@@ -28,7 +30,7 @@ func TestConcurrentTransactionManagement(t *testing.T) {
 		time.Sleep(time.Millisecond)
 
 		// 3. Commit or rollback T1.
-		tx.Rollback()
+		require.NoError(t, tx.Rollback())
 
 		// Wait for T2 to finish and return.
 		<-ch
@@ -42,7 +44,7 @@ func TestConcurrentTransactionManagement(t *testing.T) {
 		// Waits for T1 to finish.
 		tx, err := db.Begin(true)
 		require.NoError(t, err)
-		defer tx.Rollback()
+		require.NoError(t, tx.Rollback())
 
 		ch <- struct{}{}
 	}()
