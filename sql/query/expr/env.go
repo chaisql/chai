43726b2667fdf498a0cb3589205350630ecdf345
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2020-12-23 15:08:14 +0400

    Add outer environment

--- sql/query/expr/env.go
+++ sql/query/expr/env.go
@@ -15,6 +15,8 @@ const (
 type Environment struct {
 	Params []Param
 	fb     *document.FieldBuffer
+
+	Outer *Environment
 }
 
 func NewEnvironment(v document.Value, params ...Param) *Environment {
@@ -31,17 +33,23 @@ func NewEnvironment(v document.Value, params ...Param) *Environment {
 }
 
 func (e *Environment) Get(name string) (v document.Value, ok bool) {
-	if e.fb == nil {
-		return
+	if e.fb != nil {
+		v, err := e.fb.GetByField(name)
+		if err == nil {
+			return v, true
+		}
 	}
 
-	v, err := e.fb.GetByField(name)
-	return v, err == document.ErrFieldNotFound
+	if e.Outer != nil {
+		return e.Outer.Get(name)
+	}
+
+	return
 }
 
 func (e *Environment) Set(name string, v document.Value) {
 	if e.fb == nil {
-		return
+		e.fb = document.NewFieldBuffer()
 	}
 
 	e.fb.Set(document.Path{document.PathFragment{FieldName: name}}, v)
@@ -55,7 +63,13 @@ func (e *Environment) SetCurrentValue(v document.Value) {
 	e.Set(currentValueKey, v)
 }
 
-func (e *Environment) GetParamByName(name string) (document.Value, error) {
+func (e *Environment) GetParamByName(name string) (v document.Value, err error) {
+	if len(e.Params) == 0 {
+		if e.Outer != nil {
+			return e.Outer.GetParamByName(name)
+		}
+	}
+
 	for _, nv := range e.Params {
 		if nv.Name == name {
 			return document.NewValue(nv.Value)
@@ -66,6 +80,12 @@ func (e *Environment) GetParamByName(name string) (document.Value, error) {
 }
 
 func (e *Environment) GetParamByIndex(pos int) (document.Value, error) {
+	if len(e.Params) == 0 {
+		if e.Outer != nil {
+			return e.Outer.GetParamByIndex(pos)
+		}
+	}
+
 	idx := int(pos - 1)
 	if idx >= len(e.Params) {
 		return document.Value{}, fmt.Errorf("cannot find param number %d", pos)
