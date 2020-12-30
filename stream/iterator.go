1aaff902c870462be39ac9b6582c4855c4c52798
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2020-12-30 16:28:36 +0400

    Add tableIterator

--- stream/iterator.go
+++ stream/iterator.go
@@ -1,6 +1,9 @@
 package stream
 
 import (
+	"fmt"
+
+	"github.com/genjidb/genji/database"
 	"github.com/genjidb/genji/document"
 	"github.com/genjidb/genji/sql/query/expr"
 )
@@ -40,4 +43,30 @@ func (it documentIterator) Iterate(fn func(env *expr.Environment) error) error {
 // NewDocumentIterator creates an iterator that iterates over the given values.
 func NewDocumentIterator(documents ...document.Document) Iterator {
 	return documentIterator(documents)
+}
+
+// A TableIterator iterates over the documents of a table.
+type TableIterator struct {
+	Table  *database.Table
+	Params []expr.Param
+}
+
+// NewTableIterator creats an iterator that iterates over each document of the given table.
+func NewTableIterator(t *database.Table, params []expr.Param) *TableIterator {
+	return &TableIterator{Table: t, Params: params}
+}
+
+// Iterate over the documents of the table. Each document is stored in the environment
+// that is passed to the fn function, using SetCurrentValue.
+func (it *TableIterator) Iterate(fn func(env *expr.Environment) error) error {
+	var env expr.Environment
+	env.Params = it.Params
+	return it.Table.Iterate(func(d document.Document) error {
+		env.SetDocument(d)
+		return fn(&env)
+	})
+}
+
+func (it *TableIterator) String() string {
+	return fmt.Sprintf(".%s()", it.Table.Name())
 }
