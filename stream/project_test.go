c1ac620c5bc32981a7dd556652a1bdaad42b2a29
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2021-01-25 10:14:59 +0400

    Add support for Project with no input

--- stream/project_test.go
+++ stream/project_test.go
@@ -40,6 +40,13 @@ func TestProject(t *testing.T) {
 			`{"a":1,"b":[true],"a":1,"b":[true],"10":10}`,
 			false,
 		},
+		{
+			"Named",
+			[]expr.Expr{&expr.NamedExpr{Expr: parser.MustParseExpr("10"), ExprName: "foo"}},
+			docFromJSON(`{"a":1,"b":[true]}`),
+			`{"foo":10}`,
+			false,
+		},
 	}
 
 	for _, test := range tests {
@@ -47,24 +54,24 @@ func TestProject(t *testing.T) {
 			var inEnv expr.Environment
 			inEnv.SetDocument(test.in)
 
-			op, err := stream.Project(test.exprs...).Op()
-			require.NoError(t, err)
-			env, err := op(&inEnv)
-			if test.fails {
-				require.Error(t, err)
-			} else {
-				require.NoError(t, err)
-				require.Equal(t, &inEnv, env.Outer)
-				d, ok := env.GetDocument()
+			err := stream.Project(test.exprs...).Iterate(&inEnv, func(out *expr.Environment) error {
+				require.Equal(t, &inEnv, out.Outer)
+				d, ok := out.GetDocument()
 				require.True(t, ok)
 				require.JSONEq(t, test.out, document.NewDocumentValue(d).String())
 
-				err = d.Iterate(func(field string, want document.Value) error {
+				err := d.Iterate(func(field string, want document.Value) error {
 					got, err := d.GetByField(field)
 					require.NoError(t, err)
 					require.Equal(t, want, got)
 					return nil
 				})
+				require.NoError(t, err)
+				return nil
+			})
+			if test.fails {
+				require.Error(t, err)
+			} else {
 				require.NoError(t, err)
 			}
 		})
@@ -77,5 +84,16 @@ func TestProject(t *testing.T) {
 			expr.Wildcard{},
 			parser.MustParseExpr("1 +    1"),
 		).String())
+	})
+
+	t.Run("No input", func(t *testing.T) {
+		stream.Project(parser.MustParseExpr("1 + 1")).Iterate(expr.NewEnvironment(nil), func(out *expr.Environment) error {
+			d, ok := out.GetDocument()
+			require.True(t, ok)
+			enc, err := document.MarshalJSON(d)
+			require.NoError(t, err)
+			require.JSONEq(t, `{"1 + 1": 2}`, string(enc))
+			return nil
+		})
 	})
 }
