464b14a4ef97dbca9039915c074b77ebf5ba67f2
Jean-Hadrien Chabran <jh@chabran.fr>
2021-04-08 12:40:31 +0200

    Add multiline assertion support

--- cmd/examplar/extest1.sql
+++ cmd/examplar/extest1.sql
@@ -8,3 +8,11 @@ DROP TABLE foo;
 INSERT INTO foo (a) VALUES (1);
 SELECT * FROM foo;
 --- `{"a": 1}`
+
+SELECT a, b FROM foo;
+--- ```json
+--- {
+---   "a": 1,
+---   "b": null
+--- }
+--- ```
--- cmd/examplar/main.go
+++ cmd/examplar/main.go
@@ -3,104 +3,241 @@ package main
 import (
 	"bufio"
 	"io"
+	"regexp"
 	"strings"
 	"text/template"
 )
 
-const comment = "---"
+const commentPrefix = "---"
 
-type State int
+type Tag int
 
 const (
-	ILLEGAL State = iota
+	UNKNOWN Tag = iota
 	SETUP
 	TEARDOWN
 	TEST
-	ASSERT_EQ
 )
 
-type example struct {
+// Test is a list of statements.
+type Test struct {
 	name       string
 	statements []*Statement
 }
 
-type examplar struct {
+// Statement is a pair composed of a line of code and an expectation on its result when evaluated.
+type Statement struct {
+	Code        string
+	Expectation string
+}
+
+// Examplar represents a group of tests and can optionally include setup code and teardown code.
+type Examplar struct {
 	name     string
 	setup    []string
 	teardown []string
-	examples []*example
+	examples []*Test
 }
 
-type Statement struct {
-	Code        string
-	EqAssertion string
+// HasSetup returns true if setup code is provided.
+func (ex *Examplar) HasSetup() bool {
+	return len(ex.setup) > 0
 }
 
-func parse(r io.Reader, name string) (*examplar, error) {
-	ex := examplar{name: name}
-	var state State
+// HasSetup returns true if teardown code is provided.
+func (ex *Examplar) HasTeardown() bool {
+	return len(ex.teardown) > 0
+}
 
-	s := bufio.NewScanner(r)
-	s.Split(bufio.ScanLines)
+func (ex *Examplar) appendTest(name string) {
+	ex.examples = append(ex.examples, &Test{
+		name: name,
+	})
+}
 
-	for s.Scan() {
-		line := s.Text()
-		// fmt.Println("state ", state, "| ", line)
+func (ex *Examplar) currentTest() *Test {
+	return ex.examples[len(ex.examples)-1]
+}
 
-		if line == "" {
+type stateFn func(*Scanner) stateFn
+
+type Scanner struct {
+	line string
+	ex   *Examplar
+}
+
+func initialState(s *Scanner) stateFn {
+	if tag, data := parseTag(s.line); tag != UNKNOWN {
+		switch tag {
+		case SETUP:
+			return setupState
+		case TEARDOWN:
+			return teardownState
+		case TEST:
+			s.ex.appendTest(data)
+			return testState
+		}
+	}
+
+	return initialState
+}
+
+func setupState(s *Scanner) stateFn {
+	if tag, data := parseTag(s.line); tag != UNKNOWN {
+		switch tag {
+		case SETUP:
+			return errorState
+		case TEARDOWN:
+			if s.ex.HasTeardown() {
+				return errorState
+			} else {
+				return teardownState
+			}
+		case TEST:
+			s.ex.appendTest(data)
+			return testState
+		}
+	}
+
+	s.ex.setup = append(s.ex.setup, s.line)
+	return setupState
+}
+
+func teardownState(s *Scanner) stateFn {
+	if tag, data := parseTag(s.line); tag != UNKNOWN {
+		switch tag {
+		case SETUP:
+			if s.ex.HasSetup() {
+				return errorState
+			} else {
+				return setupState
+			}
+		case TEARDOWN:
+			return errorState
+		case TEST:
+			s.ex.appendTest(data)
+			return testState
+		}
+	}
+
+	s.ex.teardown = append(s.ex.teardown, s.line)
+	return teardownState
+}
+
+func testState(s *Scanner) stateFn {
+	if tag, data := parseTag(s.line); tag != UNKNOWN {
+		switch tag {
+		case SETUP:
+			return errorState
+		case TEARDOWN:
+			return errorState
+		case TEST:
+			s.ex.appendTest(data)
+			return testState
+		}
+	}
+
+	test := s.ex.currentTest()
+
+	if hasMultilineAssertionTag(s.line) {
+		return multilineAssertionState
+	}
+
+	if assertion := parseSingleAssertion(s.line); len(assertion) > 0 {
+		stmt := test.statements[len(test.statements)-1]
+		stmt.Expectation = assertion
+		return testState
+	}
+
+	test.statements = append(test.statements, &Statement{
+		Code: s.line,
+	})
+
+	return testState
+}
+
+func multilineAssertionState(s *Scanner) stateFn {
+	re := regexp.MustCompile(`^\s*` + commentPrefix + `\s*(.*)`)
+	matches := re.FindStringSubmatch(s.line)
+
+	if matches == nil {
+		return multilineAssertionState
+	}
+
+	code := strings.TrimRight(matches[1], " \t")
+
+	if code == "```" {
+		return testState
+	}
+
+	test := s.ex.currentTest()
+	test.statements[len(test.statements)-1].Expectation += code + "\n"
+	return multilineAssertionState
+}
+
+func errorState(s *Scanner) stateFn {
+	panic(s.line)
+}
+
+func (s *Scanner) Run(io *bufio.Scanner) *Examplar {
+	s.ex = &Examplar{}
+
+	for state := initialState; io.Scan(); {
+		s.line = io.Text()
+		s.line = strings.TrimSpace(s.line)
+		if s.line == "" {
 			continue
 		}
-
-		if strings.HasPrefix(line, comment) {
-			c := strings.TrimPrefix(line, comment)
-			c = strings.TrimLeft(c, " ")
-
-			switch {
-			case strings.HasPrefix(c, "setup:"):
-				state = SETUP
-			case strings.HasPrefix(c, "teardown:"):
-				state = TEARDOWN
-			case strings.HasPrefix(c, "test:"):
-				if state != TEST {
-					name := strings.TrimPrefix(c, "test:")
-					name = strings.TrimSpace(name)
-					ex.examples = append(ex.examples, &example{name: name})
-				}
-				state = TEST
-			case strings.HasPrefix(c, "`"):
-				state = ASSERT_EQ
-				expected := strings.TrimPrefix(c, "`")
-				expected = strings.TrimSuffix(expected, "`")
-
-				t := ex.examples[len(ex.examples)-1]
-				stmt := t.statements[len(t.statements)-1]
-				stmt.EqAssertion = expected
-			default:
-				state = ILLEGAL
-			}
-		} else {
-			switch state {
-			case SETUP:
-				ex.setup = append(ex.setup, line)
-			case TEARDOWN:
-				ex.teardown = append(ex.teardown, line)
-			case TEST:
-				t := ex.examples[len(ex.examples)-1]
-				t.statements = append(t.statements, &Statement{
-					Code: line,
-				})
-			case ASSERT_EQ:
-			case ILLEGAL:
-				panic(line)
-			}
-		}
+		state = state(s)
 	}
 
-	if err := s.Err(); err != nil {
-		return nil, err
+	return s.ex
+}
+
+func parseTag(line string) (Tag, string) {
+	re := regexp.MustCompile(`^\s*` + commentPrefix + `\s*(\w+):\s*(.*)`)
+	matches := re.FindStringSubmatch(line)
+	if matches == nil {
+		return UNKNOWN, ""
 	}
 
-	return &ex, nil
+	var tag Tag
+	switch strings.ToLower(matches[1]) {
+	case "setup":
+		tag = SETUP
+	case "teardown":
+		tag = TEARDOWN
+	case "test":
+		tag = TEST
+	default:
+		return UNKNOWN, ""
+	}
+
+	return tag, matches[2]
+}
+
+func parseSingleAssertion(line string) string {
+	re := regexp.MustCompile(`^\s*` + commentPrefix + `\s*` + "`" + `([^` + "`" + `]+)`)
+	matches := re.FindStringSubmatch(line)
+	if matches == nil {
+		return ""
+	}
+	return matches[1]
+}
+
+func hasMultilineAssertionTag(line string) bool {
+	re := regexp.MustCompile(`^\s*` + commentPrefix + `\s*` + "```" + `(\w*)`)
+	matches := re.FindStringSubmatch(line)
+	return matches != nil
+}
+
+func Parse(r io.Reader, name string) (*Examplar, error) {
+	scanner := &Scanner{}
+
+	ex := scanner.Run(bufio.NewScanner(r))
+	ex.name = name
+
+	return ex, nil
 }
 
 func normalizeTestName(name string) string {
@@ -109,7 +246,7 @@ func normalizeTestName(name string) string {
 	return strings.ReplaceAll(name, " ", "")
 }
 
-func generate(ex *examplar, w io.Writer) error {
+func Generate(ex *Examplar, w io.Writer) error {
 	tmpl := template.Must(template.ParseFiles("test_template.go.tmpl"))
 
 	bindings := struct {
