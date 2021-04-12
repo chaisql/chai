7e2432fe9bb81a7d605ef2603d63624a0a9d2850
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2021-04-12 23:24:10 +0400

    Remove unnecessary spaces

--- cmd/genji/dbutil/dump.go
+++ cmd/genji/dbutil/dump.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"strings"
-	
+
 	"github.com/genjidb/genji"
 	"github.com/genjidb/genji/document"
 	"go.uber.org/multierr"
@@ -19,23 +19,23 @@ func Dump(ctx context.Context, db *genji.DB, w io.Writer, tables ...string) erro
 		return err
 	}
 	defer tx.Rollback()
-	
+
 	if _, err = fmt.Fprintln(w, "BEGIN TRANSACTION;"); err != nil {
 		return err
 	}
-	
+
 	query := "SELECT table_name FROM __genji_tables"
 	if len(tables) > 0 {
 		query += " WHERE table_name IN ?"
 	}
-	
+
 	res, err := tx.Query(query, tables)
 	if err != nil {
 		_, er := fmt.Fprintln(w, "ROLLBACK;")
 		return multierr.Append(err, er)
 	}
 	defer res.Close()
-	
+
 	i := 0
 	err = res.Iterate(func(d document.Document) error {
 		// Blank separation between tables.
@@ -45,20 +45,20 @@ func Dump(ctx context.Context, db *genji.DB, w io.Writer, tables ...string) erro
 			}
 		}
 		i++
-		
+
 		// Get table name.
 		var tableName string
 		if err := document.Scan(d, &tableName); err != nil {
 			return err
 		}
-		
+
 		return dumpTable(tx, w, tableName)
 	})
 	if err != nil {
 		_, er := fmt.Fprintln(w, "ROLLBACK;")
 		return multierr.Append(err, er)
 	}
-	
+
 	_, err = fmt.Fprintln(w, "COMMIT;")
 	return err
 }
@@ -69,14 +69,14 @@ func dumpTable(tx *genji.Tx, w io.Writer, tableName string) error {
 	if err := dumpSchema(tx, w, tableName); err != nil {
 		return err
 	}
-	
+
 	q := fmt.Sprintf("SELECT * FROM %s", tableName)
 	res, err := tx.Query(q)
 	if err != nil {
 		return err
 	}
 	defer res.Close()
-	
+
 	// Inserts statements.
 	insert := fmt.Sprintf("INSERT INTO %s VALUES", tableName)
 	return res.Iterate(func(d document.Document) error {
@@ -84,11 +84,11 @@ func dumpTable(tx *genji.Tx, w io.Writer, tableName string) error {
 		if err != nil {
 			return err
 		}
-		
+
 		if _, err := fmt.Fprintf(w, "%s %s;\n", insert, string(data)); err != nil {
 			return err
 		}
-		
+
 		return nil
 	})
 }
@@ -101,18 +101,18 @@ func DumpSchema(ctx context.Context, db *genji.DB, w io.Writer, tables ...string
 		return err
 	}
 	defer tx.Rollback()
-	
+
 	query := "SELECT table_name FROM __genji_tables"
 	if len(tables) > 0 {
 		query += " WHERE table_name IN ?"
 	}
-	
+
 	res, err := tx.Query(query, tables)
 	if err != nil {
 		return err
 	}
 	defer res.Close()
-	
+
 	i := 0
 	return res.Iterate(func(d document.Document) error {
 		// Blank separation between tables.
@@ -122,13 +122,13 @@ func DumpSchema(ctx context.Context, db *genji.DB, w io.Writer, tables ...string
 			}
 		}
 		i++
-		
+
 		// Get table name.
 		var tableName string
 		if err := document.Scan(d, &tableName); err != nil {
 			return err
 		}
-		
+
 		return dumpSchema(tx, w, tableName)
 	})
 }
@@ -139,14 +139,14 @@ func dumpSchema(tx *genji.Tx, w io.Writer, tableName string) error {
 	if err != nil {
 		return err
 	}
-	
+
 	_, err = fmt.Fprintf(w, "CREATE TABLE %s", tableName)
 	if err != nil {
 		return err
 	}
-	
+
 	ti := t.Info()
-	
+
 	fcs := ti.FieldConstraints
 	// Fields constraints should be displayed between parenthesis.
 	if len(fcs) > 0 {
@@ -155,7 +155,7 @@ func dumpSchema(tx *genji.Tx, w io.Writer, tableName string) error {
 			return err
 		}
 	}
-	
+
 	for i, fc := range fcs {
 		// Don't display the last comma.
 		if i > 0 {
@@ -164,21 +164,21 @@ func dumpSchema(tx *genji.Tx, w io.Writer, tableName string) error {
 				return err
 			}
 		}
-		
+
 		// Construct the fields constraints
 		if _, err := fmt.Fprintf(w, " %s %s", fcs[i].Path.String(), strings.ToUpper(fcs[i].Type.String())); err != nil {
 			return err
 		}
-		
+
 		f := ""
 		if fc.IsPrimaryKey {
 			f += " PRIMARY KEY"
 		}
-		
+
 		if fc.IsNotNull {
 			f += " NOT NULL"
 		}
-		
+
 		if fc.HasDefaultValue() {
 			if _, err := fmt.Fprintf(w, "%s DEFAULT %s", f, fc.DefaultValue.String()); err != nil {
 				return err
@@ -189,7 +189,7 @@ func dumpSchema(tx *genji.Tx, w io.Writer, tableName string) error {
 			}
 		}
 	}
-	
+
 	// Fields constraints close parenthesis.
 	if len(fcs) > 0 {
 		if _, err := fmt.Fprintln(w, "\n);"); err != nil {
@@ -200,22 +200,22 @@ func dumpSchema(tx *genji.Tx, w io.Writer, tableName string) error {
 			return err
 		}
 	}
-	
+
 	// Indexes statements.
 	indexes := t.Indexes()
-	
+
 	for _, index := range indexes {
 		u := ""
 		if index.Info.Unique {
 			u = " UNIQUE"
 		}
-		
+
 		_, err = fmt.Fprintf(w, "CREATE%s INDEX %s ON %s (%s);\n", u, index.Info.IndexName, index.Info.TableName,
 			index.Info.Path)
 		if err != nil {
 			return err
 		}
 	}
-	
+
 	return nil
 }
