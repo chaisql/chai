a18696af1f6446a1348f848a1648b8705dd93deb
Jean-Hadrien Chabran <jh@chabran.fr>
2021-05-10 20:07:28 +0200

    Use go-cmp to compare documents

--- testutil/gen.go
+++ testutil/gen.go
@@ -8,15 +8,16 @@ import (
 	"github.com/genjidb/genji/expr"
 	"github.com/genjidb/genji/query"
 	"github.com/genjidb/genji/sql/parser"
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/require"
 )
 
-type DocsStream struct {
+type ResultStream struct {
 	*parser.Parser
 	env *expr.Environment
 }
 
-func (ds *DocsStream) Next() (*document.Value, error) {
+func (ds *ResultStream) Next() (*document.Value, error) {
 	exp, err := ds.Parser.ParseDocument()
 	if err != nil {
 		return nil, err
@@ -30,42 +31,89 @@ func (ds *DocsStream) Next() (*document.Value, error) {
 	return &v, nil
 }
 
-func ParseDocsStream(stream string) *DocsStream {
+func ParseResultStream(stream string) *ResultStream {
 	p := parser.NewParser(strings.NewReader(stream))
 	env := expr.NewEnvironment(document.NewFieldBuffer())
 
-	return &DocsStream{p, env}
+	return &ResultStream{p, env}
+}
+
+// val is a fully realized representation of a document.Value, suitable for
+// comparison with go-cmp.
+type val struct {
+	Type string
+	V    interface{}
+}
+
+func transformV(v document.Value) val {
+	var vi interface{}
+
+	if v.Type == document.DocumentValue {
+		vi = transformDoc(v.V.(document.Document))
+	} else if v.Type == document.ArrayValue {
+		vi = transformArray(v.V.(document.Array))
+	} else {
+		vi = v.String()
+	}
+
+	return val{
+		Type: v.Type.String(),
+		V:    vi,
+	}
+}
+
+type field struct {
+	Field string
+	V     val
+}
+
+// doc is a fully realized representation of a document.Document, suitable for
+// comparison with go-cmp.
+type doc []field
+
+func transformDoc(d document.Document) doc {
+	fields := make([]field, 0)
+	_ = d.Iterate(func(name string, v document.Value) error {
+		fields = append(fields, field{Field: name, V: transformV(v)})
+		return nil
+	})
+
+	return fields
+}
+
+func transformArray(a document.Array) []val {
+	fields := make([]val, 0)
+	_ = a.Iterate(func(i int, v document.Value) error {
+		fields = append(fields, transformV(v))
+		return nil
+	})
+
+	return fields
 }
 
 func RequireStreamEq(t *testing.T, raw string, res *query.Result) {
 	t.Helper()
-	var count int
-	docs := ParseDocsStream(raw)
+	docs := ParseResultStream(raw)
+	var want []*val
+
+	for v, err := docs.Next(); err == nil; v, err = docs.Next() {
+		val := transformV(*v)
+		want = append(want, &val)
+	}
+
+	var got []*val
 	err := res.Iterate(func(d document.Document) error {
-		count++
-
-		want, err := docs.Next()
-		require.NoError(t, err)
-		got := document.NewDocumentValue(d)
-
-		eq, err := want.IsEqual(got)
-
-		require.NoError(t, err, "comparing %s and %s yielded an error: %e", want, got, err)
-		require.Truef(t, eq, "expected values to be equal\nwant %s to eq got %s", want.String(), got.String())
-		return err
+		val := transformV(document.NewDocumentValue(d))
+		got = append(got, &val)
+		return nil
 	})
 	require.NoError(t, err)
 
-	// there should not be any document left
-	total := count
-	var missing []string
-	for v, err := docs.Next(); err == nil; v, err = docs.Next() {
-		missing = append(missing, v.String())
-		total++
+	// Working with simple structs rather than providing an equality function to go-cmp
+	// allows to get much better diffs, which only show where the difference is rater than
+	// fully displaying both documents.
+	diff := cmp.Diff(want, got)
+	if diff != "" {
+		require.Failf(t, "mismatched documents, (-want, +got)", "%s", diff)
 	}
-
-	require.Equalf(t, total, count, "want %d documents but got %d instead.\nMissing documents:\n%s",
-		total,
-		count,
-		strings.Join(missing, "\n"))
 }
