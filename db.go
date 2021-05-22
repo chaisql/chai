47ccd95a53b904c81c560b66ec5d38cf365dc101
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2021-05-22 21:45:54 +0400

    Add prepared statements

--- db.go
+++ db.go
@@ -12,8 +12,7 @@ import (
 
 // DB represents a collection of tables stored in the underlying engine.
 type DB struct {
-	DB *database.Database
-
+	DB  *database.Database
 	ctx context.Context
 }
 
@@ -72,11 +71,130 @@ func (db *DB) Update(fn func(tx *Tx) error) error {
 	return tx.Commit()
 }
 
+// Query the database and return the result.
+// The returned result must always be closed after usage.
+func (db *DB) Query(q string, args ...interface{}) (*query.Result, error) {
+	stmt, err := db.Prepare(q)
+	if err != nil {
+		return nil, err
+	}
+
+	return stmt.Query(args...)
+}
+
+// QueryDocument runs the query and returns the first document.
+// If the query returns no error, QueryDocument returns database.ErrDocumentNotFound.
+func (db *DB) QueryDocument(q string, args ...interface{}) (document.Document, error) {
+	stmt, err := db.Prepare(q)
+	if err != nil {
+		return nil, err
+	}
+
+	return stmt.QueryDocument(args...)
+}
+
 // Exec a query against the database without returning the result.
 func (db *DB) Exec(q string, args ...interface{}) error {
-	res, err := db.Query(q, args...)
+	stmt, err := db.Prepare(q)
 	if err != nil {
 		return err
+	}
+
+	return stmt.Exec(args...)
+}
+
+// Prepare parses the query and returns a prepared statement.
+func (db *DB) Prepare(q string) (*Statement, error) {
+	pq, err := parser.ParseQuery(q)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Statement{
+		pq: pq,
+		db: db,
+	}, nil
+}
+
+// Tx represents a database transaction. It provides methods for managing the
+// collection of tables and the transaction itself.
+// Tx is either read-only or read/write. Read-only can be used to read tables
+// and read/write can be used to read, create, delete and modify tables.
+type Tx struct {
+	*database.Transaction
+}
+
+// Query the database withing the transaction and returns the result.
+// Closing the returned result after usage is not mandatory.
+func (tx *Tx) Query(q string, args ...interface{}) (*query.Result, error) {
+	stmt, err := tx.Prepare(q)
+	if err != nil {
+		return nil, err
+	}
+
+	return stmt.Query(args...)
+}
+
+// QueryDocument runs the query and returns the first document.
+// If the query returns no error, QueryDocument returns database.ErrDocumentNotFound.
+func (tx *Tx) QueryDocument(q string, args ...interface{}) (document.Document, error) {
+	stmt, err := tx.Prepare(q)
+	if err != nil {
+		return nil, err
+	}
+
+	return stmt.QueryDocument(args...)
+}
+
+// Exec a query against the database within tx and without returning the result.
+func (tx *Tx) Exec(q string, args ...interface{}) (err error) {
+	stmt, err := tx.Prepare(q)
+	if err != nil {
+		return err
+	}
+
+	return stmt.Exec(args...)
+}
+
+// Prepare parses the query and returns a prepared statement.
+func (tx *Tx) Prepare(q string) (*Statement, error) {
+	pq, err := parser.ParseQuery(q)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Statement{
+		pq: pq,
+		tx: tx,
+	}, nil
+}
+
+// Statement is a prepared statement. If Statement has been created on a Tx,
+// it will only be valid until Tx closes. If it has been created on a DB, it
+// is valid until the DB closes.
+// It's safe for concurrent use by multiple goroutines.
+type Statement struct {
+	pq query.Query
+	db *DB
+	tx *Tx
+}
+
+// Query the database and return the result.
+// The returned result must always be closed after usage.
+func (s *Statement) Query(args ...interface{}) (*query.Result, error) {
+	if s.tx != nil {
+		return s.pq.Exec(s.tx.Transaction, argsToParams(args))
+	}
+
+	return s.pq.Run(s.db.ctx, s.db.DB, argsToParams(args))
+}
+
+// QueryDocument runs the query and returns the first document.
+// If the query returns no error, QueryDocument returns database.ErrDocumentNotFound.
+func (s *Statement) QueryDocument(args ...interface{}) (d document.Document, err error) {
+	res, err := s.Query(args...)
+	if err != nil {
+		return nil, err
 	}
 	defer func() {
 		er := res.Close()
@@ -84,31 +202,6 @@ func (db *DB) Exec(q string, args ...interface{}) error {
 			err = er
 		}
 	}()
-
-	return res.Iterate(func(d document.Document) error {
-		return nil
-	})
-}
-
-// Query the database and return the result.
-// The returned result must always be closed after usage.
-func (db *DB) Query(q string, args ...interface{}) (*query.Result, error) {
-	pq, err := parser.ParseQuery(q)
-	if err != nil {
-		return nil, err
-	}
-
-	return pq.Run(db.ctx, db.DB, argsToParams(args))
-}
-
-// QueryDocument runs the query and returns the first document.
-// If the query returns no error, QueryDocument returns database.ErrDocumentNotFound.
-func (db *DB) QueryDocument(q string, args ...interface{}) (document.Document, error) {
-	res, err := db.Query(q, args...)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Close()
 
 	return scanDocument(res)
 }
@@ -132,40 +225,9 @@ func scanDocument(res *query.Result) (document.Document, error) {
 	return fb, err
 }
 
-// Tx represents a database transaction. It provides methods for managing the
-// collection of tables and the transaction itself.
-// Tx is either read-only or read/write. Read-only can be used to read tables
-// and read/write can be used to read, create, delete and modify tables.
-type Tx struct {
-	*database.Transaction
-}
-
-// Query the database withing the transaction and returns the result.
-// Closing the returned result after usage is not mandatory.
-func (tx *Tx) Query(q string, args ...interface{}) (*query.Result, error) {
-	pq, err := parser.ParseQuery(q)
-	if err != nil {
-		return nil, err
-	}
-
-	return pq.Exec(tx.Transaction, argsToParams(args))
-}
-
-// QueryDocument runs the query and returns the first document.
-// If the query returns no error, QueryDocument returns database.ErrDocumentNotFound.
-func (tx *Tx) QueryDocument(q string, args ...interface{}) (document.Document, error) {
-	res, err := tx.Query(q, args...)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Close()
-
-	return scanDocument(res)
-}
-
-// Exec a query against the database within tx and without returning the result.
-func (tx *Tx) Exec(q string, args ...interface{}) (err error) {
-	res, err := tx.Query(q, args...)
+// Exec a query against the database without returning the result.
+func (s *Statement) Exec(args ...interface{}) (err error) {
+	res, err := s.Query(args...)
 	if err != nil {
 		return err
 	}
