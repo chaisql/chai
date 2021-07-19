31dc191468388dabfac17e313f00459c532d1e47
Jean-Hadrien Chabran <jh@chabran.fr>
2021-07-19 10:45:43 +0200

    Add math.acos and math.abs

--- internal/expr/functions/math.go
+++ internal/expr/functions/math.go
@@ -13,10 +13,12 @@ func MathFunctions() Definitions {
 }
 
 var mathFunctions = Definitions{
-	"floor": floorFunc,
+	"floor": floor,
+	"abs":   abs,
+	"acos":  acos,
 }
 
-var floorFunc = &ScalarDefinition{
+var floor = &ScalarDefinition{
 	name:  "floor",
 	arity: 1,
 	callFn: func(args ...types.Value) (types.Value, error) {
@@ -27,6 +29,56 @@ var floorFunc = &ScalarDefinition{
 			return args[0], nil
 		default:
 			return nil, stringutil.Errorf("floor(arg1) expects arg1 to be a number")
+		}
+	},
+}
+
+var abs = &ScalarDefinition{
+	name:  "abs",
+	arity: 1,
+	callFn: func(args ...document.Value) (document.Value, error) {
+		switch args[0].Type {
+		case document.NullValue:
+			return document.NewNullValue(), nil
+		case document.IntegerValue:
+			v := args[0].V.(int64)
+			if v == math.MinInt64 {
+				// If X is the  integer -9223372036854775808 then abs(X) throws an integer overflow
+				// error since there is no equivalent positive 64-bit two complement value.
+				return document.Value{}, stringutil.Errorf("integer overflow")
+			}
+			f := math.Abs(float64(v))
+			return document.NewIntegerValue(int64(f)), nil
+		case document.DoubleValue:
+			v := args[0].V.(float64)
+			return document.NewDoubleValue(math.Abs(v)), nil
+		default:
+			return document.Value{}, stringutil.Errorf("abs(arg1) expects arg1 to be a number or NULL")
+		}
+	},
+}
+
+var acos = &ScalarDefinition{
+	name:  "acos",
+	arity: 1,
+	callFn: func(args ...document.Value) (document.Value, error) {
+		switch args[0].Type {
+		case document.NullValue:
+			return document.NewNullValue(), nil
+		case document.IntegerValue:
+			v := args[0].V.(int64)
+			if v > 1 || v < -1 {
+				return document.Value{}, stringutil.Errorf("out of range, acos(arg1) expects arg1 to be within [-1, 1]")
+			}
+			return document.NewDoubleValue(math.Acos(float64(v))), nil
+		case document.DoubleValue:
+			v := args[0].V.(float64)
+			if v > 1.0 || v < -1.0 {
+				return document.Value{}, stringutil.Errorf("out of range, acos(arg1) expects arg1 to be within [-1, 1]")
+			}
+			return document.NewDoubleValue(math.Acos(v)), nil
+		default:
+			return document.Value{}, stringutil.Errorf("acos(arg1) expects arg1 to be a number within [-1, 1]")
 		}
 	},
 }
--- internal/expr/functions/testdata/math_functions.sql
+++ internal/expr/functions/testdata/math_functions.sql
@@ -1,9 +1,35 @@
 -- test: math.floor
 > math.floor(2.3)
 2.0
-
 > math.floor(2)
 2
-
 ! math.floor('a')
 'floor(arg1) expects arg1 to be a number'
+
+-- test: math.abs
+> math.abs(NULL)
+NULL
+> math.abs(-2)
+2
+> math.abs(-2.0)
+2.0
+! math.abs(-9223372036854775808)
+'integer overflow'
+! math.abs('foo')
+'abs(arg1) expects arg1 to be a number'
+
+-- test: math.acos
+> math.acos(1)
+0.0
+> math.acos(0.5)
+1.0471975511965976
+! math.acos(2)
+'out of range'
+! math.acos(-2)
+'out of range'
+! math.acos(2.2)
+'out of range'
+! math.acos(-2.2)
+'out of range'
+! math.acos('foobar')
+'acos(arg1) expects arg1 to be a number'
