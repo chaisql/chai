51eb0f1f7269ee194b7d00200a29ec1c30caa70a
Jean-Hadrien Chabran <jh@chabran.fr>
2021-07-22 15:37:11 +0200

    Use the new types.Value interface

--- internal/expr/functions/math.go
+++ internal/expr/functions/math.go
@@ -3,6 +3,7 @@ package functions
 import (
 	"math"
 
+	"github.com/genjidb/genji/document"
 	"github.com/genjidb/genji/internal/stringutil"
 	"github.com/genjidb/genji/types"
 )
@@ -41,125 +42,125 @@ var floor = &ScalarDefinition{
 var abs = &ScalarDefinition{
 	name:  "abs",
 	arity: 1,
-	callFn: func(args ...document.Value) (document.Value, error) {
-		if args[0].Type == document.NullValue {
-			return document.NewNullValue(), nil
+	callFn: func(args ...types.Value) (types.Value, error) {
+		if args[0].Type() == types.NullValue {
+			return types.NewNullValue(), nil
 		}
-		v, err := args[0].CastAs(document.DoubleValue)
+		v, err := document.CastAs(args[0], types.DoubleValue)
 		if err != nil {
-			return document.Value{}, err
+			return nil, err
 		}
-		res := math.Abs(v.V.(float64))
-		if args[0].Type == document.IntegerValue {
-			return document.NewDoubleValue(res).CastAsInteger()
+		res := math.Abs(v.V().(float64))
+		if args[0].Type() == types.IntegerValue {
+			return document.CastAs(types.NewDoubleValue(res), types.IntegerValue)
 		}
-		return document.NewDoubleValue(res), nil
+		return types.NewDoubleValue(res), nil
 	},
 }
 
 var acos = &ScalarDefinition{
 	name:  "acos",
 	arity: 1,
-	callFn: func(args ...document.Value) (document.Value, error) {
-		if args[0].Type == document.NullValue {
-			return document.NewNullValue(), nil
+	callFn: func(args ...types.Value) (types.Value, error) {
+		if args[0].Type() == types.NullValue {
+			return types.NewNullValue(), nil
 		}
-		v, err := args[0].CastAs(document.DoubleValue)
+		v, err := document.CastAs(args[0], types.DoubleValue)
 		if err != nil {
-			return document.Value{}, err
+			return nil, err
 		}
-		vv := v.V.(float64)
+		vv := v.V().(float64)
 		if vv > 1.0 || vv < -1.0 {
-			return document.Value{}, stringutil.Errorf("out of range, acos(arg1) expects arg1 to be within [-1, 1]")
+			return nil, stringutil.Errorf("out of range, acos(arg1) expects arg1 to be within [-1, 1]")
 		}
 		res := math.Acos(vv)
-		return document.NewDoubleValue(res), nil
+		return types.NewDoubleValue(res), nil
 	},
 }
 
 var acosh = &ScalarDefinition{
 	name:  "acosh",
 	arity: 1,
-	callFn: func(args ...document.Value) (document.Value, error) {
-		if args[0].Type == document.NullValue {
-			return document.NewNullValue(), nil
+	callFn: func(args ...types.Value) (types.Value, error) {
+		if args[0].Type() == types.NullValue {
+			return types.NewNullValue(), nil
 		}
-		v, err := args[0].CastAs(document.DoubleValue)
+		v, err := document.CastAs(args[0], types.DoubleValue)
 		if err != nil {
-			return document.Value{}, err
+			return nil, err
 		}
-		vv := v.V.(float64)
+		vv := v.V().(float64)
 		if vv < 1.0 {
-			return document.Value{}, stringutil.Errorf("out of range, acosh(arg1) expects arg1 >= 1")
+			return nil, stringutil.Errorf("out of range, acosh(arg1) expects arg1 >= 1")
 		}
 		res := math.Acosh(vv)
-		return document.NewDoubleValue(res), nil
+		return types.NewDoubleValue(res), nil
 	},
 }
 
 var asin = &ScalarDefinition{
 	name:  "asin",
 	arity: 1,
-	callFn: func(args ...document.Value) (document.Value, error) {
-		if args[0].Type == document.NullValue {
-			return document.NewNullValue(), nil
+	callFn: func(args ...types.Value) (types.Value, error) {
+		if args[0].Type() == types.NullValue {
+			return types.NewNullValue(), nil
 		}
-		v, err := args[0].CastAs(document.DoubleValue)
+		v, err := document.CastAs(args[0], types.DoubleValue)
 		if err != nil {
-			return document.Value{}, err
+			return nil, err
 		}
-		vv := v.V.(float64)
+		vv := v.V().(float64)
 		if vv > 1.0 || vv < -1.0 {
-			return document.Value{}, stringutil.Errorf("out of range, asin(arg1) expects arg1 to be within [-1, 1]")
+			return nil, stringutil.Errorf("out of range, asin(arg1) expects arg1 to be within [-1, 1]")
 		}
 		res := math.Asin(vv)
-		return document.NewDoubleValue(res), nil
+		return types.NewDoubleValue(res), nil
 	},
 }
 
 var asinh = &ScalarDefinition{
 	name:  "asinh",
 	arity: 1,
-	callFn: func(args ...document.Value) (document.Value, error) {
-		v, err := args[0].CastAs(document.DoubleValue)
-		if err != nil || v.Type == document.NullValue {
+	callFn: func(args ...types.Value) (types.Value, error) {
+		v, err := document.CastAs(args[0], types.DoubleValue)
+		if err != nil || v.Type() == types.NullValue {
 			return v, err
 		}
-		vv := v.V.(float64)
+		vv := v.V().(float64)
 		res := math.Asinh(vv)
-		return document.NewDoubleValue(res), nil
+		return types.NewDoubleValue(res), nil
 	},
 }
 
 var atan = &ScalarDefinition{
 	name:  "atan",
 	arity: 1,
-	callFn: func(args ...document.Value) (document.Value, error) {
-		v, err := args[0].CastAs(document.DoubleValue)
-		if err != nil || v.Type == document.NullValue {
+	callFn: func(args ...types.Value) (types.Value, error) {
+		v, err := document.CastAs(args[0], types.DoubleValue)
+		if err != nil || v.Type() == types.NullValue {
 			return v, err
 		}
-		vv := v.V.(float64)
+		vv := v.V().(float64)
 		res := math.Atan(vv)
-		return document.NewDoubleValue(res), nil
+		return types.NewDoubleValue(res), nil
 	},
 }
 
 var atan2 = &ScalarDefinition{
 	name:  "atan2",
 	arity: 2,
-	callFn: func(args ...document.Value) (document.Value, error) {
-		vA, err := args[0].CastAs(document.DoubleValue)
-		if err != nil || vA.Type == document.NullValue {
+	callFn: func(args ...types.Value) (types.Value, error) {
+		vA, err := document.CastAs(args[0], types.DoubleValue)
+		if err != nil || vA.Type() == types.NullValue {
 			return vA, err
 		}
-		vvA := vA.V.(float64)
-		vB, err := args[1].CastAs(document.DoubleValue)
-		if err != nil || vB.Type == document.NullValue {
+		vvA := vA.V().(float64)
+		vB, err := document.CastAs(args[1], types.DoubleValue)
+		if err != nil || vB.Type() == types.NullValue {
 			return vB, err
 		}
-		vvB := vB.V.(float64)
+		vvB := vB.V().(float64)
 		res := math.Atan2(vvA, vvB)
-		return document.NewDoubleValue(res), nil
+		return types.NewDoubleValue(res), nil
 	},
 }
